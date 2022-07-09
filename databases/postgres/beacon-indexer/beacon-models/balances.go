15c110617df4f4d284485d43677f4aa31473cfb7
alex <alexg89@vt.edu>
2022-07-08 19:46:03 -0700

    insert and select validator balances

--- databases/postgres/beacon-indexer/beacon-models/balances.go
+++ databases/postgres/beacon-indexer/beacon-models/balances.go
@@ -2,6 +2,7 @@ package beacon_models
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/zeus-fyi/olympus/databases/postgres"
 	"github.com/zeus-fyi/olympus/pkg/utils/strings"
@@ -9,31 +10,61 @@ import (
 
 type ValidatorBalanceEpoch struct {
 	Validator
-	Epoch            int64
-	TotalBalanceGwei int64
+	Epoch                 int64
+	TotalBalanceGwei      int64
+	CurrentEpochYieldGwei int64
+	YieldToDateGwei       int64
 }
 
 type ValidatorBalancesEpoch struct {
-	Validators []ValidatorBalanceEpoch
+	ValidatorBalance []ValidatorBalanceEpoch
 }
 
-func (v *ValidatorBalanceEpoch) GetRowValues() postgres.RowValues {
-	pgValues := postgres.RowValues{v.Epoch, v.TotalBalanceGwei}
+func (vb *ValidatorBalanceEpoch) GetRowValues() postgres.RowValues {
+	pgValues := postgres.RowValues{vb.Epoch, vb.Index, vb.TotalBalanceGwei, vb.CurrentEpochYieldGwei}
 	return pgValues
 }
 
-func (vs *ValidatorBalancesEpoch) GetManyRowValues() postgres.RowEntries {
+func (vb *ValidatorBalancesEpoch) GetManyRowValues() postgres.RowEntries {
 	var pgRows postgres.RowEntries
-	for _, v := range vs.Validators {
-		pgRows.Rows = append(pgRows.Rows, v.GetRowValues())
+	for _, val := range vb.ValidatorBalance {
+		pgRows.Rows = append(pgRows.Rows, val.GetRowValues())
 	}
 	return pgRows
 }
 
-var insertValidatorBalances = "INSERT INTO validator_balances_at_epoch (epoch, validator_index, total_balance_gwei) VALUES "
+func (vb *ValidatorBalancesEpoch) getIndexValues() postgres.RowValues {
+	pgValues := make(postgres.RowValues, len(vb.ValidatorBalance))
+	for i, val := range vb.ValidatorBalance {
+		pgValues[i] = val.Index
+	}
+	return pgValues
+}
 
-func (vs *Validators) InsertValidatorBalances(ctx context.Context) error {
-	query := strings.DelimitedSliceStrBuilderSQLRows(insertValidatorBalances, vs.GetManyRowValues())
+var insertValidatorBalances = "INSERT INTO validator_balances_at_epoch (epoch, validator_index, total_balance_gwei, current_epoch_yield_gwei) VALUES "
+
+func (vb *ValidatorBalancesEpoch) InsertValidatorBalances(ctx context.Context) error {
+	query := strings.DelimitedSliceStrBuilderSQLRows(insertValidatorBalances, vb.GetManyRowValues())
 	_, err := postgres.Pg.Query(ctx, query)
 	return err
+}
+
+func (vb *ValidatorBalancesEpoch) SelectValidatorBalances(ctx context.Context) (*ValidatorBalancesEpoch, error) {
+	params := strings.ArraySliceStrBuilderSQL(vb.getIndexValues())
+	query := fmt.Sprintf("SELECT epoch, validator_index, total_balance_gwei, current_epoch_yield_gwei, yield_to_date_gwei FROM validator_balances_at_epoch WHERE validator_index = %s", params)
+
+	rows, err := postgres.Pg.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	var selectedValidatorBalances ValidatorBalancesEpoch
+	for rows.Next() {
+		var val ValidatorBalanceEpoch
+		rowErr := rows.Scan(&val.Epoch, &val.Index, &val.TotalBalanceGwei, &val.CurrentEpochYieldGwei, &val.YieldToDateGwei)
+		if rowErr != nil {
+			return nil, rowErr
+		}
+		selectedValidatorBalances.ValidatorBalance = append(selectedValidatorBalances.ValidatorBalance, val)
+	}
+	return &selectedValidatorBalances, err
 }

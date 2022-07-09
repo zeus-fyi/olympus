fe743189d5c0661a0be39a260ea3f967b74b0c04
alex <alexg89@vt.edu>
2022-07-08 19:45:57 -0700

    insert and select validator balances

--- databases/postgres/beacon-indexer/beacon-models/balances_test.go
+++ databases/postgres/beacon-indexer/beacon-models/balances_test.go
@@ -1,6 +1,7 @@
 package beacon_models
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -10,8 +11,38 @@ type ValidatorBalancesTestSuite struct {
 	BeaconBaseTestSuite
 }
 
-func (s *ValidatorBalancesTestSuite) TestInsertValidatorBalances() {
+func (s *ValidatorBalancesTestSuite) TestInsertAndSelectValidatorBalances() {
+	vs := createFakeValidators(3)
+	ctx := context.Background()
+	err := vs.InsertValidators(ctx)
+	s.Require().Nil(err)
 
+	expVBs := make(map[int64]ValidatorBalanceEpoch, 3)
+
+	var vbs ValidatorBalancesEpoch
+	for i, v := range vs.Validators {
+		var vb ValidatorBalanceEpoch
+		vb.Epoch = int64(i)
+		vb.Validator = v
+		vb.TotalBalanceGwei = int64(32000000001 - i)
+		vbs.ValidatorBalance = append(vbs.ValidatorBalance, vb)
+		expVBs[vb.Index] = vb
+	}
+
+	err = vbs.InsertValidatorBalances(ctx)
+	s.Require().Nil(err)
+
+	selectedVBs, err := vbs.SelectValidatorBalances(ctx)
+	s.Require().Nil(err)
+
+	for _, testVB := range selectedVBs.ValidatorBalance {
+		expVB := expVBs[testVB.Index]
+		s.Require().NotEmpty(expVB)
+		s.Assert().Equal(expVB.Index, testVB.Index)
+		s.Assert().Equal(expVB.TotalBalanceGwei, testVB.TotalBalanceGwei, "validator_index %s total balance expected to be %s, but was %s", expVB.Index, expVB.TotalBalanceGwei, testVB.TotalBalanceGwei)
+		s.Assert().Equal(expVB.TotalBalanceGwei-32000000000, testVB.YieldToDateGwei, "validator_index %s yield_to_date_gwei expected to be %s, but was %s", expVB.Index, expVB.YieldToDateGwei, testVB.TotalBalanceGwei)
+		//s.Assert().Equal(expVB.CurrentEpochYieldGwei, testVB.CurrentEpochYieldGwei)
+	}
 }
 
 func TestValidatorBalancesTestSuite(t *testing.T) {

f8347845157e80ce69a706f4721d6250f9dab577
alex <alexg89@vt.edu>
2022-07-24 14:15:39 -0700

    add logs to client package

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 type Client struct {
@@ -29,7 +31,7 @@ type Reply struct {
 func (c Client) Get(ctx context.Context, url string) Reply {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Err(err).Msgf("had error when sending url request: %s", url)
 		return Reply{Err: err}
 	}
 	return c.Do(ctx, req)
@@ -38,7 +40,7 @@ func (c Client) Get(ctx context.Context, url string) Reply {
 func (c Client) GetWithPayload(ctx context.Context, url string, payload []byte) Reply {
 	req, err := http.NewRequest("GET", url, bytes.NewBuffer(payload))
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Err(err).Bytes("payload that failed", payload).Msgf("url: %s", url)
 		return Reply{Err: err}
 	}
 	return c.Do(ctx, req)
@@ -48,7 +50,7 @@ func (c Client) Post(ctx context.Context, url string, payload []byte) Reply {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Err(err).Bytes("payload that failed", payload).Msgf("url: %s", url)
 		return Reply{Err: err}
 	}
 	return c.Do(ctx, req)
@@ -60,10 +62,10 @@ func (c Client) Do(ctx context.Context, req *http.Request) Reply {
 	}
 	resp, err := c.C.Do(req)
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Err(err).Msg("had error when sending url request")
 		return Reply{Err: err}
 	}
-	return c.respParser(resp)
+	return c.respParser(ctx, resp)
 }
 
 func (c Client) AppendArbitraryHeaders(ctx context.Context, req *http.Request) *http.Request {
@@ -78,23 +80,26 @@ func (c Client) AddHeader(ctx context.Context, req *http.Request, headerKey, hea
 	return req
 }
 
-func (c Client) respParser(resp *http.Response) Reply {
+func (c Client) respParser(ctx context.Context, resp *http.Response) Reply {
 	r := Reply{}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			fmt.Printf("%v", err)
+			log.Err(err).Msg("had error closing body reader")
 		}
 	}(resp.Body)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		r.Err = err
-		fmt.Printf("%v", err)
+		log.Err(err).Bytes("body bytes in error", body)
 	}
 	r.Status = resp.Status
 	r.BodyBytes = body
 	r.StatusCode = resp.StatusCode
+	if r.StatusCode <= 599 && r.StatusCode >= 400 {
+		log.Ctx(ctx).Info().Msgf("had 4xx-5xx status code resp from request, status: %d", r.StatusCode)
+	}
 	if c.EnableBytesStrDecode {
 		r.Body = fmt.Sprintf("%s", body)
 	}

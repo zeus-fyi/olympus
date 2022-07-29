890151c313757057cc38908bbb671c686b41f55e
alex <alexg89@vt.edu>
2022-07-28 23:43:42 -0700

    refactor

--- pkg/datastores/postgres/beacon_indexer/beacon_models/checkpoint_test.go
+++ pkg/datastores/postgres/beacon_indexer/beacon_models/checkpoint_test.go
@@ -34,67 +34,52 @@ func (c *CheckpointTestSuite) TestInsertCheckpoint() {
 	// epoch 0 - 3 validators active
 	vsOne := createAndInsertFakeValidatorsByStatus(1, "active_ongoing_genesis")
 	_ = createAndInsertFakeValidatorsByStatus(2, "active_ongoing_genesis")
-
 	// epoch 1 - 4 validators_active
 	createAndInsertFakeValidatorsByStatus(1, "active_ongoing_at_epoch_one")
-
 	// epoch 2 - 5 validators_active
 	createAndInsertFakeValidatorsByStatus(1, "active_ongoing_at_epoch_two")
-
 	// epoch 3 - 6 validators_active
 	createAndInsertFakeValidatorsByStatus(1, "active_ongoing_at_epoch_three")
 
 	firstInsertCount, err := SelectCountValidatorActive(ctx, epochZero)
 	c.Assert().Equal(3, firstInsertCount)
 
+	// epoch 0
 	err = InsertEpochCheckpoint(ctx, epochZero)
 	c.Require().Nil(err)
+	c.assertCheckpointValues(ctx, epochZero, 3, 3, 0)
 
-	err = cp.GetEpochCheckpoint(ctx, epochZero)
-	c.Require().Nil(err)
-
-	c.Assert().Equal(3, cp.ValidatorsActive)
-	c.Assert().Equal(3, cp.ValidatorsBalancesRecorded)
-	c.Assert().Equal(0, cp.ValidatorsBalancesRemaining)
 	// epoch 1
 	epochOne := 1
 	err = InsertEpochCheckpoint(ctx, epochOne)
 	c.Require().Nil(err)
-	err = cp.GetEpochCheckpoint(ctx, epochOne)
-	c.Require().Nil(err)
-	c.Assert().Equal(4, cp.ValidatorsActive)
-	c.Assert().Equal(0, cp.ValidatorsBalancesRecorded)
-	c.Assert().Equal(4, cp.ValidatorsBalancesRemaining)
+	c.assertCheckpointValues(ctx, epochOne, 4, 0, 4)
 
 	// epoch 1 and new balance records
-	// balacnce updates only happens via TODO
+	// TODO needs to update this, then check balances
 	seedAndInsertNewValidatorBalances(ctx, vsOne, int64(epochOne), 32000000000+43753)
 	err = cp.GetEpochCheckpoint(ctx, epochOne)
 	c.Require().Nil(err)
-	c.Assert().Equal(4, cp.ValidatorsActive)
-	// TODO needs to update this. Currently failing on purpose
-	//c.Assert().Equal(, cp.ValidatorsBalancesRecorded)
-	//c.Assert().Equal(2, cp.ValidatorsBalancesRemaining)
-	c.Assert().Equal(0, cp.ValidatorsBalancesRecorded)
-	c.Assert().Equal(4, cp.ValidatorsBalancesRemaining)
+	c.assertCheckpointValues(ctx, epochOne, 4, 0, 4)
 
 	// epoch 2
 	epochTwo := 2
 	err = InsertEpochCheckpoint(ctx, epochTwo)
-	err = cp.GetEpochCheckpoint(ctx, epochTwo)
-	c.Require().Nil(err)
-	c.Assert().Equal(5, cp.ValidatorsActive)
-	c.Assert().Equal(0, cp.ValidatorsBalancesRecorded)
-	c.Assert().Equal(5, cp.ValidatorsBalancesRemaining)
+	c.assertCheckpointValues(ctx, epochTwo, 5, 0, 5)
 
 	// epoch 3
 	epochThree := 3
 	err = InsertEpochCheckpoint(ctx, epochThree)
-	err = cp.GetEpochCheckpoint(ctx, epochThree)
+	c.assertCheckpointValues(ctx, epochThree, 6, 0, 6)
+}
+
+func (c *CheckpointTestSuite) assertCheckpointValues(ctx context.Context, epoch, expValsActive, expValBalancesRecorded, expValBalancesRemaining int) {
+	cp := ValidatorsEpochCheckpoint{}
+	err := cp.GetEpochCheckpoint(ctx, epoch)
 	c.Require().Nil(err)
-	c.Assert().Equal(6, cp.ValidatorsActive)
-	c.Assert().Equal(0, cp.ValidatorsBalancesRecorded)
-	c.Assert().Equal(6, cp.ValidatorsBalancesRemaining)
+	c.Assert().Equal(expValsActive, cp.ValidatorsActive)
+	c.Assert().Equal(expValBalancesRecorded, cp.ValidatorsBalancesRecorded)
+	c.Assert().Equal(expValBalancesRemaining, cp.ValidatorsBalancesRemaining)
 }
 
 func (c *CheckpointTestSuite) TestInsertCheckpointWithDiffsAdded() {

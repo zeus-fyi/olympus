768ee768025c8a07663f7173304798c8a25c3b57
alex <alexg89@vt.edu>
2022-07-29 20:58:23 -0700

    add checks to db table

--- pkg/datastores/postgres_docker/docker-entrypoint-initdb.d/init.sql/4_epoch_checkpoint.sql
+++ pkg/datastores/postgres_docker/docker-entrypoint-initdb.d/init.sql/4_epoch_checkpoint.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "public"."validators_epoch_checkpoint" (
      "validators_balance_epoch" serial NOT NULL,
-     "validators_active" int4 NOT NULL,
-     "validators_balances_recorded" int4 NOT NULL DEFAULT 0,
+     "validators_active" int4 NOT NULL CHECK(validators_active >= validators_balances_recorded),
+     "validators_balances_recorded" int4 NOT NULL DEFAULT 0 CHECK(validators_balances_recorded <= validators_active),
      "validators_balances_remaining" int4 NOT NULL GENERATED ALWAYS AS (validators_active - validators_balances_recorded) STORED
 )
 ;
@@ -36,7 +36,7 @@ RETURNS VOID AS
 $BODY$
 DECLARE
 BEGIN
-    UPDATE validators_epoch_checkpoint vc SET validators_balances_recorded = (SELECT validators_balances_recorded_at_epoch(checkpoint_epoch)) WHERE vc.validators_balance_epoch = checkpoint_epoch;
+    UPDATE validators_epoch_checkpoint vc SET validators_balances_recorded = (SELECT validators_balances_recorded_at_epoch(checkpoint_epoch)), validators_active = (SELECT validators_active_at_epoch(checkpoint_epoch)) WHERE vc.validators_balance_epoch = checkpoint_epoch;
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE

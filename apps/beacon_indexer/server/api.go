72a016b63fdbcf3244dc860861c538f7c812d21b
alex <alexg89@vt.edu>
2022-08-02 01:42:06 -0700

    use localhost

--- apps/beacon_indexer/server/api.go
+++ apps/beacon_indexer/server/api.go
@@ -43,7 +43,7 @@ func Api() {
 	_ = postgres.UpdateConfigPG(ctx, pgCfg)
 
 	redisOpts := redis.Options{
-		Addr: "localhost:6379",
+		Addr: "127.0.0.1:6379",
 	}
 	r := redis_app.InitRedis(ctx, redisOpts)
 	_, err := r.Ping(ctx).Result()

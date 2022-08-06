a61388da932905583b09bf71a070a069fc9b300c
alex <alexg89@vt.edu>
2022-08-06 14:01:45 -0700

    adds some delay inbetween fetches

--- apps/beacon_indexer/beacon_indexer/beacon_fetcher/fetcher_forward_checkpoint.go
+++ apps/beacon_indexer/beacon_indexer/beacon_fetcher/fetcher_forward_checkpoint.go
@@ -35,7 +35,7 @@ func FetchAnyValidatorBalancesAfterCheckpoint() {
 		err := fetchAnyValidatorBalancesAfterCheckpoint(context.Background(), FetchAnyValidatorBalancesTimeoutFromCheckpoint)
 		log.Err(err)
 		log.Info().Interface("fetchAnyValidatorBalancesAfterCheckpoint took this many seconds to complete: ", time.Now().Sub(timeBegin))
-		time.Sleep(10 * time.Second)
+		time.Sleep(20 * time.Second)
 	}
 }
 func fetchAnyValidatorBalancesAfterCheckpoint(ctx context.Context, contextTimeout time.Duration) error {

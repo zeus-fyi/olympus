a8b91d901ab928b5ee1d7ac08a1bb9c93017eca7
alex <alexg89@vt.edu>
2022-10-09 00:23:31 -0700

    adds templates

---
+++ pkg/hera/lib/v0/core/file_shells/base/file_wrapper.go
@@ -0,0 +1,16 @@
+package base
+
+import (
+	"github.com/zeus-fyi/jennifer/jen"
+	"github.com/zeus-fyi/olympus/pkg/hera/lib/v0/core/primitives"
+)
+
+func FileBase(fw primitives.FileWrapper) *jen.File {
+	f := jen.NewFile(fw.PackageName)
+	f.Add(genHeader())
+	return f
+}
+
+func genHeader() jen.Code {
+	return jen.Null()
+}
---
+++ pkg/hera/lib/v0/core/file_shells/base/file_wrapper_test.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/olympus/pkg/hera/lib/v0/core/primitives"
+	"github.com/zeus-fyi/olympus/pkg/hera/lib/v0/core/template_test"
+)
+
+type FileBaseTestSuite struct {
+	template_test.TemplateTestSuite
+}
+
+func (s *FileBaseTestSuite) TestCodeGen() {
+	fw := primitives.FileWrapper{PackageName: "base", FileName: "base_file_example.go"}
+
+	f := FileBase(fw)
+	s.Assert().NotEmpty(f)
+	err := f.Save(fw.FileName)
+	s.Assert().Nil(err)
+
+	s.Cleanup = false
+	if s.Cleanup {
+		s.DeleteFile(fw.FileName)
+	}
+}
+
+func TestFileBaseTestSuite(t *testing.T) {
+	suite.Run(t, new(FileBaseTestSuite))
+}

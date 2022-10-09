0e01ef3aecc884c2e08d1b6058a22d61f8213ff4
alex <alexg89@vt.edu>
2022-10-08 23:29:44 -0700

    creates struct on demand

--- pkg/hera/lib/v0/core/struct/struct_test.go
+++ pkg/hera/lib/v0/core/struct/struct_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/jennifer/jen"
 	"github.com/zeus-fyi/olympus/pkg/hera/lib/v0/core/primitives"
 	"github.com/zeus-fyi/olympus/pkg/hera/lib/v0/core/template_test"
 )
@@ -31,14 +32,54 @@ func (s *StructTestSuite) TestCodeGen() {
 	}
 	structToMake.AddField(fieldTwo)
 
-	err := genFile(fw, structToMake).Save(fw.FileName)
-	s.Require().Nil(err)
+	resp := genMutateFile(fw, structToMake)
+	s.Assert().NotEmpty(resp)
 
+	err := resp.Save(fw.FileName)
+	s.Assert().Nil(err)
+
+	s.Cleanup = false
 	if s.Cleanup {
 		s.DeleteFile(fw.FileName)
 	}
 }
 
+func genMutateFile(fw primitives.FileWrapper, structGen primitives.StructGen) *jen.File {
+	ret := jen.NewFile(fw.PackageName)
+	ret.Add(genDeclAt17())
+	ret.Add(genDeclAt85())
+	ret.Add(genDeclAt117())
+	ret.Add(genDeclAt289())
+	ret.Add(genFuncGetRowValues())
+	ret.Add(AddStruct(structGen))
+	return ret
+}
+
+func AddStruct(structToWrite primitives.StructGen) jen.Code {
+	fields := make([]jen.Code, len(structToWrite.Fields))
+	for i, f := range structToWrite.Fields {
+		fields[i] = jen.Id(f.Name).Id(f.Type)
+	}
+	_struct := jen.Null().Type().Id(structToWrite.Name).Struct(fields...)
+	return _struct
+}
+
 func TestFuncTestSuite(t *testing.T) {
 	suite.Run(t, new(StructTestSuite))
+}
+
+func genDeclAt17() jen.Code {
+	return jen.Null()
+}
+func genDeclAt85() jen.Code {
+	return jen.Null().Var().Id("Sn").Op("=").Lit("StructNameExample")
+}
+func genDeclAt117() jen.Code {
+	return jen.Null().Type().Id("StructNameExample").Struct(jen.Id("Field").Id("string"), jen.Id("FieldN").Id("int"))
+}
+func genDeclAt289() jen.Code {
+	return jen.Null().Type().Id("StructNameExamples").Index().Id("StructNameExample")
+}
+func genFuncGetRowValues() jen.Code {
+	return jen.Func().Params(jen.Id("v").Op("*").Id("StructNameExample")).Id("GetRowValues").Params(jen.Id("queryName").Id("string")).Params(jen.Id("apps").Dot("RowValues")).Block(jen.Id("pgValues").Op(":=").Id("apps").Dot("RowValues").Values(), jen.Switch(jen.Id("queryName")).Block(jen.Case(jen.Lit("fieldGroup1")).Block(jen.Id("pgValues").Op("=").Id("apps").Dot("RowValues").Values(jen.Id("v").Dot("Field"))), jen.Default().Block(jen.Id("pgValues").Op("=").Id("apps").Dot("RowValues").Values(jen.Id("v").Dot("Field"), jen.Id("v").Dot("FieldN")))), jen.Return().Id("pgValues"))
 }

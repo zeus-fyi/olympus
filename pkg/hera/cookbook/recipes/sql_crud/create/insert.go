3cecfeae20c272cf7518a150ac0cc176af535838
alex <alexg89@vt.edu>
2022-10-17 19:15:35 -0700

    generates decomposed insert template scaffold

--- pkg/hera/cookbook/recipes/sql_crud/create/insert.go
+++ pkg/hera/cookbook/recipes/sql_crud/create/insert.go
@@ -29,6 +29,15 @@ func tmpGen(structName string) jen.Code {
 	return tmp
 }
 
+func genFuncStructNameExamplesFieldCase() *jen.Statement {
+	return jen.Block(genLogHeader(),
+		jen.List(genSqlExec()),
+		genSqlExecErrHandler(),
+		genSqlExecRowsAffectedHandler(),
+		genSqlExecRowsAffectedDebugLog(),
+		genSqlInsertReturnWrappedErr())
+}
+
 func tmpGenParams() *jen.Statement {
 	return jen.Params(jen.Id("ctx").Qual("context", "Context"), jen.Id("q").Id("sql_query_templates").Dot("QueryParams")).Params(jen.Id("error"))
 }
@@ -38,18 +47,25 @@ func genLogHeader() *jen.Statement {
 		jen.Id("q").Dot("LogHeader").Call(jen.Id("models").Dot("Sn")))
 }
 
-func genFuncStructNameExamplesFieldCase() *jen.Statement {
-	return jen.Block(genLogHeader(),
-		jen.List(jen.Id("r"), jen.Id("err")).Op(":=").Id("apps").
-			Dot("Pg").Dot("Exec").Call(jen.Id("ctx"), jen.Id("q").Dot("SelectQuery").Call()),
-		jen.If(jen.Id("returnErr").Op(":=").Id("misc").Dot("ReturnIfErr").Call(jen.Id("err"), jen.Id("q").
-			Dot("LogHeader").Call(jen.Id("models").
-			Dot("Sn"))), jen.Id("returnErr").Op("!=").Id("nil")).Block(jen.Return().Id("err")),
-		jen.Id("rowsAffected").Op(":=").Id("r").Dot("RowsAffected").Call(),
-		jen.Id("log").Dot("Debug").Call().Dot("Msgf").Call(jen.Lit("StructNameExamples: %s, Rows Affected: %d"),
-			jen.Id("q").Dot("LogHeader").Call(jen.Id("models").Dot("Sn")), jen.Id("rowsAffected")),
-		jen.Return().Id("misc").Dot("ReturnIfErr").Call(jen.Id("err"), jen.Id("q").Dot("LogHeader").Call(jen.Id("models").Dot("Sn"))))
+func genSqlExec() *jen.Statement {
+	return jen.Id("err").Op(":=").Id("apps").Dot("Pg").Dot("Exec").Call(jen.Id("ctx"), jen.Id("q").Dot("SelectQuery").Call())
 }
-func genDeclAt26(structName string) jen.Code {
-	return jen.Null().Type().Id(structName).Struct(jen.Id("ChartComponentKindID").Id("int"), jen.Id("ChartComponentKindName").Id("string"), jen.Id("ChartComponentApiVersion").Id("string"))
+
+func genSqlExecErrHandler() *jen.Statement {
+	return jen.If(jen.Id("returnErr").Op(":=").Id("misc").Dot("ReturnIfErr").Call(jen.Id("err"), jen.Id("q").
+		Dot("LogHeader").Call(jen.Id("models").
+		Dot("Sn"))), jen.Id("returnErr").Op("!=").Id("nil").Block(jen.Return().Id("err")))
+}
+
+func genSqlExecRowsAffectedHandler() *jen.Statement {
+	return jen.Id("rowsAffected").Op(":=").Id("r").Dot("RowsAffected").Call()
+}
+
+func genSqlExecRowsAffectedDebugLog() *jen.Statement {
+	return jen.Id("log").Dot("Debug").Call().Dot("Msgf").Call(jen.Lit("StructNameExamples: %s, Rows Affected: %d"),
+		jen.Id("q").Dot("LogHeader").Call(jen.Id("models").Dot("Sn")), jen.Id("rowsAffected"))
+}
+
+func genSqlInsertReturnWrappedErr() *jen.Statement {
+	return jen.Return().Id("misc").Dot("ReturnIfErr").Call(jen.Id("err"), jen.Id("q").Dot("LogHeader").Call(jen.Id("models").Dot("Sn")))
 }

4db21ebddcd99c039f86585b021506f5776d91d4
alex <alexg89@vt.edu>
2022-10-22 12:42:23 -0700

    use zeus table-to-go

--- pkg/hera/lib/v0/datastores/postgres/pg_table_columns.go
+++ pkg/hera/lib/v0/datastores/postgres/pg_table_columns.go
@@ -1,20 +1,5 @@
 package postgres
 
-import (
-	"strconv"
-
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/zeus-fyi/tables-to-go/pkg/settings"
-)
-
-func (d *PgSchemaAutogen) PgxConfigToSqlX(dsnStringPgx string) (*settings.Settings, error) {
-	c, err := pgxpool.ParseConfig(dsnStringPgx)
-	conf := c.ConnConfig
-	pgSettings := settings.New()
-	pgSettings.User = conf.User
-	pgSettings.Pswd = conf.Password
-	pgSettings.Host = conf.Host
-	pgSettings.Port = strconv.Itoa(int(conf.Port))
-	pgSettings.DbName = conf.Database
-	return pgSettings, err
+func (d *PgSchemaAutogen) Columns() {
+	return
 }

88bd4ffc071bf50f8271f70d015955fdfaa1d407
alex <alexg89@vt.edu>
2022-10-23 23:39:26 -0700

    refactors to make things easier

--- datastores/postgres/apps/zeus/models/create/deployments/deployments_spec_pod_template.go
+++ datastores/postgres/apps/zeus/models/create/deployments/deployments_spec_pod_template.go
@@ -4,19 +4,16 @@ import (
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/common"
 )
 
+// TODO refactor to get rid of parentSqlExpression to use other style
 func (d *Deployment) insertPodSpecTemplate(parentSqlExpression, parentClassTypeCteName string) string {
 	pts := d.Spec.Template.Spec
 
+	// this part links containers to this k8s workload resource
+	parentSqlExpression = d.Spec.DeploymentSpec.Template.InsertPodContainerGroupSQL() + parentSqlExpression
 	// TODO, maybe not even rely on this, and have the containers already exist...
 	// add containers now, this func will also get the other data from the containers loop iterator
 	parentSqlExpression = common.InsertContainerValues(parentSqlExpression, pts.PodTemplateContainers)
 
 	// optionally add template metadata here if desired later
-
-	// this part links containers to this k8s workload resource
-	// TODO disabling for now, need to get cont ids
-
-	//dbParsedPodTemplateSpec := containers.NewPodContainersGroupForDB(d.Spec.Template)
-	//dbParsedPodTemplateSpec.InsertPodContainerGroupSQL(d.Spec.Template.Spec.PodTemplateSpecClassDefinition.ChartSubcomponentChildClassTypeID)
 	return parentSqlExpression
 }

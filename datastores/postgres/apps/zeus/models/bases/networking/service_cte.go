713e9c422eef9f912e257ce66638e468000689ac
alex <alexg89@vt.edu>
2022-10-27 19:20:30 -0700

    chart refactor to not use special insert  type

---
+++ datastores/postgres/apps/zeus/models/bases/networking/service_cte.go
@@ -0,0 +1,25 @@
+package networking
+
+import (
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/charts"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/create/common"
+	"github.com/zeus-fyi/olympus/pkg/utils/string_utils/sql_query_templates"
+)
+
+func (s *Service) GetServiceCTE(chart *charts.Chart) sql_query_templates.CTE {
+
+	if chart != nil {
+		s.SetChartPackageID(chart.GetChartPackageID())
+	}
+	var combinedSubCTEs sql_query_templates.SubCTEs
+	// metadata
+	metaDataCtes := common.CreateParentMetadataSubCTEs(chart, s.Metadata)
+	// spec
+	specCtes := s.CreateServiceSpecSubCTE(chart)
+	combinedSubCTEs = sql_query_templates.AppendSubCteSlices(metaDataCtes, specCtes)
+	cteExpr := sql_query_templates.CTE{
+		Name:    "InsertServiceCTEs",
+		SubCTEs: combinedSubCTEs,
+	}
+	return cteExpr
+}
--- datastores/postgres/apps/zeus/models/create/networking/services.go
+++ datastores/postgres/apps/zeus/models/create/networking/services.go
@@ -7,7 +7,6 @@ import (
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/charts"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/networking"
-	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/create/common"
 	"github.com/zeus-fyi/olympus/pkg/utils/misc"
 	"github.com/zeus-fyi/olympus/pkg/utils/string_utils/sql_query_templates"
 )
@@ -20,7 +19,7 @@ const ModelName = "Service"
 
 func (s *Service) InsertService(ctx context.Context, q sql_query_templates.QueryParams, c *charts.Chart) error {
 	log.Debug().Interface("InsertQuery:", q.LogHeader(ModelName))
-	q.CTEQuery = s.InsertServiceCte(c)
+	q.CTEQuery = s.GetServiceCTE(c)
 	q.RawQuery = q.CTEQuery.GenerateChainedCTE()
 	r, err := apps.Pg.Exec(ctx, q.RawQuery)
 	if returnErr := misc.ReturnIfErr(err, q.LogHeader(ModelName)); returnErr != nil {
@@ -29,22 +28,4 @@ func (s *Service) InsertService(ctx context.Context, q sql_query_templates.Query
 	rowsAffected := r.RowsAffected()
 	log.Debug().Msgf("InsertService: %s, Rows Affected: %d", q.LogHeader(ModelName), rowsAffected)
 	return misc.ReturnIfErr(err, q.LogHeader(ModelName))
-}
-
-func (s *Service) InsertServiceCte(chart *charts.Chart) sql_query_templates.CTE {
-
-	if chart != nil {
-		s.SetChartPackageID(chart.GetChartPackageID())
-	}
-	var combinedSubCTEs sql_query_templates.SubCTEs
-	// metadata
-	metaDataCtes := common.CreateParentMetadataSubCTEs(chart, s.Metadata)
-	// spec
-	specCtes := s.CreateServiceSpecSubCTE(chart)
-	combinedSubCTEs = sql_query_templates.AppendSubCteSlices(metaDataCtes, specCtes)
-	cteExpr := sql_query_templates.CTE{
-		Name:    "InsertServiceCTEs",
-		SubCTEs: combinedSubCTEs,
-	}
-	return cteExpr
 }

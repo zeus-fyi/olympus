ebf859eb2462a7abd2d6ba871fb2c90d452055c8
alex <alexg89@vt.edu>
2022-10-29 14:12:01 -0700

    inserts key types

---
+++ datastores/postgres/apps/hestia/models/bases/keys/keys.go
@@ -0,0 +1,8 @@
+package keys
+
+import autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/autogen"
+
+type Key struct {
+	autogen_bases.UsersKeys
+	autogen_bases.KeyTypes
+}
--- datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/102_hestia_keys.sql
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/102_hestia_keys.sql
@@ -5,21 +5,21 @@ CREATE TABLE "public"."key_types" (
 ALTER TABLE "public"."key_types" ADD CONSTRAINT "key_type_pk" PRIMARY KEY ("key_type_id");
 
 CREATE TABLE "public"."users_keys" (
-    "public_key" text NOT NULL,
     "user_id" int8 NOT NULL REFERENCES users(user_id),
-    "public_key_name" text NOT NULL NOT NULL DEFAULT '',
-    "public_key_verified" bool NOT NULL DEFAULT false,
     "public_key_type_id" int8 NOT NULL REFERENCES key_types(key_type_id),
-    "created_at" timestamptz  NOT NULL DEFAULT NOW()
+    "created_at" timestamptz  NOT NULL DEFAULT NOW(),
+    "public_key_name" text NOT NULL NOT NULL DEFAULT '',
+    "public_key" text NOT NULL,
+    "public_key_verified" bool NOT NULL DEFAULT false
 );
 ALTER TABLE "public"."users_keys" ADD CONSTRAINT "users_keys_pk" PRIMARY KEY ("public_key");
 
 CREATE TABLE "public"."users_key_groups" (
     "user_id" int8 NOT NULL REFERENCES users(user_id),
-    "public_key" text NOT NULL REFERENCES users_keys(public_key),
     "key_group_id" int8 NOT NULL DEFAULT next_id(),
-    "key_group_name" text NOT NULL DEFAULT '',
-    "updated_at" timestamptz  NOT NULL DEFAULT NOW()
+    "updated_at" timestamptz  NOT NULL DEFAULT NOW(),
+    "public_key" text NOT NULL REFERENCES users_keys(public_key),
+    "key_group_name" text NOT NULL DEFAULT ''
 );
 ALTER TABLE "public"."users_key_groups" ADD CONSTRAINT "user_key_pk" PRIMARY KEY ("key_group_id");
 

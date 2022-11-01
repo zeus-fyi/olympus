869b1ee0e8e6489bf898ebb2a22f3e5587d93073
alex <alexg89@vt.edu>
2022-11-01 15:41:58 -0700

    updates update/delete to skeleton

--- apps/olympus/zeus/api/v1/zeus/topology/infra/delete/delete_topology.go
+++ apps/olympus/zeus/api/v1/zeus/topology/infra/delete/delete_topology.go
@@ -2,12 +2,9 @@ package delete_infra
 
 import (
 	"github.com/labstack/echo/v4"
-
-	base_infra "github.com/zeus-fyi/olympus/zeus/api/v1/zeus/topology/infra/base"
 )
 
 type TopologyActionDeleteRequest struct {
-	base_infra.TopologyInfraActionRequest
 }
 
 func (t *TopologyActionDeleteRequest) DeleteTopology(c echo.Context) error {
--- apps/olympus/zeus/api/v1/zeus/topology/infra/update/topologies_update.go
+++ apps/olympus/zeus/api/v1/zeus/topology/infra/update/topologies_update.go
@@ -2,11 +2,9 @@ package update_infra
 
 import (
 	"github.com/labstack/echo/v4"
-	base_infra "github.com/zeus-fyi/olympus/zeus/api/v1/zeus/topology/infra/base"
 )
 
 type TopologyActionUpdateRequest struct {
-	base_infra.TopologyInfraActionRequest
 }
 
 func (t *TopologyActionUpdateRequest) UpdateTopology(c echo.Context) error {

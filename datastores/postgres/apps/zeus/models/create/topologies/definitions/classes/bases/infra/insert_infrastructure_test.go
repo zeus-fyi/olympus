baf8ff7e022f0783cace99f01f06d6b9ec43496c
alex <alexg89@vt.edu>
2022-10-31 18:21:57 -0700

    insert infra

--- datastores/postgres/apps/zeus/models/create/topologies/definitions/classes/bases/infra/insert_infrastructure_test.go
+++ datastores/postgres/apps/zeus/models/create/topologies/definitions/classes/bases/infra/insert_infrastructure_test.go
@@ -1,70 +1,81 @@
 package create_infra
 
 import (
+	"database/sql"
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	hestia_test "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/test"
+	autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/autogen"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/charts"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/configuration"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/deployments"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/networking/ingresses"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/networking/services"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/create/packages"
 	conversions_test "github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/test"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
 
 type CreateInfraTestSuite struct {
-	//b hestia_test.BaseHestiaTestSuite
+	b hestia_test.BaseHestiaTestSuite
 	conversions_test.ConversionsTestSuite
 }
 
 func (s *CreateInfraTestSuite) TestInsertInfraBase() {
-	//nd := deployments.NewDeployment()
-	//nsvc := services.NewService()
-	//ing := ingresses.NewIngress()
-	//cm := configuration.NewConfigMap()
-	//pkg := packages.Packages{
-	//	Chart: charts.Chart{
-	//		ChartPackages: autogen_bases.ChartPackages{
-	//			ChartPackageID:   0,
-	//			ChartName:        rand.String(10),
-	//			ChartVersion:     rand.String(10),
-	//			ChartDescription: sql.NullString{},
-	//		},
-	//	},
-	//	Deployment: &nd,
-	//	Service:    &nsvc,
-	//	Ingress:    &ing,
-	//	ConfigMap:  &cm,
-	//}
-	//
-	//filepath := s.TestDirectory + "/apps/eth-indexer/deployment.yaml"
-	//jsonBytes, err := s.Yr.ReadYamlConfig(filepath)
-	//s.Require().Nil(err)
-	//err = json.Unmarshal(jsonBytes, &pkg.K8sDeployment)
-	//s.Require().Nil(err)
-	//err = pkg.ConvertDeploymentConfigToDB()
-	//s.Require().Nil(err)
-	//
-	//filepath = s.TestDirectory + "/apps/eth-indexer/service.yaml"
-	//jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
-	//err = json.Unmarshal(jsonBytes, &pkg.K8sService)
-	//s.Require().Nil(err)
-	//pkg.ConvertK8sServiceToDB()
-	//s.Assert().NotEmpty(pkg.Service)
-	//
-	//filepath = s.TestDirectory + "/apps/eth-indexer/ingress.yaml"
-	//jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
-	//err = json.Unmarshal(jsonBytes, &pkg.K8sIngress)
-	//s.Require().Nil(err)
-	//err = pkg.ConvertK8sIngressToDB()
-	//s.Require().Nil(err)
-	//s.Assert().NotEmpty(pkg.Ingress)
-	//
-	//filepath = s.TestDirectory + "/apps/eth-indexer/cm-eth-indexer.yaml"
-	//jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
-	//err = json.Unmarshal(jsonBytes, &cm.K8sConfigMap)
-	//s.Require().Nil(err)
-	//cm.ParseK8sConfigToDB()
-	//s.Assert().NotEmpty(cm.Data)
-	//s.Assert().NotEmpty(cm.Metadata.Name)
-	//
-	//inf := NewCreateInfrastructure()
-	//inf.Packages = pkg
+	nd := deployments.NewDeployment()
+	nsvc := services.NewService()
+	ing := ingresses.NewIngress()
+	cm := configuration.NewConfigMap()
+	pkg := packages.Packages{
+		Chart: charts.Chart{
+			ChartPackages: autogen_bases.ChartPackages{
+				ChartPackageID:   0,
+				ChartName:        rand.String(10),
+				ChartVersion:     rand.String(10),
+				ChartDescription: sql.NullString{},
+			},
+		},
+		Deployment: &nd,
+		Service:    &nsvc,
+		Ingress:    &ing,
+		ConfigMap:  &cm,
+	}
+
+	filepath := s.TestDirectory + "/apps/eth-indexer/deployment.yaml"
+	jsonBytes, err := s.Yr.ReadYamlConfig(filepath)
+	s.Require().Nil(err)
+	err = json.Unmarshal(jsonBytes, &pkg.K8sDeployment)
+	s.Require().Nil(err)
+	err = pkg.ConvertDeploymentConfigToDB()
+	s.Require().Nil(err)
+
+	filepath = s.TestDirectory + "/apps/eth-indexer/service.yaml"
+	jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
+	err = json.Unmarshal(jsonBytes, &pkg.K8sService)
+	s.Require().Nil(err)
+	pkg.ConvertK8sServiceToDB()
+	s.Assert().NotEmpty(pkg.Service)
+
+	filepath = s.TestDirectory + "/apps/eth-indexer/ingress.yaml"
+	jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
+	err = json.Unmarshal(jsonBytes, &pkg.K8sIngress)
+	s.Require().Nil(err)
+	err = pkg.ConvertK8sIngressToDB()
+	s.Require().Nil(err)
+	s.Assert().NotEmpty(pkg.Ingress)
+
+	filepath = s.TestDirectory + "/apps/eth-indexer/cm-eth-indexer.yaml"
+	jsonBytes, err = s.Yr.ReadYamlConfig(filepath)
+	err = json.Unmarshal(jsonBytes, &cm.K8sConfigMap)
+	s.Require().Nil(err)
+	cm.ParseK8sConfigToDB()
+	s.Assert().NotEmpty(cm.Data)
+	s.Assert().NotEmpty(cm.Metadata.Name)
+
+	inf := NewCreateInfrastructure()
+	inf.Packages = pkg
 	//ctx := context.Background()
 	//err = inf.InsertInfraBase(ctx)
 	//s.Require().Nil(err)

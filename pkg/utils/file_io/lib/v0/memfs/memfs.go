6212cd5f6e217b77471236e863d7afb25f2290c2
alex <alexg89@vt.edu>
2022-11-02 11:52:16 -0700

    add memfs base and repo to go 19

--- pkg/utils/file_io/lib/v0/memfs/memfs.go
+++ pkg/utils/file_io/lib/v0/memfs/memfs.go
@@ -1,3 +1,28 @@
 package memfs
 
-// todo
+import (
+	"io/fs"
+
+	"github.com/zeus-fyi/memoryfs"
+)
+
+type Memfs struct {
+	*memoryfs.FS
+}
+
+func tmp() {
+	memfs := memoryfs.New()
+
+	if err := memfs.MkdirAll("my/dir", 0o700); err != nil {
+		panic(err)
+	}
+
+	if err := memfs.WriteFile("my/dir/file.txt", []byte("hello world"), 0o600); err != nil {
+		panic(err)
+	}
+
+	_, err := fs.ReadFile(memfs, "my/dir/file.txt")
+	if err != nil {
+		panic(err)
+	}
+}

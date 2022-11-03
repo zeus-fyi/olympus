a322115ed192156026f21b5efb9e9300c72db8aa
alex <alexg89@vt.edu>
2022-11-02 17:38:14 -0700

    adds zeus updates

--- apps/olympus/zeus/server/zeus.go
+++ apps/olympus/zeus/server/zeus.go
@@ -12,19 +12,24 @@ import (
 
 var cfg = Config{}
 var authKeysCfg auth_startup.AuthKeysCfg
+var env string
 
 func Zeus() {
 	srv := NewZeusServer(cfg)
 	// Echo instance
 	ctx := context.Background()
 
-	authCfg := auth_startup.NewDefaultAuthClient(ctx, authKeysCfg)
-	inMemFs := auth_startup.RunDigitalOceanS3BucketObjAuthProcedure(ctx, authCfg)
-	cfg.K8sUtil.ConnectToK8sFromInMemFsCfgPath(inMemFs)
+	switch env {
+	case "production":
+		authCfg := auth_startup.NewDefaultAuthClient(ctx, authKeysCfg)
+		inMemFs := auth_startup.RunDigitalOceanS3BucketObjAuthProcedure(ctx, authCfg)
+		cfg.K8sUtil.ConnectToK8sFromInMemFsCfgPath(inMemFs)
+	case "local":
+		apps.Pg = apps.Db{}
+		apps.Pg.InitPG(ctx, cfg.PGConnStr)
+		cfg.K8sUtil.ConnectToK8s()
+	}
 	srv.E = router.InitRouter(srv.E, cfg.K8sUtil)
-
-	apps.Pg = apps.Db{}
-	apps.Pg.InitPG(ctx, cfg.PGConnStr)
 
 	// Start server
 	srv.Start()
@@ -33,13 +38,15 @@ func Zeus() {
 func init() {
 	viper.AutomaticEnv()
 	Cmd.Flags().StringVar(&cfg.Port, "port", "9001", "server port")
-	Cmd.Flags().StringVar(&cfg.PGConnStr, "postgres-conn-str", "", "postgres connection string")
+	Cmd.Flags().StringVar(&cfg.PGConnStr, "postgres-conn-str", "postgresql://localhost/postgres?user=postgres&password=postgres", "postgres connection string")
 
 	Cmd.Flags().StringVar(&authKeysCfg.AgePubKey, "age-public-key", "age1n97pswc3uqlgt2un9aqn9v4nqu32egmvjulwqp3pv4algyvvuggqaruxjj", "age public key")
 	Cmd.Flags().StringVar(&authKeysCfg.AgePrivKey, "age-private-key", "", "age private key")
 
 	Cmd.Flags().StringVar(&authKeysCfg.SpacesKey, "do-spaces-key", "", "do s3 spaces key")
 	Cmd.Flags().StringVar(&authKeysCfg.SpacesPrivKey, "do-spaces-private-key", "", "do s3 spaces private key")
+
+	Cmd.Flags().StringVar(&env, "env", "local", "environment")
 }
 
 // Cmd represents the base command when called without any subcommands

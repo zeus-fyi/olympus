5033798be502cd2df2213cb6acff0414f7606dac
alex <alexg89@vt.edu>
2022-11-07 22:17:32 -0800

    update testand makefile

--- test/driver.go
+++ test/driver.go
@@ -4,8 +4,6 @@ import (
 	autok8s_core "github.com/zeus-fyi/olympus/pkg/zeus/core"
 )
 
-var host = "http://localhost:9001/v1/infra/create"
-
 var kns = autok8s_core.KubeCtxNs{
 	CloudProvider: "do",
 	Region:        "sfo3",

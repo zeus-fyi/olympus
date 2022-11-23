29be9c286143bf0d1fee7946c4a9ff65ddcd4757
alex <alexg89@vt.edu>
2022-11-22 19:49:32 -0800

    adds hypnos

---
+++ apps/olympus/hypnos/server/hypnos.go
@@ -0,0 +1,32 @@
+package hypnos_server
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	v1_hypnos "github.com/zeus-fyi/olympus/hypnos/api/v1"
+)
+
+var cfg = Config{}
+
+func Hypnos() {
+	cfg.Host = "0.0.0.0"
+	srv := NewHypnosServer(cfg)
+	// Echo instance
+	srv.E = v1_hypnos.Routes(srv.E)
+	// Start server
+	srv.Start()
+}
+
+func init() {
+	viper.AutomaticEnv()
+	Cmd.Flags().StringVar(&cfg.Port, "port", "9002", "server port")
+}
+
+// Cmd represents the base command when called without any subcommands
+var Cmd = &cobra.Command{
+	Use:   "Pausing a container, pod, network, or otherwise",
+	Short: "The god of hypnosis",
+	Run: func(cmd *cobra.Command, args []string) {
+		Hypnos()
+	},
+}

83f5262fe7c12a296ae62a80dfc7d9e1b85eda9c
alex <alexg89@vt.edu>
2022-11-23 19:05:45 -0800

    remove hypnos embed

--- pkg/athena/client/endpoints/endpoints.go
+++ pkg/athena/client/endpoints/endpoints.go
@@ -1,7 +1,5 @@
 package athena_endpoints
 
-const InternalHypnosV1Path = "/v1/internal/routines/hypnos"
-
 const InternalSuspendV1Path = "/v1/internal/routines/suspend"
 
 const InternalResumeV1Path = "/v1/internal/routines/resume"

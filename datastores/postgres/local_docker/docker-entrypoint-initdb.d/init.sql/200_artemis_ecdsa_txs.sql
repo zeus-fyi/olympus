32f43eb338115a31c7dd3a5a17cb21cd37509918
alex <alexg89@vt.edu>
2022-11-30 15:07:01 -0800

    adds network id, and artemis schema

---
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/200_artemis_ecdsa_txs.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "public"."eth_ecdsa_txs" (
+    "tx_id" int8 NOT NULl DEFAULT next_id(),
+    "protocol_network_id" int8 NOT NULL REFERENCES protocol_networks(protocol_network_id) DEFAULT 1,
+    "nonce" int8 NOT NULL DEFAULT 0,
+    "gas_price_gwei" int8,
+    "gas_price_gwei_decimals" int8 DEFAULT 0,
+    "gas_limit_gwei" int8 DEFAULT 21000,
+    "gas_limit_gwei_decimals" int8 DEFAULT 0,
+    "amount_gwei" int8 DEFAULT 0,
+    "amount_gwei_decimals" int8 DEFAULT 0,
+    "public_key_type_id" int8 NOT NULL REFERENCES key_types(key_type_id) CHECK (public_key_type_id IN (5)),
+    "public_key" text NOT NULL REFERENCES users_keys(public_key),
+    "r" text NOT NULL,
+    "s" text NOT NULL,
+    "v" text NOT NULL,
+    "to" text,
+    "tx_hash" text,
+    "payload" bytea
+);
+ALTER TABLE "public"."eth_ecdsa_txs" ADD CONSTRAINT "eth_tx_id_pk" PRIMARY KEY ("tx_id");
--- datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/2_validators.sql
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/2_validators.sql
@@ -1,27 +1,9 @@
-/*
- Navicat PostgreSQL Data Transfer
-
- Source Server         : zeus-do
- Source Server Type    : PostgreSQL
- Source Server Version : 140004
- Source Host           : private-db-postgresql-sfo3-zeus-do-user-9587342-0.b.db.ondigitalocean.com:25060
- Source Catalog        : zeus
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 140004
- File Encoding         : 65001
-
- Date: 07/07/2022 20:27:04
-*/
-
-CREATE TYPE network AS ENUM ('mainnet', 'prater');
-CREATE ROLE "doadmin" SUPERUSER;
 -- ----------------------------
 -- Table structure for validators
 -- ----------------------------
 CREATE TABLE "public"."validators" (
    "index" int4 NOT NULL,
+   "protocol_network_id" int8 NOT NULL REFERENCES protocol_networks(protocol_network_id) DEFAULT 1,
    "balance" int8,
    "effective_balance" int8,
    "activation_eligibility_epoch" int8 CHECK (activation_eligibility_epoch <= activation_epoch) NOT NULL DEFAULT (9223372036854775807),
@@ -33,7 +15,6 @@ CREATE TABLE "public"."validators" (
    "pubkey" text NOT NULL CHECK(LENGTH(pubkey)=98),
    "status" text CHECK (status IN ('pending', 'active', 'exited', 'withdrawal', 'unknown')) NOT NULL DEFAULT 'unknown',
    "substatus" text CHECK (substatus IN ('pending_initialized', 'pending_queued', 'active_ongoing', 'active_exiting', 'active_slashed','exited_unslashed', 'exited_slashed', 'withdrawal_possible', 'withdrawal_done', 'unknown')) DEFAULT 'unknown',
-   "network" network NOT NULL DEFAULT 'mainnet',
    "withdrawal_credentials" text
 )
 ;

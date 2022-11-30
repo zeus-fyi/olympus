a59bb5581c30691356ba389e6c60db8485ffcdf7
alex <alexg89@vt.edu>
2022-11-30 15:42:23 -0800

    adds artemis ecdsa tx/rx

--- datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/200_artemis_ecdsa_txs.sql
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/200_artemis_ecdsa_txs.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "public"."eth_ecdsa_txs" (
-    "tx_id" int8 NOT NULl DEFAULT next_id(),
+    "tx_id" int8 NOT NULL DEFAULT next_id(),
     "protocol_network_id" int8 NOT NULL REFERENCES protocol_networks(protocol_network_id) DEFAULT 1,
     "nonce" int8 NOT NULL DEFAULT 0,
     "gas_price_gwei" int8,
@@ -18,3 +18,7 @@ CREATE TABLE "public"."eth_ecdsa_txs" (
     "payload" bytea
 );
 ALTER TABLE "public"."eth_ecdsa_txs" ADD CONSTRAINT "eth_tx_id_pk" PRIMARY KEY ("tx_id");
+ALTER TABLE "public"."eth_ecdsa_txs" ADD CONSTRAINT "key_network_nonce_unique" UNIQUE ("public_key", "protocol_network_id", "nonce");
+CREATE INDEX eth_ecdsa_nonce_tx_hash_nulls ON "public"."eth_ecdsa_txs" ("nonce", "tx_hash" DESC NULLS LAST);
+
+

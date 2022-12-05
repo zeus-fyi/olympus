f663ab4d41e03918c83f3a9cc9d4a0199dded338
alex <alexg89@vt.edu>
2022-12-04 17:26:51 -0800

    adds skeleton base component & refactors a bit

--- datastores/postgres/apps/zeus/models/bases/topologies/definitions/bases/skeletons/skeletons.go
+++ datastores/postgres/apps/zeus/models/bases/topologies/definitions/bases/skeletons/skeletons.go
@@ -1,11 +1,23 @@
 package skeletons
 
-import autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/autogen"
+import (
+	autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/autogen"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/topologies/definitions/class_types"
+)
 
 type Skeleton struct {
-	autogen_bases.TopologySkeletonBaseComponents
 }
 
 type SkeletonBase struct {
-	autogen_bases.TopologyInfrastructureComponents
+	autogen_bases.TopologySkeletonBaseComponents
+}
+
+func NewSkeletonBase(orgID, baseComponentID int, name string) SkeletonBase {
+	s := SkeletonBase{autogen_bases.TopologySkeletonBaseComponents{
+		OrgID:                    orgID,
+		TopologyBaseComponentID:  baseComponentID,
+		TopologyClassTypeID:      class_types.SkeletonBaseClassTypeID,
+		TopologySkeletonBaseName: name,
+	}}
+	return s
 }
--- datastores/postgres/apps/zeus/models/create/topologies/definitions/bases/skeleton/skeletons_test.go
+++ datastores/postgres/apps/zeus/models/create/topologies/definitions/bases/skeleton/skeletons_test.go
@@ -6,12 +6,23 @@ import (
 
 	"github.com/stretchr/testify/suite"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/topologies/definitions/bases/skeletons"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
 
 type SkeletonsTestSuite struct {
 	test_suites.DatastoresTestSuite
 }
+
+const (
+	GenericBaseComponentsID = 1670202733617147904
+
+	ConsensusClientsBaseComponentsID = 1670202869405165056
+	ExecClientsBaseComponentsID      = 1670202869402443776
+)
+
+var ctx = context.Background()
 
 func (s *SkeletonsTestSuite) SetupTest() {
 	s.InitLocalConfigs()
@@ -19,7 +30,11 @@ func (s *SkeletonsTestSuite) SetupTest() {
 }
 
 func (s *SkeletonsTestSuite) TestInsertSkeletonDefinition() {
+	sb := skeletons.NewSkeletonBase(s.Tc.ProductionLocalTemporalOrgID, GenericBaseComponentsID, "skeleton-base-"+rand.String(5))
 
+	err := InsertSkeletonBase(ctx, &sb)
+	s.Require().Nil(err)
+	s.Require().NotZero(sb.TopologySkeletonBaseVersionID)
 }
 
 func TestSkeletonsTestSuite(t *testing.T) {

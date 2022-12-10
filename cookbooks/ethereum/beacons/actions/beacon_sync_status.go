e993fbfc391a4797446fece145f336270ec8ccb9
alex <alexg89@vt.edu>
2022-12-10 13:59:09 -0800

    adds sync status ports

--- cookbooks/ethereum/beacons/actions/beacon_sync_status.go
+++ cookbooks/ethereum/beacons/actions/beacon_sync_status.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	beacon_cookbooks "github.com/zeus-fyi/olympus/cookbooks/ethereum/beacons"
+	client_consts "github.com/zeus-fyi/olympus/cookbooks/ethereum/beacons/constants"
 	"github.com/zeus-fyi/olympus/pkg/utils/string_utils"
 	zeus_pods_reqs "github.com/zeus-fyi/olympus/pkg/zeus/client/zeus_req_types/pods"
 	zeus_pods_resp "github.com/zeus-fyi/olympus/pkg/zeus/client/zeus_resp_types/pods"
@@ -13,7 +14,7 @@ func (b *BeaconActionsClient) GetConsensusClientSyncStatus(ctx context.Context) 
 	cliReq := zeus_pods_reqs.ClientRequest{
 		MethodHTTP: "GET",
 		Endpoint:   "eth/v1/node/syncing",
-		Ports:      []string{"5052:5052"},
+		Ports:      client_consts.GetClientBeaconPortsHTTP(b.ConsensusClient),
 	}
 	filter := string_utils.FilterOpts{Contains: b.ConsensusClient}
 	routeHeader := beacon_cookbooks.DeployConsensusClientKnsReq

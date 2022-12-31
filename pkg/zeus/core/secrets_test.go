dfb76281ed3c8607dd6409268d99e3ff56bbd22c
alex <alexg89@vt.edu>
2022-12-31 14:17:30 -0800

    adds web3signer secret create & custom image

--- pkg/zeus/core/secrets_test.go
+++ pkg/zeus/core/secrets_test.go
@@ -103,6 +103,37 @@ func (s *SecretsTestSuite) TestCopySecretToAnotherNs() {
 	s.Require().NotEmpty(newSecret)
 }
 
+func (s *SecretsTestSuite) TestCreateCWeb3SignerSecret() {
+	ctx := context.Background()
+	var kns = zeus_common_types.CloudCtxNs{CloudProvider: "do", Region: "sfo3", Context: "do-sfo3-dev-do-sfo3-zeus", Namespace: "ephemeral-staking"}
+	m := make(map[string]string)
+
+	m["postgres-db"] = s.Tc.DevWeb3SignerPgconn
+	m["postgres-username"] = "doadmin"
+	m["postgres-auth"] = s.Tc.DevWeb3SignerPgconnAuth
+	m["network"] = "minimal"
+
+	sec := v1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "web3signer",
+			Namespace: kns.Namespace,
+		},
+		StringData: m,
+		Type:       "Opaque",
+	}
+
+	_, err := s.K.CreateNamespaceIfDoesNotExist(ctx, kns)
+	s.Require().Nil(err)
+
+	newSecret, err := s.K.CreateSecretWithKns(ctx, kns, &sec, nil)
+	s.Require().Nil(err)
+	s.Require().NotEmpty(newSecret)
+}
+
 func TestSecretsTestSuite(t *testing.T) {
 	suite.Run(t, new(SecretsTestSuite))
 }

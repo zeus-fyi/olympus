7d439d40021ca61255e84f0198a3d08af84358a7
alex <alexg89@vt.edu>
2023-01-14 19:31:09 -0800

    adds hestia setup

--- apps/olympus/hestia/server/hestia.go
+++ apps/olympus/hestia/server/hestia.go
@@ -9,11 +9,13 @@ import (
 	"github.com/zeus-fyi/olympus/configs"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	v1hestia "github.com/zeus-fyi/olympus/hestia/api/v1"
+	"github.com/zeus-fyi/olympus/pkg/aegis/auth_startup/auth_keys_config"
 )
 
 var (
-	cfg = Config{}
-	env string
+	cfg         = Config{}
+	authKeysCfg auth_keys_config.AuthKeysCfg
+	env         string
 )
 
 func Hestia() {
@@ -40,6 +42,11 @@ func Hestia() {
 func init() {
 	viper.AutomaticEnv()
 	Cmd.Flags().StringVar(&cfg.Port, "port", "9002", "server port")
+	Cmd.Flags().StringVar(&env, "env", "production-local", "environment")
+	Cmd.Flags().StringVar(&authKeysCfg.AgePubKey, "age-public-key", "age1n97pswc3uqlgt2un9aqn9v4nqu32egmvjulwqp3pv4algyvvuggqaruxjj", "age public key")
+	Cmd.Flags().StringVar(&authKeysCfg.AgePrivKey, "age-private-key", "", "age private key")
+	Cmd.Flags().StringVar(&authKeysCfg.SpacesKey, "do-spaces-key", "", "do s3 spaces key")
+	Cmd.Flags().StringVar(&authKeysCfg.SpacesPrivKey, "do-spaces-private-key", "", "do s3 spaces private key")
 }
 
 // Cmd represents the base command when called without any subcommands

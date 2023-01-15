298371a6f49a04140de8503774559254f01e30d7
alex <alexg89@vt.edu>
2023-01-14 18:55:37 -0800

    adds validator service groups

--- datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/8_validator_groups.sql
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/8_validator_groups.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "public"."validator_service_org_group" (
-    "group_id" int8 NOT NULL,
+    "group_name" text NOT NULL,
     "org_id" int8 NOT NULL REFERENCES orgs(org_id),
     "pubkey" text NOT NULL CHECK(LENGTH(pubkey)=98),
     "protocol_network_id" int8 NOT NULL REFERENCES protocol_networks(protocol_network_id) DEFAULT 1,
@@ -7,6 +7,6 @@ CREATE TABLE "public"."validator_service_org_group" (
 );
 
 ALTER TABLE "public"."validator_service_org_group" ADD CONSTRAINT "validators_org_group_pubkey_org_uniq" PRIMARY KEY ("org_id", "pubkey");
-ALTER TABLE "public"."validator_service_org_group" ADD CONSTRAINT "validators_org_group_pkey" UNIQUE ("group_id");
-ALTER TABLE "public"."validator_service_org_group" ADD CONSTRAINT "validators_org_group_pubkey_uniq" UNIQUE ("pubkey", "group_id");
 ALTER TABLE "public"."validator_service_org_group" ADD CONSTRAINT "validators_org_group_validator_pubkey_uniq" UNIQUE ("pubkey");
+ALTER TABLE "public"."validator_service_org_group" ADD CONSTRAINT "validators_org_group_validator_pubkey_network_uniq" UNIQUE ("pubkey", "protocol_network_id");
+CREATE INDEX "org_group_index" ON "public"."validator_service_org_group" ("group_name", "org_id", "protocol_network_id");

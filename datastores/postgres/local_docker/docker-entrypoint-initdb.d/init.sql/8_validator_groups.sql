768160e14b069638a3246c52f391ddf2311b0117
alex <alexg89@vt.edu>
2023-01-15 16:38:51 -0800

    add table for service to cloud location

--- datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/8_validator_groups.sql
+++ datastores/postgres/local_docker/docker-entrypoint-initdb.d/init.sql/8_validator_groups.sql
@@ -11,3 +11,10 @@ ALTER TABLE "public"."validators_service_org_groups" ADD CONSTRAINT "validators_
 ALTER TABLE "public"."validators_service_org_groups" ADD CONSTRAINT "validators_org_group_validator_pubkey_uniq" UNIQUE ("pubkey");
 ALTER TABLE "public"."validators_service_org_groups" ADD CONSTRAINT "validators_org_group_validator_pubkey_network_uniq" UNIQUE ("pubkey", "protocol_network_id");
 CREATE INDEX "org_group_index" ON "public"."validators_service_org_groups" ("group_name", "org_id", "protocol_network_id");
+
+CREATE TABLE "public"."validators_service_org_groups_cloud_ctx_ns" (
+    "cloud_ctx_ns_id" int8 NOT NULL REFERENCES topologies_org_cloud_ctx_ns(cloud_ctx_ns_id),
+    "pubkey" text NOT NULL CHECK(LENGTH(pubkey)=98) NOT NULL REFERENCES validators_service_org_groups(pubkey)
+);
+
+ALTER TABLE "public"."validators_service_org_groups_cloud_ctx_ns" ADD CONSTRAINT "validators_service_org_groups_cloud_ctx_ns_pk" PRIMARY KEY ("pubkey", "cloud_ctx_ns_id");

6417692f51feb4a2d38da431cfd40ffc423220e9
alex <alexg89@vt.edu>
2023-01-27 17:27:53 -0800

    adds cmd args athena fn

--- cookbooks/olympus/ethereum/beacons/athena_config.go
+++ cookbooks/olympus/ethereum/beacons/athena_config.go
@@ -5,30 +5,55 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
+// TODO env vars
+
 var (
+	AthenaPort = v1.ContainerPort{
+		Name:          "athena",
+		ContainerPort: 9003,
+		Protocol:      v1.Protocol("TCP"),
+	}
 	AthenaContainer = zeus_topology_config_drivers.ContainerDriver{
 		Container: v1.Container{
-			Name:                     "",
-			Image:                    "",
-			Command:                  nil,
-			Args:                     nil,
-			WorkingDir:               "",
-			Ports:                    nil,
-			EnvFrom:                  nil,
-			Env:                      nil,
-			Resources:                v1.ResourceRequirements{},
-			VolumeMounts:             nil,
-			VolumeDevices:            nil,
-			LivenessProbe:            nil,
-			ReadinessProbe:           nil,
-			StartupProbe:             nil,
-			Lifecycle:                nil,
-			TerminationMessagePath:   "",
-			TerminationMessagePolicy: "",
-			ImagePullPolicy:          "Always",
-			SecurityContext:          nil,
-			Stdin:                    false,
-			StdinOnce:                false,
-			TTY:                      false,
+			Name:            "athena",
+			Image:           "registry.digitalocean.com/zeus-fyi/athena:latest",
+			Command:         []string{"/bin/sh"},
+			Args:            AthenaCmdArgs(),
+			Ports:           []v1.ContainerPort{AthenaPort},
+			EnvFrom:         nil,
+			Env:             nil,
+			Resources:       v1.ResourceRequirements{},
+			VolumeMounts:    nil,
+			VolumeDevices:   nil,
+			LivenessProbe:   nil,
+			ReadinessProbe:  nil,
+			StartupProbe:    nil,
+			ImagePullPolicy: "Always",
+			SecurityContext: nil,
+			Stdin:           false,
+			StdinOnce:       false,
+			TTY:             false,
 		}}
 )
+
+const (
+	startDashC     = "-c"
+	athenaStartCmd = "athena"
+)
+
+func AthenaCmdArgs() []string {
+	args := []string{startDashC, athenaStartCmd}
+
+	args = append(args, "--bearer=\"${BEARER}\"")
+	args = append(args, "--cloud-provider=\"${CLOUD_PROVIDER}\"")
+	args = append(args, "--ctx=\"${CTX}\"")
+	args = append(args, "--ns=\"${NS}\"")
+	args = append(args, "--region=\"${REGION}\"")
+
+	args = append(args, "--age-private-key=\"${AGE_PKEY}\"")
+	args = append(args, "--do-spaces-key=\"${DO_SPACES_KEY}\"")
+	args = append(args, "--do-spaces-private-key=\"${DO_SPACES_PKEY}\"")
+	args = append(args, "--env=\"production\"")
+
+	return args
+}

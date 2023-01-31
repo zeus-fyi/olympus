b70b73758965a8e68ff4ba4ba66da0947a8192ff
alex <alexg89@vt.edu>
2023-01-31 14:29:51 -0800

    updates log messages

--- pkg/aegis/auth_startup/secrets_fetch.go
+++ pkg/aegis/auth_startup/secrets_fetch.go
@@ -114,10 +114,10 @@ func RunPoseidonDigitalOceanS3BucketObjSecretsProcedure(ctx context.Context, aut
 }
 
 func RunAthenaDigitalOceanS3BucketObjSecretsProcedure(ctx context.Context, authCfg AuthConfig) (memfs.MemFS, SecretsWrapper) {
-	log.Info().Msg("Zeus: RunDigitalOceanS3BucketObjSecretsProcedure starting")
+	log.Info().Msg("Athena: RunDigitalOceanS3BucketObjSecretsProcedure starting")
 
 	inMemSecrets := ReadEncryptedSecretsData(ctx, authCfg)
-	log.Info().Msg("RunDigitalOceanS3BucketObjSecretsProcedure finished")
+	log.Info().Msg("Athena: RunDigitalOceanS3BucketObjSecretsProcedure finished")
 	sw := SecretsWrapper{}
 	sw.PostgresAuth = sw.ReadSecret(ctx, inMemSecrets, pgSecret)
 
@@ -132,12 +132,12 @@ func RunAthenaDigitalOceanS3BucketObjSecretsProcedure(ctx context.Context, authC
 	}
 	rcloneConf, err := inMemSecrets.ReadFile(rcloneSecret)
 	if err != nil {
-		log.Err(err).Msg("RunAthenaDigitalOceanS3BucketObjSecretsProcedure failed to set rclone conf")
+		log.Err(err).Msg("Athena:  RunAthenaDigitalOceanS3BucketObjSecretsProcedure failed to set rclone conf")
 		misc.DelayedPanic(err)
 	}
 	err = p.WriteToFileOutPath(rcloneConf)
 	if err != nil {
-		log.Err(err).Msg("RunAthenaDigitalOceanS3BucketObjSecretsProcedure failed to set rclone conf")
+		log.Err(err).Msg("Athena:  RunAthenaDigitalOceanS3BucketObjSecretsProcedure failed to set rclone conf")
 		misc.DelayedPanic(err)
 	}
 	return inMemSecrets, sw

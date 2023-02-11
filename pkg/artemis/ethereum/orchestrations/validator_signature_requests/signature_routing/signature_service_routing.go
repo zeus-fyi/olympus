43d151414749b8a238ba8e7ddfc183c236b35c4c
alex <alexg89@vt.edu>
2023-02-11 15:45:23 -0800

    refactor

--- pkg/artemis/ethereum/orchestrations/validator_signature_requests/signature_routing/signature_service_routing.go
+++ pkg/artemis/ethereum/orchestrations/validator_signature_requests/signature_routing/signature_service_routing.go
@@ -3,18 +3,25 @@ package artemis_validator_signature_service_routing
 import (
 	"context"
 	"fmt"
-	artemis_hydra_orchestrations_aws_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/validator_signature_requests/aws_auth"
-	aws_secrets "github.com/zeus-fyi/zeus/pkg/aegis/aws"
-	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
-	hestia_req_types "github.com/zeus-fyi/zeus/pkg/hestia/client/req_types"
 	"time"
 
 	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog/log"
 	artemis_validator_service_groups_models "github.com/zeus-fyi/olympus/datastores/postgres/apps/artemis/models"
+	artemis_hydra_orchestrations_aws_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/validator_signature_requests/aws_auth"
+	"github.com/zeus-fyi/olympus/pkg/utils/file_io/lib/v0/memfs"
+	aws_secrets "github.com/zeus-fyi/zeus/pkg/aegis/aws"
 	bls_serverless_signing "github.com/zeus-fyi/zeus/pkg/aegis/aws/serverless_signing"
+	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
+	hestia_req_types "github.com/zeus-fyi/zeus/pkg/hestia/client/req_types"
 	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_common_types"
 )
+
+var RouteMapInMemFS memfs.MemFS
+
+func InitRouteMapInMemFS(ctx context.Context) {
+	RouteMapInMemFS = memfs.NewMemFs()
+}
 
 type ServiceRoutes struct {
 	To []ServiceRoute
@@ -69,14 +76,17 @@ func GetServiceRoutesAuths(ctx context.Context, vsRoute artemis_validator_servic
 		}
 		fmt.Sprint(s)
 	}
-
 	return nil
 }
 
 func GroupSigRequestsByServiceURL(ctx context.Context, sr aegis_inmemdbs.EthereumBLSKeySignatureRequests) map[string]aegis_inmemdbs.EthereumBLSKeySignatureRequests {
 	m := make(map[string]aegis_inmemdbs.EthereumBLSKeySignatureRequests)
 	for pubkey, signReq := range sr.Map {
-		svcURL := MockGetServiceURL(pubkey)
+		svcURL, err := GetServiceURLFromInMemFS(ctx, pubkey)
+		if err != nil {
+			log.Ctx(ctx).Error().Err(err).Msg("GroupSigRequestsByServiceURL")
+			continue
+		}
 		if _, ok := m[svcURL]; !ok {
 			m[svcURL] = aegis_inmemdbs.EthereumBLSKeySignatureRequests{Map: make(map[string]aegis_inmemdbs.EthereumBLSKeySignatureRequest)}
 		}
@@ -85,7 +95,11 @@ func GroupSigRequestsByServiceURL(ctx context.Context, sr aegis_inmemdbs.Ethereu
 	return m
 }
 
-func MockGetServiceURL(pubkey string) string {
-	// TODO lookup in cache
-	return "http://localhost:8080"
+func GetServiceURLFromInMemFS(ctx context.Context, pubkey string) (string, error) {
+	svcURL, err := RouteMapInMemFS.ReadFile(pubkey)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("MockGetServiceURL")
+		return "", err
+	}
+	return string(svcURL), nil
 }
--- pkg/artemis/ethereum/orchestrations/validator_signature_requests/signature_routing/signature_service_routing_test.go
+++ pkg/artemis/ethereum/orchestrations/validator_signature_requests/signature_routing/signature_service_routing_test.go
@@ -2,14 +2,16 @@ package artemis_validator_signature_service_routing
 
 import (
 	"context"
+	"testing"
+
 	"github.com/stretchr/testify/suite"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
 	hestia_test "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/test"
 	artemis_hydra_orchestrations_aws_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/validator_signature_requests/aws_auth"
+	"github.com/zeus-fyi/olympus/pkg/utils/file_io/lib/v0/filepaths"
 	aws_secrets "github.com/zeus-fyi/zeus/pkg/aegis/aws"
 	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
 	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_common_types"
-	"testing"
 )
 
 type ValidatorServiceAuthRoutesTestSuite struct {
@@ -31,7 +33,18 @@ func (s *ValidatorServiceAuthRoutesTestSuite) SetupTest() {
 func (s *ValidatorServiceAuthRoutesTestSuite) TestServiceGroupingHelper() {
 	srs := aegis_inmemdbs.EthereumBLSKeySignatureRequests{Map: make(map[string]aegis_inmemdbs.EthereumBLSKeySignatureRequest)}
 
-	// TODO fill in srs
+	keyOne := "0x1"
+	srs.Map[keyOne] = aegis_inmemdbs.EthereumBLSKeySignatureRequest{Message: "one"}
+	srs.Map["0x2"] = aegis_inmemdbs.EthereumBLSKeySignatureRequest{Message: "two"}
+	srs.Map["0x3"] = aegis_inmemdbs.EthereumBLSKeySignatureRequest{Message: "one"}
+	InitRouteMapInMemFS(ctx)
+	p := filepaths.Path{
+		DirIn: ".",
+		FnIn:  keyOne,
+	}
+	err := RouteMapInMemFS.MakeFileIn(&p, []byte("https://fake-service.com"))
+	s.Require().Nil(err)
+
 	resp := GroupSigRequestsByServiceURL(ctx, srs)
 	s.Require().NotEmpty(resp)
 }

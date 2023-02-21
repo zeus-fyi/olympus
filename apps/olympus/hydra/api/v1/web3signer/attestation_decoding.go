cd6176ed16d62e25957c9b30abe92160d7ad2b2d
alex <alexg89@vt.edu>
2023-02-21 13:29:19 -0800

    update attestation watermarker

--- apps/olympus/hydra/api/v1/web3signer/attestation_decoding.go
+++ apps/olympus/hydra/api/v1/web3signer/attestation_decoding.go
@@ -9,12 +9,7 @@ import (
 )
 
 func CanSignAttestation(ctx context.Context, pubkey string, att consensys_eth2_openapi.AttestationData) error {
-	sourceEpoch, targetEpoch, err := ethereum_slashing_protection_watermarking.ConvertAttSourceTargetsToInt(ctx, att)
-	if err != nil {
-		log.Ctx(ctx).Error().Err(err).Msg("failed to convert attestation source & target epoch to int")
-		return err
-	}
-	err = ethereum_slashing_protection_watermarking.WatermarkAttestation(ctx, pubkey, sourceEpoch, targetEpoch)
+	err := ethereum_slashing_protection_watermarking.WatermarkAttestation(ctx, pubkey, att)
 	if err != nil {
 		log.Ctx(ctx).Error().Err(err).Msg("failed to check watermark for attestation")
 		return err

bde38a2fdbd796b5865746daa1781b289c7f6a14
alex <alexg89@vt.edu>
2023-02-22 18:29:12 -0800

    adds user create for wix

--- datastores/postgres/apps/hestia/models/create/org_users/insert_org_user.go
+++ datastores/postgres/apps/hestia/models/create/org_users/insert_org_user.go
@@ -43,7 +43,37 @@ const (
 	EthereumMainnetService    = "ethereumMainnetValidators"
 	ZeusServiceID             = 1677100016195486976
 	ZeusService               = "zeus"
+	ZeusWebhooksService       = "zeusWebhooks"
+	ZeusWebhooksServiceID     = 1677111502304971000
 )
+
+func (o *OrgUser) InsertOrgUserWithNewKeyForService(ctx context.Context, metadata []byte, keyname string, serviceID int) (string, error) {
+	q := sql_query_templates.NewQueryParam("NewDemoOrgUser", "org_users", "where", 1000, []string{})
+	q.RawQuery = `WITH new_user_id AS (
+					INSERT INTO users(metadata)
+					VALUES ($2)
+					RETURNING user_id
+				), cte_org_users AS (
+					INSERT INTO org_users(org_id, user_id)
+					VALUES($1, (SELECT user_id FROM new_user_id))
+					RETURNING (SELECT user_id FROM new_user_id)
+				), cte_users_key AS (
+				  INSERT INTO users_keys(user_id, public_key_name, public_key_verified, public_key_type_id, public_key)
+				  VALUES((SELECT user_id FROM new_user_id), $3, true, $4, $5)
+			    ) INSERT INTO users_key_services(public_key, service_id)
+			      VALUES($5, $6)
+				  RETURNING (SELECT user_id FROM new_user_id)
+				`
+	log.Debug().Interface("InsertQuery:", q.LogHeader(Sn))
+	var userID int64
+	userKey := rand.String(120)
+	err := apps.Pg.QueryRowWArgs(ctx, q.RawQuery, o.OrgID, metadata, keyname, keys.BearerKeyTypeID, userKey, serviceID).Scan(&userID)
+	if returnErr := misc.ReturnIfErr(err, q.LogHeader(Sn)); returnErr != nil {
+		return "", err
+	}
+	o.UserID = int(userID)
+	return userKey, misc.ReturnIfErr(err, q.LogHeader(Sn))
+}
 
 func (o *OrgUser) InsertDemoOrgUserWithNewKey(ctx context.Context, metadata []byte, keyname string, serviceID int) (string, error) {
 	q := sql_query_templates.NewQueryParam("NewDemoOrgUser", "org_users", "where", 1000, []string{})

3db5a9b770389356a45994fe829bcf02f5873cca
alex <alexg89@vt.edu>
2023-02-25 17:25:31 -0800

    adds service monitors

---
+++ cookbooks/olympus/ethereum/beacons/beacon_config.go
@@ -0,0 +1,35 @@
+package olympus_beacon_cookbooks
+
+import (
+	zeus_topology_config_drivers "github.com/zeus-fyi/zeus/pkg/zeus/workload_config_drivers"
+	v1 "k8s.io/api/core/v1"
+)
+
+// TODO
+var (
+	consensusClientStsEphemeralCfg = zeus_topology_config_drivers.StatefulSetDriver{
+		ContainerDrivers: map[string]zeus_topology_config_drivers.ContainerDriver{
+			"zeus-consensus-client": {
+				Container: v1.Container{
+					Name:  "zeus-consensus-client",
+					Image: "sigp/lighthouse:capella",
+				},
+			},
+		}}
+	consensusClientStsEphemeralCfgDriver = zeus_topology_config_drivers.TopologyConfigDriver{
+		StatefulSetDriver: &consensusClientStsEphemeralCfg,
+	}
+
+	execClientStsEphemeralCfg = zeus_topology_config_drivers.StatefulSetDriver{
+		ContainerDrivers: map[string]zeus_topology_config_drivers.ContainerDriver{
+			"zeus-exec-client": {
+				Container: v1.Container{
+					Name:  "zeus-exec-client",
+					Image: "ethpandaops/geth:master",
+				},
+			},
+		}}
+	execClientStsEphemeralCfgDriver = zeus_topology_config_drivers.TopologyConfigDriver{
+		StatefulSetDriver: &execClientStsEphemeralCfg,
+	}
+)

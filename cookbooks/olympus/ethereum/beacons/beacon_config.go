79d32e6f66c22a44354da0e07351bfbc9886a5ea
alex <alexg89@vt.edu>
2023-02-28 14:23:19 -0800

    config mainnet beacon

--- cookbooks/olympus/ethereum/beacons/beacon_config.go
+++ cookbooks/olympus/ethereum/beacons/beacon_config.go
@@ -1,11 +1,15 @@
 package olympus_beacon_cookbooks
 
 import (
+	"fmt"
+
+	olympus_common_vals_cookbooks "github.com/zeus-fyi/olympus/cookbooks/olympus/common"
+	hestia_req_types "github.com/zeus-fyi/zeus/pkg/hestia/client/req_types"
+	zeus_cluster_config_drivers "github.com/zeus-fyi/zeus/pkg/zeus/cluster_config_drivers"
 	zeus_topology_config_drivers "github.com/zeus-fyi/zeus/pkg/zeus/workload_config_drivers"
 	v1 "k8s.io/api/core/v1"
 )
 
-// TODO
 var (
 	consensusClientStsEphemeralCfg = zeus_topology_config_drivers.StatefulSetDriver{
 		ContainerDrivers: map[string]zeus_topology_config_drivers.ContainerDriver{
@@ -32,3 +36,48 @@ var (
 		StatefulSetDriver: &execClientStsEphemeralCfg,
 	}
 )
+
+func ClusterConfigEnvVars(cd *zeus_cluster_config_drivers.ClusterDefinition, network string) []v1.EnvVar {
+	var envVar v1.EnvVar
+	switch network {
+	case "mainnet":
+		cdTmp := zeus_topology_config_drivers.ContainerDriver{}
+		envVar = cdTmp.CreateEnvVarKeyValue(protocolNetworkKeyEnv, fmt.Sprintf("%d", hestia_req_types.EthereumMainnetProtocolNetworkID))
+	case "ephemery":
+	}
+
+	depCfgOverride := zeus_topology_config_drivers.DeploymentDriver{}
+	depCfgOverride.ContainerDrivers = make(map[string]zeus_topology_config_drivers.ContainerDriver)
+	stsCfgOverride := zeus_topology_config_drivers.StatefulSetDriver{}
+	stsCfgOverride.ContainerDrivers = make(map[string]zeus_topology_config_drivers.ContainerDriver)
+	envVarsChoreography := olympus_common_vals_cookbooks.GetChoreographyEnvVars()
+	internalAuthEnvVars := olympus_common_vals_cookbooks.GetCommonInternalAuthEnvVars()
+	combinedEnvVars := append(envVarsChoreography, internalAuthEnvVars...)
+	combinedEnvVars = append(combinedEnvVars, envVar)
+
+	if cd == nil {
+		return combinedEnvVars
+	}
+	for k, v := range cd.ComponentBases {
+		if k == "hydra" || k == "hydraChoreography" {
+			containCfg := zeus_topology_config_drivers.ContainerDriver{}
+			containCfg.Env = combinedEnvVars
+
+			// deployments
+			depCfgOverride.ContainerDrivers["zeus-hydra-choreography"] = containCfg
+			cfgOverride := zeus_topology_config_drivers.TopologyConfigDriver{
+				IngressDriver:     nil,
+				StatefulSetDriver: nil,
+				ServiceDriver:     nil,
+				DeploymentDriver:  &depCfgOverride,
+			}
+			tmp := v
+
+			tmpSb := tmp.SkeletonBases[k]
+			tmpSb.TopologyConfigDriver = &cfgOverride
+			tmp.SkeletonBases[k] = tmpSb
+			cd.ComponentBases[k] = tmp
+		}
+	}
+	return combinedEnvVars
+}
--- cookbooks/olympus/ethereum/beacons/beacons_test.go
+++ cookbooks/olympus/ethereum/beacons/beacons_test.go
@@ -1,16 +1,69 @@
 package olympus_beacon_cookbooks
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 	olympus_cookbooks "github.com/zeus-fyi/olympus/cookbooks"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_base"
+	api_configs "github.com/zeus-fyi/olympus/test/configs"
+	zeus_client "github.com/zeus-fyi/zeus/pkg/zeus/client"
 )
 
 type BeaconsTestCookbookTestSuite struct {
 	test_suites_base.TestSuite
+	ZeusTestClient zeus_client.ZeusClient
+}
+
+var ctx = context.Background()
+
+func (t *BeaconsTestCookbookTestSuite) TestMainnetClusterDeploy() {
+	olympus_cookbooks.ChangeToCookbookDir()
+	ClusterConfigEnvVars(&MainnetBeaconBaseClusterDefinition, "mainnet")
+
+	_, err := MainnetBeaconBaseClusterDefinition.UploadChartsFromClusterDefinition(ctx, t.ZeusTestClient, true)
+	t.Require().Nil(err)
+
+	cdep := MainnetBeaconBaseClusterDefinition.GenerateDeploymentRequest()
+	_, err = t.ZeusTestClient.DeployCluster(ctx, cdep)
+	t.Require().Nil(err)
+}
+
+func (t *BeaconsTestCookbookTestSuite) TestMainnetClusterSetup() {
+	olympus_cookbooks.ChangeToCookbookDir()
+	ClusterConfigEnvVars(&MainnetBeaconBaseClusterDefinition, "mainnet")
+
+	gcd := MainnetBeaconBaseClusterDefinition.BuildClusterDefinitions()
+	t.Assert().NotEmpty(gcd)
+	fmt.Println(gcd)
+
+	gdr := MainnetBeaconBaseClusterDefinition.GenerateDeploymentRequest()
+	t.Assert().NotEmpty(gdr)
+	fmt.Println(gdr)
+
+	sbDefs, err := MainnetBeaconBaseClusterDefinition.GenerateSkeletonBaseCharts()
+	t.Require().Nil(err)
+	t.Assert().NotEmpty(sbDefs)
+}
+func (t *BeaconsTestCookbookTestSuite) TestMainnetClusterCreate() {
+	olympus_cookbooks.ChangeToCookbookDir()
+	cdCfg := MainnetBeaconBaseClusterDefinition
+
+	gcd := cdCfg.BuildClusterDefinitions()
+	t.Assert().NotEmpty(gcd)
+	fmt.Println(gcd)
+	err := gcd.CreateClusterClassDefinitions(ctx, t.ZeusTestClient)
+	t.Require().Nil(err)
+
+	gdr := cdCfg.GenerateDeploymentRequest()
+	t.Assert().NotEmpty(gdr)
+	fmt.Println(gdr)
+
+	sbDefs, err := cdCfg.GenerateSkeletonBaseCharts()
+	t.Require().Nil(err)
+	t.Assert().NotEmpty(sbDefs)
 }
 
 func (t *BeaconsTestCookbookTestSuite) TestClusterSetup() {
@@ -31,6 +84,9 @@ func (t *BeaconsTestCookbookTestSuite) TestClusterSetup() {
 
 func (t *BeaconsTestCookbookTestSuite) SetupTest() {
 	olympus_cookbooks.ChangeToCookbookDir()
+
+	tc := api_configs.InitLocalTestConfigs()
+	t.ZeusTestClient = zeus_client.NewDefaultZeusClient(tc.Bearer)
 }
 
 func TestBeaconsTestCookbookTestSuite(t *testing.T) {

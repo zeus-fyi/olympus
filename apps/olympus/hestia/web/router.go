83713fa7b21fa5c5e4d1a9a4478ccf5f27864854
alex <alexg89@vt.edu>
2023-03-04 14:23:22 -0800

    adds user email to verify password query

--- apps/olympus/hestia/web/router.go
+++ apps/olympus/hestia/web/router.go
@@ -1,19 +1,11 @@
 package hestia_web_router
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	hestia_login "github.com/zeus-fyi/olympus/hestia/web/login"
 )
 
 func WebRoutes(e *echo.Echo) *echo.Echo {
-	// Routes
-
 	e.POST("/login", hestia_login.CreateLoginHandler)
 	return e
-}
-
-func Login(c echo.Context) error {
-	return c.String(http.StatusOK, "Healthy")
 }

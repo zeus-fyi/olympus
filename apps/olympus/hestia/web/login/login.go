ade78b61a4b3df166fcbe45fc5bb9d13f7a138b4
alex <alexg89@vt.edu>
2023-03-17 20:02:33 -0700

    add domain

--- apps/olympus/hestia/web/login/login.go
+++ apps/olympus/hestia/web/login/login.go
@@ -62,6 +62,7 @@ func (l *LoginRequest) VerifyPassword(c echo.Context) error {
 		Name:     aegis_sessions.SessionIDNickname,
 		Value:    sessionID,
 		Path:     "/",
+		Domain:   ".zeus.fyi",
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteNoneMode,

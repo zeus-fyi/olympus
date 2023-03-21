b083182059ca5db7969a70b3fb9ea60c175ed267
alex <alexg89@vt.edu>
2023-03-21 11:51:06 -0700

    adds email template

--- pkg/hermes/email/email.go
+++ pkg/hermes/email/email.go
@@ -1,1 +1,70 @@
 package hermes_email_notifications
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
+	"github.com/rs/zerolog/log"
+	aws_aegis_auth "github.com/zeus-fyi/zeus/pkg/aegis/aws/auth"
+)
+
+type HermesEmailNotifications struct {
+	*sesv2.Client
+}
+
+func InitHermesEmailNotifications(ctx context.Context, a aws_aegis_auth.AuthAWS) HermesEmailNotifications {
+	cfg, err := a.CreateConfig(ctx)
+	if err != nil {
+		log.Ctx(ctx).Err(err).Msg("InitSES: error loading config")
+		panic(err)
+	}
+	client := sesv2.NewFromConfig(cfg)
+	return HermesEmailNotifications{Client: client}
+}
+
+func (h *HermesEmailNotifications) SendEmailTo(ctx context.Context, toEmails []string) (*sesv2.SendEmailOutput, error) {
+
+	// TODO token creation
+	params := &sesv2.SendEmailInput{
+		Content: &types.EmailContent{
+			Raw: nil,
+			Simple: &types.Message{
+				Body: &types.Body{
+					Html: &types.Content{
+						Data:    nil,
+						Charset: nil,
+					},
+					Text: &types.Content{
+						Data:    nil,
+						Charset: nil,
+					},
+				},
+				Subject: &types.Content{
+					Data:    aws.String("Email Verification"),
+					Charset: nil,
+				},
+			},
+			Template: nil,
+		},
+		ConfigurationSetName: nil,
+		Destination: &types.Destination{
+			BccAddresses: nil,
+			CcAddresses:  nil,
+			ToAddresses:  toEmails,
+		},
+		FeedbackForwardingEmailAddress:            nil,
+		FeedbackForwardingEmailAddressIdentityArn: nil,
+		FromEmailAddress:                          nil,
+		FromEmailAddressIdentityArn:               nil,
+		ListManagementOptions:                     nil,
+		ReplyToAddresses:                          nil,
+	}
+	resp, err := h.SendEmail(ctx, params)
+	if err != nil {
+		log.Ctx(ctx).Info().Interface("resp", resp).Err(err).Msg("HermesEmailNotifications: SendEmailVerify: error")
+		panic(err)
+	}
+	return resp, err
+}
--- pkg/hermes/email/email_test.go
+++ pkg/hermes/email/email_test.go
@@ -1,1 +1,33 @@
 package hermes_email_notifications
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_base"
+	aws_aegis_auth "github.com/zeus-fyi/zeus/pkg/aegis/aws/auth"
+)
+
+type EmailTestSuite struct {
+	test_suites_base.TestSuite
+}
+
+var ctx = context.Background()
+
+func (s *EmailTestSuite) SendTestEmail() {
+	auth := aws_aegis_auth.AuthAWS{
+		Region:    "us-west-1",
+		AccessKey: s.Tc.AwsAccessKeySES,
+		SecretKey: s.Tc.AwsSecretKeySES,
+	}
+	h := InitHermesEmailNotifications(ctx, auth)
+	s.Require().Nil(h.Client)
+	r, err := h.SendEmailTo(ctx, []string{"alex@zeus.fyi"})
+	s.Require().Nil(err)
+	s.Require().NotNil(r)
+}
+
+func TestEmailTestSuite(t *testing.T) {
+	suite.Run(t, new(EmailTestSuite))
+}

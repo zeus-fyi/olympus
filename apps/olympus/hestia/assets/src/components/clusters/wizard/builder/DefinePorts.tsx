0ca2ae7f0042f46c5093dc14fed7c0c5515c2be8
alex <alexg89@vt.edu>
2023-03-29 13:33:19 -0700

    adds remove port button

--- apps/olympus/hestia/assets/src/components/clusters/wizard/builder/DefinePorts.tsx
+++ apps/olympus/hestia/assets/src/components/clusters/wizard/builder/DefinePorts.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import {useState} from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import {Box} from "@mui/material";
+import {Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../../redux/store";
+import {setDockerImagePort} from "../../../../redux/clusters/clusters.builder.reducer";
 
 export function AddPortsInputFields() {
     const dispatch = useDispatch();
@@ -13,7 +14,7 @@ export function AddPortsInputFields() {
     const selectedSkeletonBaseName = useSelector((state: RootState) => state.clusterBuilder.selectedSkeletonBaseName);
     const selectedContainerName = useSelector((state: RootState) => state.clusterBuilder.selectedContainerName);
     const skeletonBaseKeys = cluster.componentBases[selectedComponentBaseName];
-    const [inputFields, setInputFields] = useState<{ name: string; number: number; protocol: string }[]>([{ name: '', number: 0, protocol: '' }]);
+    const [inputFields, setInputFields] = useState<{ name: string; number: number; protocol: string }[]>([{ name: '', number: 0, protocol: 'TCP' }]);
 
     if (cluster.componentBases === undefined) {
         return <div></div>
@@ -34,19 +35,40 @@ export function AddPortsInputFields() {
         values[index] = {...values[index], [event.target.name]: event.target.value};
         //console.log("values: " + values[index].name + " " + values[index].number + " " + values[index].protocol)
         setInputFields(values);
-        // dispatch(setDockerImagePort({
-        //     componentBaseKey: selectedComponentBaseName,
-        //     skeletonBaseKey: selectedSkeletonBaseName,
-        //     containerName: selectedContainerName,
-        //     dockerImageKey: skeletonBaseContainerNames.containers[selectedContainerName].dockerImage.imageName,
-        //     portIndex: index,
-        //     port: values[index],
-        // }));
+        dispatch(setDockerImagePort({
+            componentBaseKey: selectedComponentBaseName,
+            skeletonBaseKey: selectedSkeletonBaseName,
+            containerName: selectedContainerName,
+            dockerImageKey: skeletonBaseContainerNames.containers[selectedContainerName].dockerImage.imageName,
+            portIndex: index,
+            port: values[index],
+        }));
     };
 
+    const handleRemoveField = (index: number) => {
+        const values = [...inputFields];
+        values.splice(index, 1);
+        setInputFields(values);
+    };
+    const handleChangeSelect = (index: number, event: SelectChangeEvent<string>) => {
+        const values = [...inputFields];
+        values[index] = { ...values[index], [event.target.name]: event.target.value };
+        setInputFields(values);
+        dispatch(
+            setDockerImagePort({
+                componentBaseKey: selectedComponentBaseName,
+                skeletonBaseKey: selectedSkeletonBaseName,
+                containerName: selectedContainerName,
+                dockerImageKey:
+                skeletonBaseContainerNames.containers[selectedContainerName].dockerImage.imageName,
+                portIndex: index,
+                port: values[index],
+            })
+        );
+    };
 
     const handleAddField = () => {
-        setInputFields([...inputFields, { name: '', number: 0, protocol: ''}]);
+        setInputFields([...inputFields, { name: '', number: 0, protocol: 'TCP'}]);
     };
 
     return (
@@ -75,20 +97,34 @@ export function AddPortsInputFields() {
                             onChange={(event) => handleChange(index, event)}
                             sx={{ mr: 1 }}
                         />
-                        <TextField
-                            name="protocol"
-                            fullWidth
-                            id={`portProtocol-${index}`}
-                            label={`Protocol`}
-                            variant="outlined"
-                            value={inputField.protocol}
-                            onChange={(event) => handleChange(index, event)}
-                        />
+                        <FormControl fullWidth variant="outlined">
+                            <InputLabel id={`portProtocolLabel-${index}`}>Protocol</InputLabel>
+                            <Select
+                                labelId={`portProtocolLabel-${index}`}
+                                id={`portProtocol-${index}`}
+                                name="protocol"
+                                value={inputField.protocol}
+                                onChange={(event) => handleChangeSelect(index, event)}
+                                label="Protocol"
+                            >
+                                <MenuItem value="TCP">TCP</MenuItem>
+                                <MenuItem value="UDP">UDP</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Box sx={{ ml: 2 }}>
+                            <Button
+                                variant="contained"
+                                onClick={() => handleRemoveField(index)}
+                            >
+                                Remove
+                            </Button>
+                        </Box>
                     </Box>
                 ))}
                 <Button variant="contained" onClick={handleAddField}>
                     Add Item
                 </Button>
+
             </Box>
         </div>
     );

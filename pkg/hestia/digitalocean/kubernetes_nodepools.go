b739bdc9a2a4fa8591013ea62a0569689ee4c1f5
alex <alexg89@vt.edu>
2023-04-09 15:39:12 -0700

    updates ui -> deploy params

--- pkg/hestia/digitalocean/kubernetes_nodepools.go
+++ pkg/hestia/digitalocean/kubernetes_nodepools.go
@@ -8,16 +8,10 @@ import (
 )
 
 func (d *DigitalOcean) AddToNodePool(ctx context.Context, clusterID string, nodesReq *godo.KubernetesNodePoolCreateRequest) (*godo.KubernetesNodePool, error) {
-	//createRequest := &godo.KubernetesNodePoolCreateRequest{
-	//	Name:   "pool-02",
-	//	Size:   "s-2vcpu-4gb",
-	//	Count:  1,
-	//	Tags:   []string{"web"},
-	//	Labels: map[string]string{"service": "web", "priority": "high"},
-	//}
 	nodePool, _, err := d.Kubernetes.CreateNodePool(ctx, clusterID, nodesReq)
 	if err != nil {
 		log.Ctx(ctx).Error().Err(err).Msg("failed to create node pool")
+		return nil, err
 	}
 	return nodePool, err
 }

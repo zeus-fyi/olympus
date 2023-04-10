0aa212edf11e8336668b9e320196f3b603f5f348
alex <alexg89@vt.edu>
2023-04-09 18:47:06 -0700

    adds stripe products

---
+++ apps/olympus/hestia/web/billing/stripe.go
@@ -0,0 +1,37 @@
+package hestia_billing
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/setupintent"
+)
+
+type StripeBillingRequest struct {
+}
+
+func StripeBillingRequestHandler(c echo.Context) error {
+	request := new(StripeBillingRequest)
+	if err := c.Bind(request); err != nil {
+		return err
+	}
+	return request.Dothing(c)
+}
+
+type CheckoutData struct {
+	ClientSecret string `json:"client_secret"`
+}
+
+func (s *StripeBillingRequest) Dothing(c echo.Context) error {
+	params := &stripe.SetupIntentParams{
+		Customer:           stripe.String("{{CUSTOMER_ID}}"),
+		PaymentMethodTypes: []*string{stripe.String("card"), stripe.String("link")},
+	}
+	result, err := setupintent.New(params)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	resp := CheckoutData{result.ClientSecret}
+	return c.JSON(http.StatusOK, resp)
+}

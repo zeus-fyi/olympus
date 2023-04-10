8a12ed572bb452c2d13e877599ed7c08faabfa77
alex <alexg89@vt.edu>
2023-04-10 10:02:19 -0700

    adds context id

--- pkg/hestia/digitalocean/kubernetes_nodepools_test.go
+++ pkg/hestia/digitalocean/kubernetes_nodepools_test.go
@@ -4,20 +4,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_base"
 )
 
 type DoKubernetesTestSuite struct {
 	test_suites_base.TestSuite
+	do DigitalOcean
 }
 
-func (s *DoKubernetesTestSuite) TestGetSizes() {
+func (s *DoKubernetesTestSuite) SetupTest() {
+	s.InitLocalConfigs()
+	s.do = InitDoClient(ctx, s.Tc.DigitalOceanAPIKey)
+	s.Require().NotNil(s.do.Client)
+	apps.Pg.InitPG(ctx, s.Tc.LocalDbPgconn)
+	//apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+}
+func (s *DoKubernetesTestSuite) TestGetNodePools() {
 	s.InitLocalConfigs()
 
-	do := InitDoClient(ctx, "token")
-	s.Require().NotNil(do.Client)
-
-	// TODO
+	nodePools, _, err := s.do.Client.Kubernetes.List(ctx, nil)
+	s.Require().NoError(err)
+	s.Require().NotEmpty(nodePools)
 }
 
 func (s *DoKubernetesTestSuite) TestCreateNodePool() {

44ef5a5ff229f74231b93df5e12279e2268257cd
alex <alexg89@vt.edu>
2023-04-18 14:48:36 -0700

    omit empty

--- apps/olympus/zeus/api/v1/zeus/topology/external_actions/nodes/nodes.go
+++ apps/olympus/zeus/api/v1/zeus/topology/external_actions/nodes/nodes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/topologies/definitions/kns"
 	autok8s_core "github.com/zeus-fyi/olympus/pkg/zeus/core"
 	"github.com/zeus-fyi/olympus/zeus/pkg/zeus"
@@ -16,18 +17,20 @@ func ExternalApiNodesRoutes(e *echo.Group, k8Cfg autok8s_core.K8Util) *echo.Grou
 }
 
 type ActionRequest struct {
-	kns.TopologyKubeCtxNs
-	Labels map[string]string `json:"labels,omitempty"`
-	Action string            `json:"action"`
+	kns.TopologyKubeCtxNs `json:"topologyKubeCtxNs,omitempty"`
+	Labels                map[string]string `json:"labels,omitempty"`
+	Action                string            `json:"action"`
 }
 
 func NodeActionsRequestHandler(c echo.Context) error {
 	request := new(ActionRequest)
 	if err := c.Bind(request); err != nil {
+		log.Err(err).Msg("NodeActionsRequestHandler, Bind error")
 		return err
 	}
 	if request.Action == "list" {
 		return ListNodesRequest(c, request)
 	}
+	log.Info().Msg("NodeActionsRequestHandler, no valid actions found")
 	return c.JSON(http.StatusBadRequest, nil)
 }

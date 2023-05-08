b9415df98fb70eb7bb578a58a914d020e78fd536
alex <alexg89@vt.edu>
2023-05-07 20:34:29 -0700

    unpacks valid abi data

--- pkg/artemis/web3_client/web3_client_test.go
+++ pkg/artemis/web3_client/web3_client_test.go
@@ -2,12 +2,16 @@ package web3_client
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"runtime"
 	"testing"
 
 	"github.com/gochain/gochain/v4/common"
 	"github.com/stretchr/testify/suite"
 	"github.com/zeus-fyi/gochain/web3/accounts"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_encryption"
+	signing_automation_ethereum "github.com/zeus-fyi/zeus/pkg/artemis/signing_automation/ethereum"
 )
 
 type Web3ClientTestSuite struct {
@@ -62,12 +66,17 @@ func (s *Web3ClientTestSuite) TestWeb3ConnectMainnet() {
 }
 
 func (s *Web3ClientTestSuite) TestReadMempool() {
+	forceDirToLocation()
 	s.MainnetWeb3User.Web3Actions.Dial()
 	defer s.MainnetWeb3User.Close()
 	mempool, err := s.MainnetWeb3User.Web3Actions.GetTxPoolContent(ctx)
 	s.Require().Nil(err)
 	s.Assert().NotNil(mempool)
+	//abiConst := artemis_oly_contract_abis.UniswapV2RouterABI
 	addrFilter := "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+	abiFile, err := signing_automation_ethereum.ABIOpenFile(ctx, "./contract_abis/uniswapV2Abi.json")
+	s.Require().Nil(err)
+
 	smartContractAddrFilter := common.HexToAddress(addrFilter)
 	smartContractAddrFilterString := smartContractAddrFilter.String()
 	for userAddr, txPoolQueue := range mempool["pending"] {
@@ -75,11 +84,36 @@ func (s *Web3ClientTestSuite) TestReadMempool() {
 			if tx.To != nil && tx.To.String() == smartContractAddrFilterString {
 				fmt.Println(userAddr, order, tx)
 				fmt.Println("Found")
+				if tx.Input != nil {
+					input := *tx.Input
+					calldata := []byte(input)
+					sigdata := calldata[:4]
+					method, merr := abiFile.MethodById(sigdata[:4])
+					s.Assert().Nil(merr)
+					fmt.Println(method.Name)
+					argdata := calldata[4:]
+					if len(argdata)%32 != 0 {
+						fmt.Println("invalid argdata")
+						continue
+					}
+					inputValues, uerr := method.Inputs.UnpackValues(argdata)
+					s.Assert().Nil(uerr)
+					fmt.Println(inputValues)
+				}
 			}
 		}
 	}
 }
 
+func forceDirToLocation() string {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err.Error())
+	}
+	return dir
+}
 func TestWeb3ClientTestSuite(t *testing.T) {
 	suite.Run(t, new(Web3ClientTestSuite))
 }

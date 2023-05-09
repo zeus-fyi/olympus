1289647cc0a99dbc064dd8aacac04df60957824a
alex <alexg89@vt.edu>
2023-05-09 13:27:16 -0700

    adds pair & factory abis

---
+++ pkg/artemis/web3_client/uniswap_factory.go
@@ -0,0 +1,8 @@
+package web3_client
+
+import "github.com/gochain/gochain/v4/common"
+
+func (u *UniswapV2Client) GetPairContractFromFactory(addressOne, addressTwo common.Address) common.Address {
+	// TODO, smart contract get pair call
+	return common.Address{}
+}
--- pkg/artemis/web3_client/uniswap_pair.go
+++ pkg/artemis/web3_client/uniswap_pair.go
@@ -2,9 +2,16 @@ package web3_client
 
 import "github.com/gochain/gochain/v4/common"
 
-func (u *UniswapV2Client) GetPairContract(addressOne, addressTwo common.Address) common.Address {
+func (u *UniswapV2Client) GetPairContractPrices(addressOne, addressTwo common.Address) {
 
 	// TODO, smart contract get pair call
-
-	return common.Address{}
+	// price0CumulativeLast
+	// price1CumulativeLast
+	// token0
+	// token1
+	// getReserves
+	return
 }
+
+// TODO
+// function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;

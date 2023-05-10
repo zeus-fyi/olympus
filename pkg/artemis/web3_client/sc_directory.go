aa8a1debca718017098e16355ebdc8cf7e84e805
alex <alexg89@vt.edu>
2023-05-09 17:49:31 -0700

    adds common token trade addresses

--- pkg/artemis/web3_client/sc_directory.go
+++ pkg/artemis/web3_client/sc_directory.go
@@ -1,12 +1,25 @@
 package web3_client
 
+// TODO should also check decimal places when calculating rates
+
 const (
 	PepeContractAddr     = "0x6982508145454Ce325dDbE47a25d4ec3d2311933"
 	TetherContractAddr   = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 	BinanceCoinAddr      = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"
-	UsdCoinAddr          = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-	StEthAddr            = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84"
-	HexAddr              = "0x2B591e99fE9f32eAA6214f2a1D39E1aADa37bbd"
+	UsdCoinAddr          = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+	LidoSEthAddr         = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
 	MaticTokenAddr       = "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0"
 	WETH9ContractAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+	FourCoinAddr         = "0x244b797d622D4DEe8b188b03546ACAAbD0Cf91A0"
+	PoohCoinAddr         = "0xB69753c06BB5c366BE51E73bFc0cC2e3DC07E371"
+	BobTokenAddr         = "0x7D8146cf21e8D7cbe46054e01588207b51198729"
+	MongCoinAddr         = "0x1ce270557C1f68Cfb577b856766310Bf8B47FD9C"
+	WojakTokenAddr       = "0x5026F006B85729a8b14553FAE6af249aD16c9aaB"
+	GyoshiTokenAddr      = "0x1F17D72cBe65Df609315dF5c4f5F729eFbd00Ade"
+	PPizzaTokenAddr      = "0xab306326bC72c2335Bd08F42cbec383691eF8446"
+	WassieTokenAddr      = "0x2c95D751DA37A5C1d9c5a7Fd465c1d50F3d96160"
+	CapyBaraTokenAddr    = "0xF03D5fC6E08dE6Ad886fCa34aBF9a59ef633b78a"
+
+	// 8 decimal places
+	HexTokenAddr = "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39"
 )

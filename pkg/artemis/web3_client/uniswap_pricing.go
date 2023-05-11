aeb3a77530a515784e6095517de6fa2f50325d51
alex <alexg89@vt.edu>
2023-05-10 17:03:30 -0700

    adds inverse price impact and makes fn op name clear

--- pkg/artemis/web3_client/uniswap_pricing.go
+++ pkg/artemis/web3_client/uniswap_pricing.go
@@ -7,7 +7,7 @@ import (
 
 const uniswapPriceFeeConstant = 0.3 / 100
 
-func (p *UniswapV2Pair) PriceImpact(tokenOneBuyAmount *big.Int) (*big.Float, *big.Float) {
+func (p *UniswapV2Pair) PriceImpactToken1BuyToken0(tokenOneBuyAmount *big.Int) (*big.Float, *big.Float) {
 	// From example: 3 Token A
 	fmt.Println("tokenOneBuyAmount", tokenOneBuyAmount.String())
 	tokenOneAmountFloat := new(big.Float).SetInt(tokenOneBuyAmount)
@@ -42,6 +42,52 @@ func (p *UniswapV2Pair) PriceImpact(tokenOneBuyAmount *big.Int) (*big.Float, *bi
 	p.Reserve0.Add(p.Reserve0, feeTokenZeroInt)
 	tokenZeroPurchaseAmount, _ := tokenZeroReturned.Int(nil)
 	p.Reserve0.Sub(p.Reserve0, tokenZeroPurchaseAmount)
+
+	// From example: 1200 Token A + 3 Token A + 0.009 Token A = 1203.009 Token A
+	fmt.Println("reserve0", p.Reserve0.String())
+	// From example: 400 Token B - 1 Token B + 0.003 Token B = 399.003 Token B
+	fmt.Println("reserve1", p.Reserve1.String())
+	// Calculate new price
+	newPriceToken1, _ := p.GetToken1Price()
+	newPriceToken0, _ := p.GetToken0Price()
+	return newPriceToken1, newPriceToken0
+}
+
+func (p *UniswapV2Pair) PriceImpactToken0BuyToken1(tokenZeroBuyAmount *big.Int) (*big.Float, *big.Float) {
+	// From example: 3 Token A
+	fmt.Println("tokenZeroBuyAmount", tokenZeroBuyAmount.String())
+	tokenZeroAmountFloat := new(big.Float).SetInt(tokenZeroBuyAmount)
+	// From example: 3 Token A * 0.3% fee
+	feeTokenZero := new(big.Float).Mul(tokenZeroAmountFloat, big.NewFloat(uniswapPriceFeeConstant))
+	// From example: 3 Token A * 0.3% fee = 0.009 Token A
+	fmt.Println("feeTokenZero", feeTokenZero.String())
+	// From example: 1200 Token A / 400 Token B = 3
+	priceToken1, err := p.GetToken1Price()
+	if err != nil {
+		return nil, nil
+	}
+	fmt.Println("price token A per token B", priceToken1.String())
+	priceToken0, err := p.GetToken0Price()
+	if err != nil {
+		return nil, nil
+	}
+	fmt.Println("price token B per token A", priceToken0.String())
+	tokenOneReturned := new(big.Float).Mul(tokenZeroAmountFloat, priceToken0)
+
+	// From example: 3 Token A * (1 Token B / 3 Token A) = 1 Token B
+	fmt.Println("tokenOneReturnedBeforeFee", tokenOneReturned.String())
+	feeTokenOne := new(big.Float).Mul(tokenOneReturned, big.NewFloat(uniswapPriceFeeConstant))
+	// From example: 1 Token B * 0.3% fee = 0.003 Token B
+	fmt.Println("feeTokenOne", feeTokenOne.String())
+	// Update reserves
+	tokenZeroFeeInt, _ := feeTokenZero.Int(nil)
+	p.Reserve0.Add(p.Reserve0, tokenZeroFeeInt)
+	p.Reserve0.Add(p.Reserve0, tokenZeroBuyAmount)
+
+	feeTokenOneInt, _ := feeTokenOne.Int(nil)
+	p.Reserve1.Add(p.Reserve1, feeTokenOneInt)
+	tokenOnePurchaseAmount, _ := tokenOneReturned.Int(nil)
+	p.Reserve1.Sub(p.Reserve1, tokenOnePurchaseAmount)
 
 	// From example: 1200 Token A + 3 Token A + 0.009 Token A = 1203.009 Token A
 	fmt.Println("reserve0", p.Reserve0.String())
--- pkg/artemis/web3_client/uniswap_pricing_test.go
+++ pkg/artemis/web3_client/uniswap_pricing_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func (s *Web3ClientTestSuite) TestPricingImpact() {
-	reserve0, _ := new(big.Int).SetString("400000", 10)
-	reserve1, _ := new(big.Int).SetString("1200000", 10)
+	reserve0, _ := new(big.Int).SetString("400000", 10)  // TokenB
+	reserve1, _ := new(big.Int).SetString("1200000", 10) // TokenA
 	token0Addr, token1Addr := StringsToAddresses(PepeContractAddr, WETH9ContractAddress)
 	mockPairResp := UniswapV2Pair{
 		KLast:    big.NewInt(0),
@@ -17,9 +17,25 @@ func (s *Web3ClientTestSuite) TestPricingImpact() {
 		Reserve1: reserve1,
 	}
 	originalRate, _ := mockPairResp.GetToken1Price()
-	newRateToken1, newRateToken0 := mockPairResp.PriceImpact(big.NewInt(3000))
+	newRateToken1, newRateToken0 := mockPairResp.PriceImpactToken1BuyToken0(big.NewInt(3000))
 	fmt.Println("originalRate", originalRate)
 	fmt.Println("newRateToken0", newRateToken0)
 	fmt.Println("newRateToken1", newRateToken1)
 	s.Assert().Equal("3.015037481", newRateToken0.String())
+
+	reserve0, _ = new(big.Int).SetString("400000", 10)  // TokenB
+	reserve1, _ = new(big.Int).SetString("1200000", 10) // TokenA
+	mockPairResp = UniswapV2Pair{
+		KLast:    big.NewInt(0),
+		Token0:   token0Addr,
+		Token1:   token1Addr,
+		Reserve0: reserve0,
+		Reserve1: reserve1,
+	}
+	originalRate, _ = mockPairResp.GetToken1Price()
+	newRateToken1, newRateToken0 = mockPairResp.PriceImpactToken0BuyToken1(big.NewInt(1000))
+	fmt.Println("originalRate", originalRate)
+	fmt.Println("newRateToken0", newRateToken0)
+	fmt.Println("newRateToken1", newRateToken1)
+	s.Assert().Equal("2.985037518", newRateToken0.String())
 }

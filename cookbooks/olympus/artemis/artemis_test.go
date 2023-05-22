ded4f4de36c9d4ac106243eae5df0d9761f7eed2
alex <alexg89@vt.edu>
2023-05-21 19:48:43 -0700

    use v2 cluster type for artemis

--- cookbooks/olympus/artemis/artemis_test.go
+++ cookbooks/olympus/artemis/artemis_test.go
@@ -1,17 +1,14 @@
 package artemis_cookbook
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/zeus-fyi/olympus/cookbooks"
-	"github.com/zeus-fyi/olympus/datastores/postgres/apps/zeus/models/bases/topologies/definitions/kns"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_base"
-	zeus_client "github.com/zeus-fyi/olympus/pkg/zeus/client"
-	"github.com/zeus-fyi/olympus/pkg/zeus/client/zeus_req_types"
-	"github.com/zeus-fyi/olympus/pkg/zeus/client/zeus_req_types/internal_reqs"
 	api_configs "github.com/zeus-fyi/olympus/test/configs"
-	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_common_types"
+	zeus_client "github.com/zeus-fyi/zeus/pkg/zeus/client"
 )
 
 type ArtemisCookbookTestSuite struct {
@@ -30,53 +27,25 @@ func (t *ArtemisCookbookTestSuite) SetupTest() {
 }
 
 func (t *ArtemisCookbookTestSuite) TestDeploy() {
-	resp, err := t.ZeusTestClient.Deploy(ctx, ArtemisDeployKnsReq)
+	olympus_cookbooks.ChangeToCookbookDir()
+	cdep := ArtemisClusterDefinition.GenerateDeploymentRequest()
+	_, err := t.ZeusTestClient.DeployCluster(ctx, cdep)
 	t.Require().Nil(err)
-	t.Assert().NotEmpty(resp)
-	t.TestArtemisSecretsCopy()
 }
 
 func (t *ArtemisCookbookTestSuite) TestUploadCharts() {
-	resp, err := t.ZeusTestClient.UploadChart(ctx, artemisChartPath, uploadChart)
-	t.Require().Nil(err)
-	t.Assert().NotZero(resp.TopologyID)
-	ArtemisDeployKnsReq.TopologyID = resp.TopologyID
-	tar := zeus_req_types.TopologyRequest{TopologyID: ArtemisDeployKnsReq.TopologyID}
-	chartResp, err := t.ZeusTestClient.ReadChart(ctx, tar)
-	t.Require().Nil(err)
-	t.Assert().NotEmpty(chartResp)
-	err = chartResp.PrintWorkload(artemisChartPath)
+	cd := ArtemisClusterDefinition
+	_, err := cd.UploadChartsFromClusterDefinition(ctx, t.ZeusTestClient, true)
 	t.Require().Nil(err)
 }
 
-func (t *ArtemisCookbookTestSuite) TestArtemisSecretsCopy() {
-	s1 := "spaces-auth"
-	s2 := "spaces-key"
-	s3 := "age-auth"
-	req := internal_reqs.InternalSecretsCopyFromTo{
-		SecretNames: []string{s1, s2, s3},
-		FromKns: kns.TopologyKubeCtxNs{
-			TopologyID: 0,
-			CloudCtxNs: zeus_common_types.CloudCtxNs{
-				CloudProvider: "do",
-				Region:        "sfo3",
-				Context:       "do-sfo3-dev-do-sfo3-zeus",
-				Namespace:     "zeus",
-				Env:           "dev",
-			},
-		},
-		ToKns: kns.TopologyKubeCtxNs{
-			TopologyID: 0,
-			CloudCtxNs: zeus_common_types.CloudCtxNs{
-				CloudProvider: "do",
-				Region:        "sfo3",
-				Context:       "do-sfo3-dev-do-sfo3-zeus",
-				Namespace:     "artemis",
-				Env:           "dev",
-			},
-		},
-	}
-	err := t.ZeusTestClient.CopySecretsFromToNamespace(ctx, req)
+func (t *ArtemisCookbookTestSuite) TestCreateClusterClass() {
+	cd := ArtemisClusterDefinition
+	gcd := cd.BuildClusterDefinitions()
+	t.Assert().NotEmpty(gcd)
+	fmt.Println(gcd)
+
+	err := gcd.CreateClusterClassDefinitions(ctx, t.ZeusTestClient)
 	t.Require().Nil(err)
 }
 

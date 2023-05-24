1719b5f6c124d46a79f7689286c5c9fbd4b0567b
alex <alexg89@vt.edu>
2023-05-23 22:13:45 -0700

    matches tradein

--- pkg/artemis/web3_client/trade_exec_test.go
+++ pkg/artemis/web3_client/trade_exec_test.go
@@ -48,3 +48,36 @@ func (s *Web3ClientTestSuite) TestTradeExec() {
 		s.Assert().Equal(tfRegular.FrontRunTrade.AmountIn.String(), b.String())
 	}
 }
+
+func (s *Web3ClientTestSuite) TestMatchInputs() {
+	//forceDirToLocation()
+	//swapAbi, bc, err := LoadSwapAbiPayload()
+	//s.Require().NoError(err)
+	//s.Require().NotNil(swapAbi)
+	//s.Require().NotNil(bc)
+	apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+	ForceDirToTestDirLocation()
+	//uni := InitUniswapV2Client(ctx, s.LocalHardhatMainnetUser)
+	mevTxs, merr := artemis_validator_service_groups_models.SelectMempoolTxAtBlockNumber(ctx, hestia_req_types.EthereumMainnetProtocolNetworkID, 17326677)
+	s.Require().Nil(merr)
+	s.Require().NotEmpty(mevTxs)
+
+	for _, mevTx := range mevTxs {
+		tf := TradeExecutionFlow{}
+		by := []byte(mevTx.TxFlowPrediction)
+		berr := json.Unmarshal(by, &tf)
+		s.Require().Nil(berr)
+
+		if tf.FrontRunTrade.AmountIn == "" {
+			continue
+		}
+
+		tfRegular := tf.ConvertToBigIntType()
+		err := s.LocalHardhatMainnetUser.MatchFrontRunTradeValues(tfRegular)
+		s.Require().Nil(err)
+		b, err := s.LocalHardhatMainnetUser.ReadERC20TokenBalance(ctx, tf.FrontRunTrade.AmountInAddr.String(), s.LocalHardhatMainnetUser.PublicKey())
+		s.Require().Nil(err)
+		s.Require().Equal(tfRegular.FrontRunTrade.AmountIn.String(), b.String())
+		fmt.Println(b.String(), tfRegular.FrontRunTrade.AmountIn.String())
+	}
+}

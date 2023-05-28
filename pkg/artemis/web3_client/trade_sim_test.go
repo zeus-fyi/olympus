8445e14e35b957fdeb0d70ea7cd7c8ca2535d0d1
alex <alexg89@vt.edu>
2023-05-27 18:25:31 -0700

    wip

--- pkg/artemis/web3_client/trade_sim_test.go
+++ pkg/artemis/web3_client/trade_sim_test.go
@@ -2,6 +2,7 @@ package web3_client
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	artemis_validator_service_groups_models "github.com/zeus-fyi/olympus/datastores/postgres/apps/artemis/models"
@@ -32,3 +33,39 @@ func (s *Web3ClientTestSuite) TestFullSandwichTradeSim() {
 		s.Require().Nil(err)
 	}
 }
+
+func (s *Web3ClientTestSuite) TestFullSandwichTradeSimAny() {
+	apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+	ForceDirToTestDirLocation()
+	mevTxs, merr := artemis_validator_service_groups_models.SelectMempoolTxAtMaxBlockNumber(ctx, hestia_req_types.EthereumMainnetProtocolNetworkID)
+	s.Require().Nil(merr)
+	s.Require().NotEmpty(mevTxs)
+
+	for _, mevTx := range mevTxs {
+		tf := TradeExecutionFlow{}
+		by := []byte(mevTx.TxFlowPrediction)
+		berr := json.Unmarshal(by, &tf)
+		s.Require().Nil(berr)
+		if tf.FrontRunTrade.AmountIn == "" {
+			continue
+		}
+		fmt.Println("blockNum", tf.CurrentBlockNumber)
+		fmt.Println("tradeMethod", tf.Trade.TradeMethod)
+		fmt.Println("txHash", tf.Tx.Hash())
+
+		err := s.LocalHardhatMainnetUser.HardhatResetNetworkToBlockBeforeTxMined(ctx, s.Tc.HardhatNode, s.LocalHardhatMainnetUser, s.MainnetWeb3User, tf.Tx.Hash())
+		s.Require().Nil(err)
+		tfRegular := tf.ConvertToBigIntType()
+		uni := InitUniswapV2Client(ctx, s.LocalHardhatMainnetUser)
+		uni.DebugPrint = true
+		err = uni.SimFullSandwichTrade(&tfRegular)
+		s.Require().Nil(err)
+	}
+}
+
+/*
+blockNum 17354228
+TRANSFER_FAILED
+tradeMethod swapTokensForExactTokens
+txHash 0x5f29de88cd07de0582923590cdaf77dcc35c73d458549e6ec103f8e5e80b06ed
+*/

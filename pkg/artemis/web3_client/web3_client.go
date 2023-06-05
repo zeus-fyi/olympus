f4e0ab4320510dbd239c9c45a7ade7cf502cde1e
alex <alexg89@vt.edu>
2023-06-05 13:37:23 -0700

    adds relay header

--- pkg/artemis/web3_client/web3_client.go
+++ pkg/artemis/web3_client/web3_client.go
@@ -19,6 +19,11 @@ func NewWeb3ClientFakeSigner(nodeUrl string) Web3Client {
 	return Web3Client{w}
 }
 
+func NewWeb3ClientWithRelay(nodeUrl, relayUrl string, acc *accounts.Account) Web3Client {
+	w := web3_actions.NewWeb3ActionsClientWithRelay(nodeUrl, relayUrl, acc)
+	return Web3Client{w}
+}
+
 func NewWeb3Client(nodeUrl string, acc *accounts.Account) Web3Client {
 	w := web3_actions.NewWeb3ActionsClientWithAccount(nodeUrl, acc)
 	return Web3Client{w}

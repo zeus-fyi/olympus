71c70e7545b98fa28a088d9fd9a7741d71ae5200
alex <alexg89@vt.edu>
2023-06-06 11:51:13 -0700

    adds jobs to chart workload deploy

--- pkg/zeus/topologies/orchestrations/workers/topology/worker.go
+++ pkg/zeus/topologies/orchestrations/workers/topology/worker.go
@@ -37,6 +37,38 @@ func (t *TopologyWorker) ExecuteDeployFleetUpgrade(ctx context.Context, params b
 	return err
 }
 
+func (t *TopologyWorker) ExecuteDeployCronJob(ctx context.Context, params base_deploy_params.TopologyWorkflowRequest) error {
+	c := t.ConnectTemporalClient()
+	defer c.Close()
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: t.TaskQueueName,
+	}
+	deployWf := deploy_workflow.NewDeployTopologyWorkflow()
+	wf := deployWf.DeployCronJobWorkflow
+	_, err := c.ExecuteWorkflow(ctx, workflowOptions, wf, params)
+	if err != nil {
+		log.Err(err).Msg("DeployCronJobWorkflow")
+		return err
+	}
+	return err
+}
+
+func (t *TopologyWorker) ExecuteDeployJob(ctx context.Context, params base_deploy_params.TopologyWorkflowRequest) error {
+	c := t.ConnectTemporalClient()
+	defer c.Close()
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: t.TaskQueueName,
+	}
+	deployWf := deploy_workflow.NewDeployTopologyWorkflow()
+	wf := deployWf.DeployJobWorkflow
+	_, err := c.ExecuteWorkflow(ctx, workflowOptions, wf, params)
+	if err != nil {
+		log.Err(err).Msg("ExecuteDeployJob")
+		return err
+	}
+	return err
+}
+
 func (t *TopologyWorker) ExecuteDeployCluster(ctx context.Context, params base_deploy_params.ClusterTopologyWorkflowRequest) error {
 	c := t.ConnectTemporalClient()
 	defer c.Close()

fc9bd819f7f6b05a9d5b9edecd2efa8af1e6498d
alex <alexg89@vt.edu>
2023-06-06 14:26:52 -0700

    refactor decode args fn

--- pkg/artemis/web3_client/mev_mempool_processing.go
+++ pkg/artemis/web3_client/mev_mempool_processing.go
@@ -2,13 +2,61 @@ package web3_client
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/rs/zerolog/log"
 	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
 )
+
+func DecodeTxArgData(ctx context.Context, tx *types.Transaction, abiFile *abi.ABI, methodFilter *strings_filter.FilterOpts) (string, map[string]interface{}, error) {
+	if tx.Data() == nil {
+		return "", nil, errors.New("tx data is nil")
+	}
+	input := tx.Data()
+	calldata := input
+	if len(calldata) < 4 {
+		log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs invalid calldata length")
+		return "", nil, errors.New("invalid calldata length")
+	}
+	sigdata := calldata[:4]
+	if abiFile == nil {
+		log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Abi Invalid")
+		return "", nil, errors.New("abi invalid")
+	}
+	method, merr := abiFile.MethodById(sigdata[:4])
+	if merr != nil {
+		log.Info().Err(merr).Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
+		return "", nil, errors.New("abi method invalid")
+	}
+	if method == nil {
+		log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
+		return "", nil, errors.New("abi method invalid")
+	}
+	if method.Name == "" {
+		log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
+		return "", nil, errors.New("abi method name empty")
+	}
+	if !strings_filter.FilterStringWithOpts(method.Name, methodFilter) {
+		//log.Debug().Msg("Web3Client| GetFilteredPendingMempoolTxs Method Filtered")
+		return "", nil, errors.New("no methods left after filtering")
+	}
+	argdata := calldata[4:]
+	if len(argdata)%32 != 0 || len(argdata) == 0 {
+		//log.Info().Msg("Web3Client| GetFilteredPendingMempoolTxs invalid argdata length")
+		return "", nil, errors.New("invalid argdata length")
+	}
+	m := make(map[string]interface{})
+	err := method.Inputs.UnpackIntoMap(m, argdata)
+	if err != nil {
+		log.Info().Err(err).Msg("Web3Client| UnpackIntoMap invalid")
+		return "", nil, errors.New("unpack into map invalid")
+	}
+	return method.Name, m, nil
+}
 
 func ProcessMempoolTxs(ctx context.Context, mempool map[string]map[string]*types.Transaction, mevTxMap MevSmartContractTxMap) (MevSmartContractTxMap, error) {
 	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
@@ -23,54 +71,19 @@ func ProcessMempoolTxs(ctx context.Context, mempool map[string]map[string]*types
 		for order, tx := range txPoolQueue {
 			if tx.To() != nil && tx.To().String() == smartContractAddrFilterString {
 				if tx.Data() != nil {
-					input := tx.Data()
-					calldata := input
-					if len(calldata) < 4 {
-						log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs invalid calldata length")
-						continue
-					}
-					sigdata := calldata[:4]
-					if mevTxMap.Abi == nil {
-						log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Abi Invalid")
-						continue
-					}
-					method, merr := mevTxMap.Abi.MethodById(sigdata[:4])
-					if merr != nil {
-						log.Info().Err(merr).Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
-						continue
-					}
-					if method == nil {
-						log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
-						continue
-					}
-					if method.Name == "" {
-						log.Info().Interface("method", "unknown").Msg("Web3Client| GetFilteredPendingMempoolTxs Method Invalid")
-						continue
-					}
-					if !strings_filter.FilterStringWithOpts(method.Name, mevTxMap.Filter) {
-						//log.Debug().Msg("Web3Client| GetFilteredPendingMempoolTxs Method Filtered")
-						continue
-					}
-					argdata := calldata[4:]
-					if len(argdata)%32 != 0 || len(argdata) == 0 {
-						//log.Info().Msg("Web3Client| GetFilteredPendingMempoolTxs invalid argdata length")
-						continue
-					}
-					m := make(map[string]interface{})
-					err := method.Inputs.UnpackIntoMap(m, argdata)
+					methodName, args, err := DecodeTxArgData(ctx, tx, mevTxMap.Abi, mevTxMap.Filter)
 					if err != nil {
-						log.Info().Err(err).Msg("Web3Client| UnpackIntoMap invalid")
 						continue
 					}
 					singleTx := MevTx{
 						UserAddr:    userAddr,
-						Args:        m,
+						Args:        args,
 						Order:       order,
 						TxPoolQueue: txPoolQueue,
 						Tx:          tx,
 					}
 					mevTxs = append(mevTxs, singleTx)
-					mevTxMap.MethodTxMap[method.Name] = singleTx
+					mevTxMap.MethodTxMap[methodName] = singleTx
 				}
 			}
 		}
--- pkg/artemis/web3_client/uniswap_universal_router_decoding_test.go
+++ pkg/artemis/web3_client/uniswap_universal_router_decoding_test.go
@@ -1,1 +1,30 @@
 package web3_client
+
+import (
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
+	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
+)
+
+func (s *Web3ClientTestSuite) TestUniswapUniversalRouterDecoding() {
+	// needs to get txs sent to UR
+	// can lookup a tx hash on etherscan and get the input data
+	apps.Pg.InitPG(ctx, s.Tc.LocalDbPgconn)
+	ForceDirToTestDirLocation()
+	uni := InitUniswapClient(ctx, s.MainnetWeb3User)
+	uni.PrintOn = true
+	uni.PrintLocal = true
+	uni.Path = filepaths.Path{
+		PackageName: "",
+		DirIn:       "",
+		DirOut:      "./trade_analysis",
+		FnIn:        "",
+		FnOut:       "",
+		Env:         "",
+	}
+	tx := &types.Transaction{}
+	mn, args, err := DecodeTxArgData(ctx, tx, uni.UniversalRouterAbi, nil)
+	s.Require().Nil(err)
+	s.Require().NotEmpty(mn)
+	s.Require().NotEmpty(args)
+}

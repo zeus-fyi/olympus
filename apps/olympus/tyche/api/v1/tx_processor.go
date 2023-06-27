fbac320da460ae9d561dbb5c45cdc4b8ff7a6af7
alex <alexg89@vt.edu>
2023-06-27 15:03:16 -0700

    adds tx processor handler wip

--- apps/olympus/tyche/api/v1/tx_processor.go
+++ apps/olympus/tyche/api/v1/tx_processor.go
@@ -1,19 +1,19 @@
 package v1_tyche
 
 import (
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 	artemis_realtime_trading "github.com/zeus-fyi/olympus/pkg/artemis/trading"
 )
 
 type TxProcessingRequest struct {
-	Body echo.Map
+	Txs []*types.Transaction
 }
 
 func TxProcessingRequestHandler(c echo.Context) error {
 	request := new(TxProcessingRequest)
-	request.Body = echo.Map{}
-	if err := c.Bind(&request.Body); err != nil {
+	if err := c.Bind(&request); err != nil {
 		log.Err(err)
 		return err
 	}
@@ -22,6 +22,8 @@ func TxProcessingRequestHandler(c echo.Context) error {
 
 func (t *TxProcessingRequest) ProcessTx(c echo.Context) error {
 	a := artemis_realtime_trading.ActiveTrading{}
+
+	// should process in parallel
 	a.ProcessTx(c.Request().Context(), nil)
 	return nil
 }

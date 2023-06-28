5d16399e0a285b1eb3f89ea2eff4041695ddad3d
alex <alexg89@vt.edu>
2023-06-27 17:09:33 -0700

    add metrics route

--- apps/olympus/tyche/api/v1/router.go
+++ apps/olympus/tyche/api/v1/router.go
@@ -4,27 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	tyche_metrics "github.com/zeus-fyi/olympus/tyche/metrics"
 )
 
 func Routes(e *echo.Echo) *echo.Echo {
 	e.GET("/health", Health)
-	e.POST("/mev/incoming/tx", TxProcessingRequestHandler)
+	e.POST("/v1/mev/mempool/tx", TxProcessingRequestHandler)
+	e.GET("/metrics", tyche_metrics.MetricsRequestHandler)
 	return e
 }
 
 func Health(c echo.Context) error {
 	return c.String(http.StatusOK, "Healthy")
 }
-
-/*
-
-needs an endpoint to kickoff active trade analysis
-
-peers -> get total peer count
-
-decoder
-  -> get tx to address
-  -> get starting token/end token
-  -> get trade method
-
-*/

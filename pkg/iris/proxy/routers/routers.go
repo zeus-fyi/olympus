8fb2c00ac4abd319c597b8f5d4a1f6856d704f4f
alex <alexg89@vt.edu>
2023-07-02 18:18:01 -0700

    adds proxy wip

---
+++ pkg/iris/proxy/routers/routers.go
@@ -0,0 +1,30 @@
+package iris_routers
+
+/*
+
+TODO: Implement the following:
+
+Define the following:
+- type Router struct {}
+- func NewRouter() Router
+
+1. Types of routing logic:
+
+- Lock session
+- Durable session
+- Sub/Pub
+- Request/Response
+- Retry logic
+- Routing Rule Settings
+	- eg. time to live
+
+2. how to store routing logic
+	 - postgres schema
+
+3. how to query routing logic
+
+4. how to update routing logic
+
+5. how to delete routing logic
+
+*/

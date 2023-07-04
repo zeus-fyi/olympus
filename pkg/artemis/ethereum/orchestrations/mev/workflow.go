059f0555775f2838be51ede82495a032aadde90b
alex <alexg89@vt.edu>
2023-07-04 16:04:10 -0700

    update to use active trade module to record mempool

--- pkg/artemis/ethereum/orchestrations/mev/workflow.go
+++ pkg/artemis/ethereum/orchestrations/mev/workflow.go
@@ -28,7 +28,7 @@ func NewArtemisMevWorkflow() ArtemisMevWorkflow {
 
 func (t *ArtemisMevWorkflow) GetWorkflows() []interface{} {
 	return []interface{}{t.ArtemisSendEthTxWorkflow, t.ArtemisSendSignedTxWorkflow, t.ArtemisMevWorkflow, t.ArtemisTxBlacklistWorkflow,
-		t.ArtemisRemoveProcessedTxsWorkflow, t.ArtemisHistoricalSimTxWorkflow, t.BlacklistProcessedTxs}
+		t.ArtemisRemoveProcessedTxsWorkflow, t.ArtemisHistoricalSimTxWorkflow, t.ArtemisTxBlacklistWorkflow}
 }
 
 func (t *ArtemisMevWorkflow) ArtemisSendEthTxWorkflow(ctx workflow.Context, params web3_actions.SendEtherPayload) error {

f74fcd7c4a66e3c5b444f6b8b56a2bd2aaad0773
alex <alexg89@vt.edu>
2023-07-04 18:14:29 -0700

    adds metrics to reg

--- pkg/artemis/trading/active_trading_analysis.go
+++ pkg/artemis/trading/active_trading_analysis.go
@@ -57,40 +57,36 @@ func (a *ActiveTrading) ProcessTxs(ctx context.Context) ([]*web3_client.TradeExe
 	}
 
 	for _, tf := range tfSlice {
-		tmTradingEnabled := artemis_trading_cache.TokenMap[tf.UserTrade.AmountInAddr.String()].TradingEnabled
-		if tmTradingEnabled == nil {
-			tradeEnabled := false
-			log.Info().Msgf("ActiveTrading: EntryTxFilter, erc20 at address %s not registered", tf.UserTrade.AmountInAddr.String())
-			chainId := tf.Tx.ChainId().Int64()
-			err := artemis_validator_service_groups_models.InsertERC20TokenInfo(ctx, artemis_autogen_bases.Erc20TokenInfo{
-				Address:           tf.UserTrade.AmountInAddr.String(),
-				ProtocolNetworkID: int(chainId),
-				BalanceOfSlotNum:  -2, // -1 means balanceOf it wasn't cracked within 100 slots, -2 means cracking hasn't been attempted yet
-				TradingEnabled:    &tradeEnabled,
-			})
-			if err != nil {
-				log.Err(err).Msg("ActiveTrading: EntryTxFilter, InsertERC20TokenInfo")
-				return nil, errors.New("ActiveTrading: EntryTxFilter, erc20 at address %s not registered")
-			}
-			return nil, errors.New("ActiveTrading: EntryTxFilter, erc20 at address %s not registered")
+		chainID := tf.Tx.ChainId()
+		err := CheckTokenRegistry(ctx, tf.UserTrade.AmountInAddr.String(), chainID.Int64())
+		if err != nil {
+			log.Err(err).Msg("ActiveTrading: EntryTxFilter, CheckTokenRegistry")
+			return nil, err
 		}
-		tmTradingEnabled = artemis_trading_cache.TokenMap[tf.UserTrade.AmountOutAddr.String()].TradingEnabled
-		if tmTradingEnabled == nil {
-			tradeEnabled := false
-			log.Info().Msgf("ActiveTrading: EntryTxFilter, erc20 at address %s not registered", tf.UserTrade.AmountInAddr.String())
-			chainId := tf.Tx.ChainId().Int64()
-			err := artemis_validator_service_groups_models.InsertERC20TokenInfo(ctx, artemis_autogen_bases.Erc20TokenInfo{
-				Address:           tf.UserTrade.AmountInAddr.String(),
-				ProtocolNetworkID: int(chainId),
-				BalanceOfSlotNum:  -2, // -1 means balanceOf it wasn't cracked within 100 slots, -2 means cracking hasn't been attempted yet
-				TradingEnabled:    &tradeEnabled,
-			})
-			if err != nil {
-				log.Err(err).Msg("ActiveTrading: EntryTxFilter, InsertERC20TokenInfo")
-				return nil, errors.New("ActiveTrading: EntryTxFilter, erc20 at address %s not registered")
-			}
-			return nil, errors.New("ActiveTrading: EntryTxFilter, erc20 at address %s not registered")
+		err = CheckTokenRegistry(ctx, tf.UserTrade.AmountOutAddr.String(), chainID.Int64())
+		if err != nil {
+			log.Err(err).Msg("ActiveTrading: EntryTxFilter, CheckTokenRegistry")
+			return nil, err
 		}
 	}
 	return tfSlice, nil
+}
+
+func CheckTokenRegistry(ctx context.Context, tokenAddress string, chainID int64) error {
+	tmTradingEnabled := artemis_trading_cache.TokenMap[tokenAddress].TradingEnabled
+	if tmTradingEnabled == nil {
+		tradeEnabled := false
+		log.Info().Msgf("ActiveTrading: EntryTxFilter, erc20 at address %s not registered", tokenAddress)
+		err := artemis_validator_service_groups_models.InsertERC20TokenInfo(ctx, artemis_autogen_bases.Erc20TokenInfo{
+			Address:           tokenAddress,
+			ProtocolNetworkID: int(chainID),
+			BalanceOfSlotNum:  -2, // -1 means balanceOf it wasn't cracked within 100 slots, -2 means cracking hasn't been attempted yet
+			TradingEnabled:    &tradeEnabled,
+		})
+		if err != nil {
+			log.Err(err).Msg("ActiveTrading: EntryTxFilter, InsertERC20TokenInfo")
+			return errors.New("ActiveTrading: EntryTxFilter, erc20 at address %s not registered")
+		}
+	}
+	return nil
 }

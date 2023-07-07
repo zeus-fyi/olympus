4e400056557333550b75f0e45fc1a9683206e009
alex <alexg89@vt.edu>
2023-07-06 21:50:05 -0700

    adds risk wip & bundle wip

--- pkg/artemis/flashbots/flashbots_bundle.go
+++ pkg/artemis/flashbots/flashbots_bundle.go
@@ -7,6 +7,16 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+/*
+TODO: implement setters for the following fields:
+
+	Txs          []string  `json:"txs"`                         // Array[String], A list of signed transactions to execute in an atomic bundle
+	BlockNumber  string    `json:"blockNumber"`                 // String, a hex encoded block number for which this bundle is valid on
+	MinTimestamp *uint64   `json:"minTimestamp,omitempty"`      // (Optional) Number, the minimum timestamp for which this bundle is valid, in seconds since the unix epoch
+	MaxTimestamp *uint64   `json:"maxTimestamp,omitempty"`      // (Optional) Number, the maximum timestamp for which this bundle is valid, in seconds since the unix epoch
+	RevertingTxs *[]string `json:"revertingTxHashes,omitempty"` // (Optional) Array[String], A list of tx hashes that are allowed to revert
+*/
+
 func (f *FlashbotsClient) SendBundle(ctx context.Context, bundle flashbotsrpc.FlashbotsSendBundleRequest) (flashbotsrpc.FlashbotsSendBundleResponse, error) {
 	resp, err := f.FlashbotsRPC.FlashbotsSendBundle(f.EcdsaPrivateKey(), bundle)
 	if err != nil {
--- pkg/artemis/flashbots/flashbots_bundle_test.go
+++ pkg/artemis/flashbots/flashbots_bundle_test.go
@@ -3,10 +3,13 @@ package artemis_flashbots
 import "github.com/metachris/flashbotsrpc"
 
 func (s *FlashbotsTestSuite) TestGetFlashbotsBundleSubmission() {
-	bundle := flashbotsrpc.FlashbotsGetBundleStatsParam{
-		BlockNumber: "0x10C063C", // 17565244
-		BundleHash:  "0x9f93055488f7b9db678c14c1c5056c3ea01ef91e35c4f5e4cbeb6d8eb434f32d",
+	bundle := flashbotsrpc.FlashbotsSendBundleRequest{
+		Txs:          []string{"0x1", "0x2", "0x3"},
+		BlockNumber:  "0x10C063C", // 17565244
+		MinTimestamp: nil,
+		MaxTimestamp: nil,
+		RevertingTxs: nil,
 	}
-	_, err := s.fb.FlashbotsGetBundleStatsV2(s.fb.EcdsaPrivateKey(), bundle)
+	_, err := s.fb.SendBundle(ctx, bundle)
 	s.Assert().Nil(err)
 }

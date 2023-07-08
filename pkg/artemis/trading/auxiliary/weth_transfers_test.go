0062095d91c75f9b7c9a6058d9b13b5c948262ba
alex <alexg89@vt.edu>
2023-07-07 17:34:53 -0700

    adds testcase

--- pkg/artemis/trading/auxiliary/weth_transfers_test.go
+++ pkg/artemis/trading/auxiliary/weth_transfers_test.go
@@ -1,7 +1,9 @@
 package artemis_trading_auxiliary
 
 import (
+	artemis_trading_constants "github.com/zeus-fyi/olympus/pkg/artemis/trading/lib/constants"
 	artemis_eth_units "github.com/zeus-fyi/olympus/pkg/artemis/trading/lib/units"
+	"github.com/zeus-fyi/olympus/pkg/artemis/web3_client"
 	hestia_req_types "github.com/zeus-fyi/zeus/pkg/hestia/client/req_types"
 )
 
@@ -9,7 +11,20 @@ func (t *ArtemisAuxillaryTestSuite) TestWETH() {
 	ta := InitAuxiliaryTradingUtils(ctx, t.goerliNode, hestia_req_types.Goerli, t.acc)
 	t.Require().NotEmpty(ta)
 
-	cmd, err := ta.GenerateCmdToExchangeETHtoWETH(ctx, nil, artemis_eth_units.EtherMultiple(1), nil)
+	toExchAmount := artemis_eth_units.EtherMultiple(1)
+	cmd, err := ta.GenerateCmdToExchangeETHtoWETH(ctx, nil, toExchAmount, nil)
 	t.Require().Nil(err)
 	t.Require().NotEmpty(cmd)
+
+	found := false
+	for i, sc := range cmd.Commands {
+		if i == 0 && sc.Command == artemis_trading_constants.WrapETH {
+			found = true
+			t.Require().NotNil(cmd.Payable)
+			t.Require().Equal(toExchAmount.String(), cmd.Payable.Amount.String())
+			t.Require().Equal(toExchAmount.String(), sc.DecodedInputs.(web3_client.WrapETHParams).AmountMin.String())
+			t.Require().Equal(ta.Address().String(), sc.DecodedInputs.(web3_client.WrapETHParams).Recipient.String())
+		}
+	}
+	t.Require().True(found)
 }

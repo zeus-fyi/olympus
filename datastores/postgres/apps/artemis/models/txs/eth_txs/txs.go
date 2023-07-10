326aff6321ee2e8b315f7b20a2baf776a4a56948
alex <alexg89@vt.edu>
2023-07-09 20:33:34 -0700

    adds eth tx

--- datastores/postgres/apps/artemis/models/txs/eth_txs/txs.go
+++ datastores/postgres/apps/artemis/models/txs/eth_txs/txs.go
@@ -1,9 +1,79 @@
 package artemis_eth_txs
 
 import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	artemis_autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/artemis/models/bases/autogen"
+	"github.com/zeus-fyi/olympus/pkg/utils/chronos"
+	"github.com/zeus-fyi/olympus/pkg/utils/misc"
+	"github.com/zeus-fyi/olympus/pkg/utils/string_utils/sql_query_templates"
+	hestia_req_types "github.com/zeus-fyi/zeus/pkg/hestia/client/req_types"
 )
+
+/*
+type EthTx struct {
+	ProtocolNetworkID int    `db:"protocol_network_id" json:"protocolNetworkID"`
+	TxHash            string `db:"tx_hash" json:"txHash"`
+	Nonce             int    `db:"nonce" json:"nonce"`
+	From              string `db:"from" json:"from"`
+	Type              string `db:"type" json:"type"`
+	EventID           int    `db:"event_id" json:"eventID"`
+}
+
+type EthTxGas struct {
+	TxHash    string        `db:"tx_hash" json:"txHash"`
+	GasPrice  sql.NullInt64 `db:"gasPrice" json:"gasPrice"`
+	GasLimit  sql.NullInt64 `db:"gasLimit" json:"gasLimit"`
+	GasTipCap sql.NullInt64 `db:"gasTipCap" json:"gasTipCap"`
+	GasFeeCap sql.NullInt64 `db:"gasFeeCap" json:"gasFeeCap"`
+}
+
+
+type Permit2Tx struct {
+	Nonce    int    `db:"nonce" json:"nonce"`
+	Owner    string `db:"owner" json:"owner"`
+	Deadline int    `db:"deadline" json:"deadline"`
+	EventID  int    `db:"event_id" json:"eventID"`
+	Token    string `db:"token" json:"token"`
+}
+*/
 
 type EthTx struct {
 	artemis_autogen_bases.EthTx
+}
+
+const ArtemisScheduledDelivery = "EthTx"
+
+/*
+ProtocolNetworkID int    `db:"protocol_network_id" json:"protocolNetworkID"`
+TxHash            string `db:"tx_hash" json:"txHash"`
+Nonce             int    `db:"nonce" json:"nonce"`
+From              string `db:"from" json:"from"`
+Type              string `db:"type" json:"type"`
+EventID           int    `db:"event_id" json:"eventID"`
+*/
+var ts chronos.Chronos
+
+func (e *EthTx) InsertTx(ctx context.Context) (err error) {
+	q := sql_query_templates.QueryParams{}
+	q.RawQuery = `WITH cte_insert_id AS (
+						INSERT INTO events(event_id) VALUES ($1) RETURNING event_id
+					) 
+					INSERT INTO eth_tx(event_id, tx_hash, protocol_network_id, nonce, "from", type) 
+					VALUES ($1, $2, $3, $4, $5, $6);`
+	log.Debug().Interface("InsertTx", q.LogHeader(ArtemisScheduledDelivery))
+	if e.ProtocolNetworkID == 0 {
+		e.ProtocolNetworkID = hestia_req_types.EthereumMainnetProtocolNetworkID
+	}
+	if e.Type == "" {
+		e.Type = "0x02"
+	}
+	e.EventID = ts.UnixTimeStampNow()
+	_, err = apps.Pg.Exec(ctx, q.RawQuery, e.EventID, e.TxHash, e.ProtocolNetworkID, e.Nonce, e.From, e.Type)
+	if err != nil {
+		return err
+	}
+	return misc.ReturnIfErr(err, q.LogHeader(ArtemisScheduledDelivery))
 }
--- datastores/postgres/apps/artemis/models/txs/eth_txs/txs_test.go
+++ datastores/postgres/apps/artemis/models/txs/eth_txs/txs_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	artemis_autogen_bases "github.com/zeus-fyi/olympus/datastores/postgres/apps/artemis/models/bases/autogen"
 	hestia_test "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/test"
 )
 
@@ -17,6 +18,37 @@ type TxTestSuite struct {
 func (s *TxTestSuite) SetupTest() {
 	s.InitLocalConfigs()
 	s.Pg.InitPG(ctx, s.Tc.LocalDbPgconn)
+}
+
+func (s *TxTestSuite) TestInsertPermit2Tx() {
+	etx := EthTx{
+		artemis_autogen_bases.EthTx{
+			ProtocolNetworkID: 0,
+			TxHash:            "",
+			Nonce:             0,
+			From:              "",
+			Type:              "",
+			EventID:           0,
+		},
+	}
+	err := etx.InsertTx(ctx)
+	s.Require().Nil(err)
+}
+
+func (s *TxTestSuite) TestInsertTx() {
+	etx := EthTx{
+		artemis_autogen_bases.EthTx{
+			ProtocolNetworkID: 1,
+			TxHash:            "0x012fad",
+			Nonce:             0,
+			From:              "0x0gsdg32",
+			Type:              "0x02",
+			EventID:           0,
+		},
+	}
+	err := etx.InsertTx(ctx)
+	s.Require().Nil(err)
+	s.Assert().NotZerof(etx.EventID, "event id should not be zero")
 }
 
 func (s *TxTestSuite) TestSelect() {

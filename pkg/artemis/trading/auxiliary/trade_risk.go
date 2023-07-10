2e3385afe7115d7eb7f170bc0dba3c9a53a7b118
alex <alexg89@vt.edu>
2023-07-09 22:34:49 -0700

    adds gas to profit fee check

--- pkg/artemis/trading/auxiliary/trade_risk.go
+++ pkg/artemis/trading/auxiliary/trade_risk.go
@@ -6,6 +6,7 @@ import (
 	"math/big"
 
 	"github.com/rs/zerolog/log"
+	artemis_trading_cache "github.com/zeus-fyi/olympus/pkg/artemis/trading/cache"
 	artemis_eth_units "github.com/zeus-fyi/olympus/pkg/artemis/trading/lib/units"
 	"github.com/zeus-fyi/olympus/pkg/artemis/web3_client"
 )
@@ -16,6 +17,12 @@ func (a *AuxiliaryTradingUtils) maxTradeSize() *big.Int {
 }
 
 func (a *AuxiliaryTradingUtils) isProfitHigherThanGasFee(tf *web3_client.TradeExecutionFlow) (bool, error) {
+	if tf.FrontRunTrade.TotalGasCost == 0 {
+		return false, errors.New("front run gas cost is 0")
+	}
+	if tf.SandwichTrade.TotalGasCost == 0 {
+		return false, errors.New("sandwich gas cost is 0")
+	}
 	totalGasCost := tf.FrontRunTrade.TotalGasCost + tf.SandwichTrade.TotalGasCost
 	if !artemis_eth_units.IsXGreaterThanY(tf.SandwichTrade.AmountOut, artemis_eth_units.NewBigIntFromUint(totalGasCost)) {
 		return false, errors.New("profit is not higher than gas fee")
@@ -24,8 +31,16 @@ func (a *AuxiliaryTradingUtils) isProfitHigherThanGasFee(tf *web3_client.TradeEx
 	return true, nil
 }
 
-func (a *AuxiliaryTradingUtils) isTradingEnabledOnToken() bool {
-	return false
+func (a *AuxiliaryTradingUtils) isTradingEnabledOnToken(tk string) (bool, error) {
+	tan := artemis_trading_cache.TokenMap[tk].TradingEnabled
+	if tan == nil {
+		return false, errors.New("token not found in cache")
+	}
+	if *tan {
+		return *tan, nil
+	} else {
+		return false, errors.New("trading is disabled")
+	}
 }
 
 // in sandwich trade the tokenIn on the first trade is the profit currency
@@ -34,17 +49,20 @@ func (a *AuxiliaryTradingUtils) isProfitTokenAcceptable(ctx context.Context, tf 
 	if tf.FrontRunTrade.AmountInAddr.String() != wethAddr.String() {
 		return false, errors.New("profit token is not WETH")
 	}
-	err := tf.FrontRunTrade.GetGasUsageForAllTxs(ctx, a.U.Web3Client.Web3Actions)
+	ok, err := a.isTradingEnabledOnToken(tf.FrontRunTrade.AmountOutAddr.String())
+	if err != nil {
+		log.Info().Interface("tf.FrontRunTrade.AmountOutAddr", tf.FrontRunTrade.AmountOutAddr.String()).Msg("trading is disabled for token")
+		return false, err
+	}
+	err = tf.FrontRunTrade.GetGasUsageForAllTxs(ctx, a.U.Web3Client.Web3Actions)
 	if err != nil {
 		return false, errors.New("could not get gas usage for front run txs")
 	}
-
 	err = tf.SandwichTrade.GetGasUsageForAllTxs(ctx, a.U.Web3Client.Web3Actions)
 	if err != nil {
 		return false, errors.New("could not get gas usage for sandwich txs")
 	}
-
-	ok, err := a.isProfitHigherThanGasFee(tf)
+	ok, err = a.isProfitHigherThanGasFee(tf)
 	if err != nil {
 		return false, err
 	}

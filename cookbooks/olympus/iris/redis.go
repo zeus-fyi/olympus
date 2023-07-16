e7c197f8d20ef47fd252686259a4d7ee97aa314d
alex <alexg89@vt.edu>
2023-07-16 11:21:01 -0700

    adds redis wip

--- cookbooks/olympus/iris/redis.go
+++ cookbooks/olympus/iris/redis.go
@@ -1,13 +1,9 @@
 package iris_olympus_cookbook
 
 import (
-	"fmt"
-	"time"
-
 	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
 	zeus_cluster_config_drivers "github.com/zeus-fyi/zeus/zeus/cluster_config_drivers"
 	"github.com/zeus-fyi/zeus/zeus/z_client/zeus_common_types"
-	"github.com/zeus-fyi/zeus/zeus/z_client/zeus_req_types"
 )
 
 var (
@@ -16,50 +12,50 @@ var (
 		CloudCtxNs:       redisCloudCtxNs,
 		ComponentBases:   redisComponentBases,
 	}
-	// todo: add replicas
 	redisComponentBases = map[string]zeus_cluster_config_drivers.ComponentBaseDefinition{
-		"master":   redisComponentBase,
-		"replicas": redisComponentBase,
+		"master":   redisMasterComponentBase,
+		"replicas": redisReplicasComponentBase,
 	}
-	redisComponentBase = zeus_cluster_config_drivers.ComponentBaseDefinition{
+	redisMasterComponentBase = zeus_cluster_config_drivers.ComponentBaseDefinition{
 		SkeletonBases: map[string]zeus_cluster_config_drivers.ClusterSkeletonBaseDefinition{
-			"redis": redisSeletonBaseConfig,
+			"redis-master": redisMasterSkeletonBaseConfig,
 		},
 	}
-	redisSeletonBaseConfig = zeus_cluster_config_drivers.ClusterSkeletonBaseDefinition{
-		SkeletonBaseNameChartPath: redisChartPath,
+	redisReplicasComponentBase = zeus_cluster_config_drivers.ComponentBaseDefinition{
+		SkeletonBases: map[string]zeus_cluster_config_drivers.ClusterSkeletonBaseDefinition{
+			"redis-replicas": redisReplicasSkeletonBaseConfig,
+		},
+	}
+	redisMasterSkeletonBaseConfig = zeus_cluster_config_drivers.ClusterSkeletonBaseDefinition{
+		SkeletonBaseNameChartPath: redisMasterChartPath,
+	}
+	redisReplicasSkeletonBaseConfig = zeus_cluster_config_drivers.ClusterSkeletonBaseDefinition{
+		SkeletonBaseNameChartPath: redisReplicasChartPath,
 	}
 )
 
-var redisUploadChart = zeus_req_types.TopologyCreateRequest{
-	TopologyName:      "redis",
-	ChartName:         "redis",
-	ChartDescription:  "redis",
-	SkeletonBaseName:  "redis",
-	ComponentBaseName: "redis",
-	ClusterClassName:  "redis",
-	Tag:               "latest",
-	Version:           fmt.Sprintf("v0.0.%d", time.Now().Unix()),
-}
-
-var redisCloudCtxNs = zeus_common_types.CloudCtxNs{
-	CloudProvider: "do",
-	Region:        "sfo3",
-	Context:       "do-sfo3-dev-do-sfo3-zeus",
-	Namespace:     "redis", // set with your own namespace
-	Env:           "production",
-}
-
-var redisDeployKnsReq = zeus_req_types.TopologyDeployRequest{
-	TopologyID: 0,
-	CloudCtxNs: redisCloudCtxNs,
-}
-
-var redisChartPath = filepaths.Path{
-	PackageName: "",
-	DirIn:       "./olympus/iris/redis/master",
-	DirOut:      "./olympus/outputs",
-	FnIn:        "redis", // filename for your gzip workload
-	FnOut:       "",
-	Env:         "",
-}
+var (
+	redisCloudCtxNs = zeus_common_types.CloudCtxNs{
+		CloudProvider: "do",
+		Region:        "sfo3",
+		Context:       "do-sfo3-dev-do-sfo3-zeus",
+		Namespace:     "redis", // set with your own namespace
+		Env:           "production",
+	}
+	redisMasterChartPath = filepaths.Path{
+		PackageName: "",
+		DirIn:       "./olympus/iris/redis/master",
+		DirOut:      "./olympus/outputs",
+		FnIn:        "redisMaster", // filename for your gzip workload
+		FnOut:       "",
+		Env:         "",
+	}
+	redisReplicasChartPath = filepaths.Path{
+		PackageName: "",
+		DirIn:       "./olympus/iris/redis/replicas",
+		DirOut:      "./olympus/outputs",
+		FnIn:        "redisReplicas", // filename for your gzip workload
+		FnOut:       "",
+		Env:         "",
+	}
+)

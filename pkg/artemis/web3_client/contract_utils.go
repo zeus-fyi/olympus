ddd29b9a551549cbca9ae678e07c0224256ff56c
alex <alexg89@vt.edu>
2023-07-17 14:36:50 -0700

    adds eth call

--- pkg/artemis/web3_client/contract_utils.go
+++ pkg/artemis/web3_client/contract_utils.go
@@ -2,7 +2,9 @@ package web3_client
 
 import (
 	"context"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rs/zerolog/log"
@@ -29,4 +31,35 @@ func (w *Web3Client) GetOwner(ctx context.Context, abiFile *abi.ABI, contractAdd
 		return common.Address{}, err
 	}
 	return owner[0].(common.Address), err
+}
+
+func (w *Web3Client) EthCall(ctx context.Context, from common.Address, payload *web3_actions.SendContractTxPayload, bn *big.Int) ([]byte, error) {
+	w.Dial()
+	defer w.C.Close()
+	if payload.Data == nil {
+		payload.Data = []byte{}
+		err := payload.GenerateBinDataFromParamsAbi(ctx)
+		if err != nil {
+			log.Ctx(ctx).Err(err).Msg("EthCall: GenerateBinDataFromParamsAbi")
+			return nil, err
+		}
+	}
+	toAddr := common.HexToAddress(payload.SmartContractAddr)
+	msg := ethereum.CallMsg{
+		From:       from,
+		To:         &toAddr,
+		Gas:        payload.GasLimit,
+		GasPrice:   payload.GasPrice,
+		GasFeeCap:  payload.GasFeeCap,
+		GasTipCap:  payload.GasTipCap,
+		Value:      payload.SendEtherPayload.Amount,
+		Data:       payload.Data,
+		AccessList: nil,
+	}
+	resp, err := w.C.CallContract(ctx, msg, bn)
+	if err != nil {
+		log.Ctx(ctx).Err(err).Msg("EthCall")
+		return nil, err
+	}
+	return resp, nil
 }

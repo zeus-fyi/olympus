58f9d7e4fd8172989818546fb677ef3508fb8fd8
alex <alexg89@vt.edu>
2023-07-17 14:11:04 -0700

    adds multicall + test

--- pkg/artemis/web3_client/multicall3.go
+++ pkg/artemis/web3_client/multicall3.go
@@ -15,12 +15,20 @@ const (
 	Aggregate3Value = "aggregate3Value"
 )
 
+type MultiCallElement struct {
+	Name          string        `json:"name"`
+	AbiFile       *abi.ABI      `json:"abiFile"`
+	Call          Call          `json:"callData"`
+	DecodedInputs []interface{} `json:"decodedInputs,omitempty"`
+}
+type Call struct {
+	Target       common.Address `abi:"target"`
+	AllowFailure bool           `abi:"allowFailure"`
+	Data         []byte         `abi:"callData"`
+}
+
 type Multicall3 struct {
-	Name          string         `json:"name"`
-	Target        common.Address `json:"target"`
-	AbiFile       *abi.ABI       `json:"abiFile"`
-	CallData      []byte         `json:"callData"`
-	DecodedInputs []any          `json:"decodedInputs,omitempty"`
+	Calls []Call `json:"calls"`
 }
 
 // Multicall3Result is an auto generated low-level Go binding around an user-defined struct.
@@ -29,22 +37,32 @@ type Multicall3Result struct {
 	ReturnData []byte `json:"returnData"`
 }
 
-func CreateMulticall3Payload(ctx context.Context, calls []Multicall3) (web3_actions.SendContractTxPayload, error) {
+func CreateMulticall3Payload(ctx context.Context, calls []MultiCallElement) (web3_actions.SendContractTxPayload, error) {
+	callInputs := make([]Call, len(calls))
+	for i, _ := range calls {
+		method := calls[i].AbiFile.Methods[calls[i].Name]
+		targetName := method.ID
+		inputs, err := method.Inputs.Pack(calls[i].DecodedInputs...)
+		if err != nil {
+			return web3_actions.SendContractTxPayload{}, err
+		}
+		inputs = append(targetName[:], inputs[:]...)
+		callInputs[i] = Call{
+			Target:       calls[i].Call.Target,
+			AllowFailure: calls[i].Call.AllowFailure,
+			Data:         inputs,
+		}
+	}
+	return makeAggregate3Payload(callInputs), nil
+}
+
+func makeAggregate3Payload(calls []Call) web3_actions.SendContractTxPayload {
 	payload := web3_actions.SendContractTxPayload{
 		SmartContractAddr: artemis_trading_constants.Multicall3Address,
 		SendEtherPayload:  web3_actions.SendEtherPayload{},
 		ContractABI:       artemis_oly_contract_abis.MultiCall3,
 		MethodName:        Aggregate3,
-		Params:            []interface{}{},
+		Params:            []interface{}{calls},
 	}
-	for _, ca := range calls {
-		inputs, err := ca.AbiFile.Methods[ca.Name].Inputs.Pack(ca.DecodedInputs...)
-		if err != nil {
-			return web3_actions.SendContractTxPayload{}, err
-		}
-		ca.CallData = inputs
-		callParams := []interface{}{ca.Target, ca.CallData}
-		payload.Params = append(payload.Params, callParams)
-	}
-	return payload, nil
+	return payload
 }
--- pkg/artemis/web3_client/mutlicall3_test.go
+++ pkg/artemis/web3_client/mutlicall3_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	web3_actions "github.com/zeus-fyi/gochain/web3/client"
 	artemis_trading_constants "github.com/zeus-fyi/olympus/pkg/artemis/trading/lib/constants"
 	artemis_test_cache "github.com/zeus-fyi/olympus/pkg/artemis/trading/test_suite/test_cache"
@@ -24,20 +25,43 @@ func (s *Web3ClientTestSuite) TestMulticall3() {
 		MethodName:        "balanceOf",
 		Params:            []interface{}{userAddr},
 	}
-
 	bal, err := wc.CallConstantFunction(ctx, &payload)
 	s.Assert().Nil(err)
 	s.Assert().NotNil(bal)
 	fmt.Println(bal[0].(*big.Int).String())
 
-	// CreateMulticall3Payload()
-	//scAddr := artemis_trading_constants.Multicall3Address
-	//userAddr := "0x6B44ba0a126a2A1a8aa6cD1AdeeD002e141Bcd44"
-	//payload := web3_actions.SendContractTxPayload{
-	//	SmartContractAddr: scAddr,
-	//	SendEtherPayload:  web3_actions.SendEtherPayload{},
-	//	ContractABI:       artemis_oly_contract_abis.MustLoadERC20Abi(),
-	//	MethodName:        "balanceOf",
-	//	Params:            []interface{}{userAddr},
-	//}
+	userAddr2 := "0xc261aA0F3fe1Ce69972F65397D9d062511efe9cA"
+	m3 := []MultiCallElement{{
+		Name: "balanceOf",
+		Call: Call{
+			Target:       common.HexToAddress(artemis_trading_constants.WETH9ContractAddress),
+			AllowFailure: false,
+			Data:         nil,
+		},
+		AbiFile:       artemis_oly_contract_abis.MustLoadERC20Abi(),
+		DecodedInputs: []interface{}{common.HexToAddress(userAddr)},
+	}, {
+		Name: "balanceOf",
+		Call: Call{
+			Target:       common.HexToAddress(artemis_trading_constants.LidoSEthAddr),
+			AllowFailure: false,
+			Data:         nil,
+		},
+		AbiFile:       artemis_oly_contract_abis.MustLoadERC20Abi(),
+		DecodedInputs: []interface{}{common.HexToAddress(userAddr2)},
+	}}
+	payload, err = CreateMulticall3Payload(ctx, m3)
+	s.Assert().Nil(err)
+	resp, err := wc.CallConstantFunction(ctx, &payload)
+	s.Assert().Nil(err)
+	s.Assert().NotNil(resp)
+	for _, r := range resp {
+		encData := r.([]struct {
+			Success    bool    "json:\"success\""
+			ReturnData []uint8 "json:\"returnData\""
+		})[0]
+		fmt.Println(encData.Success)
+		bi := new(big.Int).SetBytes(encData.ReturnData)
+		fmt.Println(bi.String())
+	}
 }

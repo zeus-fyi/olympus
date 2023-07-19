4f517480694441115f6d73b8846ee92de0b2c102
alex <alexg89@vt.edu>
2023-07-18 20:42:31 -0700

    use one go f

--- pkg/artemis/trading/active_trading_bundler.go
+++ pkg/artemis/trading/active_trading_bundler.go
@@ -14,6 +14,7 @@ func (a *ActiveTrading) ProcessBundleStage(ctx context.Context, w3c web3_client.
 		err := ActiveTradingFilter(ctx, w3c, tf)
 		if err != nil {
 			log.Err(err).Msg("ProcessBundleStage: failed to pass active filter trade")
+			err = nil
 			continue
 		}
 		log.Info().Msgf("ProcessBundleStage: passed active filter trade: %s", tf.Tx.Hash().String())
@@ -21,6 +22,7 @@ func (a *ActiveTrading) ProcessBundleStage(ctx context.Context, w3c web3_client.
 		resp, err := a.GetAuxClient().StagingPackageSandwichAndCall(ctx, &tf)
 		if err != nil {
 			log.Err(err).Msg("ProcessBundleStage: failed to package sandwich")
+			err = nil
 			continue
 		}
 		log.Info().Msgf("ProcessBundleStage: StagingPackageSandwichAndCall passed bundle hash: %s", resp.BundleHash)

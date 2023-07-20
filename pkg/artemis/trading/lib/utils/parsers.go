92a38da60e4ec4a43f31119d286de3dbc44d602a
alex <alexg89@vt.edu>
2023-07-19 23:47:12 -0700

    refactor to clean up errors

--- pkg/artemis/trading/lib/utils/parsers.go
+++ pkg/artemis/trading/lib/utils/parsers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/gochain/web3/accounts"
 )
 
@@ -19,11 +20,18 @@ func ParseBigInt(i interface{}) (*big.Int, error) {
 			return nil, fmt.Errorf("failed to parse string '%s' into big.Int", v)
 		}
 		return result, nil
+	case int:
+		return big.NewInt(int64(v)), nil
+	case uint64:
+		return new(big.Int).SetUint64(v), nil
 	case uint32:
 		return big.NewInt(int64(v)), nil
 	case int64:
 		return big.NewInt(v), nil
+	case []byte:
+		return new(big.Int).SetBytes(v), nil
 	default:
+		log.Warn().Msgf("ParseBigInt: unknown type %T", v)
 		return nil, fmt.Errorf("input is not a string or int64")
 	}
 }

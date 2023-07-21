f3a26604cbc20f4845b21abbf427bc1ce0b9e754
alex <alexg89@vt.edu>
2023-07-20 18:17:31 -0700

    refactor json conversion stages

--- pkg/artemis/trading/trade_debugger/trade_debugger.go
+++ pkg/artemis/trading/trade_debugger/trade_debugger.go
@@ -75,7 +75,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactTokensForTokensSupportingFeeOnTransferTokensParams:
 		params := v.(*web3_client.SwapExactTokensForTokensSupportingFeeOnTransferTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -85,7 +85,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactETHForTokensSupportingFeeOnTransferTokensParams:
 		params := v.(*web3_client.SwapExactETHForTokensSupportingFeeOnTransferTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -95,7 +95,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactTokensForETHSupportingFeeOnTransferTokensParams:
 		params := v.(*web3_client.SwapExactTokensForETHSupportingFeeOnTransferTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -105,7 +105,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactTokensForTokensParams:
 		params := v.(*web3_client.SwapExactTokensForTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -115,7 +115,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactETHForTokensParams:
 		params := v.(*web3_client.SwapExactETHForTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -125,7 +125,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapExactTokensForETHParams:
 		params := v.(*web3_client.SwapExactTokensForETHParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -135,7 +135,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapTokensForExactTokensParams:
 		params := v.(*web3_client.SwapTokensForExactTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -145,7 +145,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapTokensForExactETHParams:
 		params := v.(*web3_client.SwapTokensForExactETHParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -155,7 +155,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	case *web3_client.SwapETHForExactTokensParams:
 		params := v.(*web3_client.SwapETHForExactTokensParams)
 		if h.TradePrediction.InitialPair == nil {
@@ -165,7 +165,7 @@ func (h *HistoricalAnalysisDebug) BinarySearch() (web3_client.TradeExecutionFlow
 		if err != nil {
 			return tf, fmt.Errorf("error in binary search: %w", err)
 		}
-		return search.ConvertToBigIntTypeWithoutTx(), nil
+		return search, nil
 	default:
 		fmt.Println(v)
 	}

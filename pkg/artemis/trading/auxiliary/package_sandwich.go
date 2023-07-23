c9ad03b1e7035ba39209ab92694511ad2ab49d57
alex <alexg89@vt.edu>
2023-07-22 17:26:36 -0700

    fix gas

--- pkg/artemis/trading/auxiliary/package_sandwich.go
+++ pkg/artemis/trading/auxiliary/package_sandwich.go
@@ -43,7 +43,7 @@ func packageFrontRun(ctx context.Context, w3c web3_client.Web3Client, tf *web3_c
 			return nil, err
 		}
 		scInfoFrontRun.GasTipCap = artemis_eth_units.NewBigInt(0)
-		scInfoFrontRun.GasFeeCap = artemis_eth_units.AddBigIntUsingInt(baseFee, 69)
+		scInfoFrontRun.GasFeeCap = artemis_eth_units.MulBigIntWithFloat(baseFee, 1.1)
 		scInfoFrontRun.GasPrice = scInfoFrontRun.GasFeeCap
 		toAddr := common.HexToAddress(scInfoFrontRun.ToAddress.Hex())
 		msg := ethereum.CallMsg{

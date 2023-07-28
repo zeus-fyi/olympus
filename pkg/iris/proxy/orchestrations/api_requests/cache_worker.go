9329ef790ee48d01c8af646cbfb70c3b680a9fdb
alex <alexg89@vt.edu>
2023-07-28 14:02:55 -0700

    updates wf for iris cache updater

--- pkg/iris/proxy/orchestrations/api_requests/cache_worker.go
+++ pkg/iris/proxy/orchestrations/api_requests/cache_worker.go
@@ -7,22 +7,6 @@ import (
 	"go.temporal.io/sdk/client"
 )
 
-func (t *IrisApiRequestsWorker) ExecuteIrisCacheUpdateOrAddOrgRoutingTablesWorkflow(ctx context.Context, orgID int) error {
-	tc := t.ConnectTemporalClient()
-	defer tc.Close()
-	workflowOptions := client.StartWorkflowOptions{
-		TaskQueue: t.TaskQueueName,
-	}
-	txWf := NewIrisApiRequestsWorkflow()
-	wf := txWf.CacheUpdateOrAddOrgRoutingTablesWorkflow
-	_, err := tc.ExecuteWorkflow(ctx, workflowOptions, wf, orgID)
-	if err != nil {
-		log.Err(err).Msg("ExecuteIrisCacheUpdateOrAddOrgRoutingTablesWorkflow")
-		return err
-	}
-	return err
-}
-
 func (t *IrisApiRequestsWorker) ExecuteIrisCacheRefreshAllOrgRoutingTablesWorkflow(ctx context.Context) error {
 	tc := t.ConnectTemporalClient()
 	defer tc.Close()
@@ -34,6 +18,38 @@ func (t *IrisApiRequestsWorker) ExecuteIrisCacheRefreshAllOrgRoutingTablesWorkfl
 	_, err := tc.ExecuteWorkflow(ctx, workflowOptions, wf)
 	if err != nil {
 		log.Err(err).Msg("ExecuteIrisCacheRefreshAllOrgRoutingTablesWorkflow")
+		return err
+	}
+	return err
+}
+
+func (t *IrisApiRequestsWorker) ExecuteIrisCacheUpdateOrAddOrgRoutingTablesWorkflow(ctx context.Context, orgID int) error {
+	tc := t.ConnectTemporalClient()
+	defer tc.Close()
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: t.TaskQueueName,
+	}
+	txWf := NewIrisApiRequestsWorkflow()
+	wf := txWf.CacheRefreshOrgRoutingTablesWorkflow
+	_, err := tc.ExecuteWorkflow(ctx, workflowOptions, wf, orgID)
+	if err != nil {
+		log.Err(err).Msg("ExecuteIrisCacheUpdateOrAddOrgRoutingTablesWorkflow")
+		return err
+	}
+	return err
+}
+
+func (t *IrisApiRequestsWorker) ExecuteIrisCacheUpdateOrAddOrgGroupRoutingTableWorkflow(ctx context.Context, orgID int, groupName string) error {
+	tc := t.ConnectTemporalClient()
+	defer tc.Close()
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: t.TaskQueueName,
+	}
+	txWf := NewIrisApiRequestsWorkflow()
+	wf := txWf.CacheRefreshOrgGroupTableWorkflow
+	_, err := tc.ExecuteWorkflow(ctx, workflowOptions, wf, orgID, groupName)
+	if err != nil {
+		log.Err(err).Msg("ExecuteIrisCacheUpdateOrAddOrgGroupRoutingTableWorkflow")
 		return err
 	}
 	return err

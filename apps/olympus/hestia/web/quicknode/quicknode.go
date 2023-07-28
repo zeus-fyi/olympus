02453e6d0c6185e299fe977e68c19b36c8d0ba1a
alex <alexg89@vt.edu>
2023-07-28 12:34:17 -0700

    adds jwt setup for hestia

--- apps/olympus/hestia/web/quicknode/quicknode.go
+++ apps/olympus/hestia/web/quicknode/quicknode.go
@@ -1,7 +1,6 @@
 package hestia_quicknode_dashboard
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -19,15 +18,37 @@ func InitQuickNodeDashboardRoutes(e *echo.Echo) {
 	}))
 
 	e.GET("/dashboard", func(c echo.Context) error {
+		resp := QuickNodeResponse{
+			Status:       "",
+			DashboardURL: "",
+			AccessURL:    "",
+		}
 		token, ok := c.Get("user").(*jwt.Token) // by default token is stored under `user` key
 		if !ok {
-			return errors.New("JWT token missing or invalid")
+			resp.Status = "error: JWT token missing or invalid"
+			return c.JSON(http.StatusBadRequest, resp)
 		}
-		claims, ok := token.Claims.(jwt.MapClaims) // by default claims is of type `jwt.MapClaims`
+		// claims
+		_, ok = token.Claims.(jwt.MapClaims) // by default claims is of type `jwt.MapClaims`
 		if !ok {
-			return errors.New("failed to cast claims as jwt.MapClaims")
+			resp.Status = "error: failed to cast claims as jwt.MapClaims"
+			return c.JSON(http.StatusBadRequest, resp)
 		}
-		return c.JSON(http.StatusOK, claims)
+		/*
+			c.JSON(200, gin.H{
+				"status":        "success",
+				"dashboard-url": scheme + c.Request.Host + "/dashboard",
+				"access-url":    scheme + c.Request.Host + "/api", // Note: should be protected by API key
+			})
+		*/
+		resp.Status = "success"
+		return c.JSON(http.StatusOK, resp)
 	})
 
+}
+
+type QuickNodeResponse struct {
+	Status       string `json:"status"`
+	DashboardURL string `json:"dashboard-url"`
+	AccessURL    string `json:"access-url"`
 }

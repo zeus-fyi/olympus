76f1d736fd77a1b01de39d8e4d92b2c81f2ba065
alex <alexg89@vt.edu>
2023-07-29 21:03:22 -0700

    ui updates

--- apps/olympus/hestia/assets/src/components/loadbalancing/LoadBalancingRoutesTable.tsx
+++ apps/olympus/hestia/assets/src/components/loadbalancing/LoadBalancingRoutesTable.tsx
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import * as React from "react";
 import {useEffect, useState} from "react";
 import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
 import {TableContainer, TableRow} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
@@ -17,6 +18,7 @@ export function LoadBalancingRoutesTable() {
     const params = useParams();
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(true);
+    const [selected, setSelected] = useState<string[]>([]);
     const endpoints = useSelector((state: RootState) => state.loadBalancing.routes);
 
     useEffect(() => {
@@ -29,9 +31,33 @@ export function LoadBalancingRoutesTable() {
                 console.log("error", error);
             } finally {
                 setLoading(false); // Set loading to false regardless of success or failure.
-            }}
+            }
+        }
         fetchData(params);
     }, []);
+
+    const handleClick = (name: string) => {
+        const currentIndex = selected.indexOf(name);
+        const newSelected = [...selected];
+
+        if (currentIndex === -1) {
+            newSelected.push(name);
+        } else {
+            newSelected.splice(currentIndex, 1);
+        }
+
+        setSelected(newSelected);
+    };
+
+    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+            const newSelecteds = endpoints.map((endpoint) => endpoint);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
     if (loading) {
         return <div>Loading...</div> // Display loading message while data is fetching
     }
@@ -43,27 +69,42 @@ export function LoadBalancingRoutesTable() {
     return (
         <div>
             <Box sx={{ mt: 4, mb: 4 }}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow style={{ backgroundColor: '#333'}} >
-                            <TableCell style={{ color: 'white'}} align="left">Endpoints</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {endpoints.map((row: any, i: number) => (
-                            <TableRow
-                                key={i}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row}
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow style={{ backgroundColor: '#333'}} >
+                                <TableCell padding="checkbox">
+                                    <Checkbox
+                                        color="primary"
+                                        indeterminate={selected.length > 0 && selected.length < endpoints.length}
+                                        checked={endpoints.length > 0 && selected.length === endpoints.length}
+                                        onChange={handleSelectAllClick}
+                                    />
                                 </TableCell>
+                                <TableCell style={{ color: 'white'}} align="left">Endpoints</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {endpoints.map((row: any, i: number) => (
+                                <TableRow
+                                    key={i}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell padding="checkbox">
+                                        <Checkbox
+                                            checked={selected.indexOf(row) !== -1}
+                                            onChange={() => handleClick(row)}
+                                            color="primary"
+                                        />
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {row}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Box>
         </div>
     );

2d198fb8197433a5233ba163f2277e7911899c9d
alex <alexg89@vt.edu>
2023-07-31 00:16:07 -0700

    update link

--- apps/olympus/hestia/assets/src/components/app/ResourceRequirementsTable.tsx
+++ apps/olympus/hestia/assets/src/components/app/ResourceRequirementsTable.tsx
@@ -41,7 +41,7 @@ export function ResourceRequirementsTable(props: any) {
 
     // const handleClick = async (event: any, app: any) => {
     //     event.preventDefault();
-    //     navigate('/clusters/app/'+app.topologySystemComponentID);
+    //     navigate('/app/'+app.topologySystemComponentID);
     // }
 
     if (cluster == null) {

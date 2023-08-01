cca2f0e021be55621c43bfc8150a3f12b3fc29a9
alex <alexg89@vt.edu>
2023-08-01 16:03:10 -0700

    adds auth & plan cache

--- apps/olympus/iris/api/v1/v1router.go
+++ apps/olympus/iris/api/v1/v1router.go
@@ -2,20 +2,16 @@ package v1_iris
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
 	create_org_users "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/create/org_users"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/read/auth"
+	iris_redis "github.com/zeus-fyi/olympus/datastores/redis/apps/iris"
 )
-
-var authCache = cache.New(time.Minute*5, time.Minute*10)
 
 func InitV1Routes(e *echo.Echo) {
 	eg := e.Group("/v1")
@@ -36,18 +32,16 @@ func InitV1Routes(e *echo.Echo) {
 			c.Set(QuickNodeEndpointID, qnEndpointID)
 			c.Set(QuickNodeChain, qnChain)
 			c.Set(QuickNodeNetwork, qnNetwork)
-			val, ok := authCache.Get(token)
-			if ok {
-				orgUser, ok1 := val.(org_users.OrgUser)
-				if ok1 {
-					servicePlan, ok2 := authCache.Get(fmt.Sprintf("%d-servicePlan", orgUser.OrgID))
-					if ok2 && len(servicePlan.(string)) > 0 {
-						c.Set("servicePlan", servicePlan)
-						c.Set("orgUser", orgUser)
-						c.Set("bearer", val)
-						return true, nil
-					}
-				}
+			orgID, plan, err := iris_redis.IrisRedis.GetAuthCacheIfExists(ctx, token)
+			if err != nil && orgID > 0 && plan != "" {
+				c.Set("servicePlan", plan)
+				c.Set("orgUser", org_users.NewOrgUserWithID(int(orgID), 0))
+				c.Set("bearer", token)
+				return true, nil
+			} else {
+				plan = ""
+				orgID = -1
+				err = nil
 			}
 			key, err := auth.VerifyBearerTokenServiceWithQuickNodePlan(ctx, token, create_org_users.IrisQuickNodeService)
 			if err != nil {
@@ -57,15 +51,18 @@ func InitV1Routes(e *echo.Echo) {
 			if len(key.PublicKeyName) <= 0 {
 				return false, c.JSON(http.StatusUnauthorized, Response{Message: "no service plan found"})
 			}
-			ou := org_users.NewOrgUserWithID(key.OrgID, key.GetUserID())
-			if key.PublicKeyVerified {
-				authCache.SetDefault(fmt.Sprintf("%d-servicePlan", key.OrgID), key.PublicKeyName)
-				authCache.SetDefault(token, key)
-				authCache.SetDefault(fmt.Sprintf("%d", key.OrgID), ou)
+			if err == nil && key.PublicKeyVerified && orgID > 0 && plan != "" {
+				go func(oID int, token, plan string) {
+					err = iris_redis.IrisRedis.SetAuthCache(context.Background(), oID, token, key.PublicKeyName)
+					if err != nil {
+						log.Err(err).Msg("InitV1Routes: SetAuthCache")
+					}
+				}(int(orgID), token, key.PublicKeyName)
+				ou := org_users.NewOrgUserWithID(key.OrgID, key.GetUserID())
+				c.Set("servicePlan", plan)
+				c.Set("orgUser", ou)
+				c.Set("bearer", token)
 			}
-			c.Set("servicePlan", key.PublicKeyName)
-			c.Set("orgUser", ou)
-			c.Set("bearer", key.PublicKey)
 			return key.PublicKeyVerified, err
 		},
 	}))

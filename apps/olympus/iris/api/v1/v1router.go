cd79b60fe676a73baf65d251f5e51de0ebe65123
alex <alexg89@vt.edu>
2023-08-02 12:47:24 -0700

    adds new auth for services

--- apps/olympus/iris/api/v1/v1router.go
+++ apps/olympus/iris/api/v1/v1router.go
@@ -2,16 +2,16 @@ package v1_iris
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
-	create_org_users "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/create/org_users"
-	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/read/auth"
+	read_keys "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/read/keys"
 	iris_redis "github.com/zeus-fyi/olympus/datastores/redis/apps/iris"
 )
+
+const QuickNodeMarketPlace = "quickNodeMarketPlace"
 
 func InitV1Routes(e *echo.Echo) {
 	eg := e.Group("/v1")
@@ -43,13 +43,15 @@ func InitV1Routes(e *echo.Echo) {
 				orgID = -1
 				err = nil
 			}
-			key, err := auth.VerifyBearerTokenServiceWithQuickNodePlan(ctx, token, create_org_users.IrisQuickNodeService)
+			key := read_keys.NewKeyReader()
+			services, err := key.QueryUserAuthedServices(ctx, token)
 			if err != nil {
-				log.Err(err).Msg("InitV1Routes")
-				return false, c.JSON(http.StatusUnauthorized, nil)
+				log.Err(err).Msg("InitV1Routes: QueryUserAuthedServices error")
+				return false, err
 			}
-			if len(key.PublicKeyName) <= 0 {
-				return false, c.JSON(http.StatusUnauthorized, Response{Message: "no service plan found"})
+			c.Set("servicePlans", key.Services)
+			if val, ok := key.Services[QuickNodeMarketPlace]; ok {
+				plan = val
 			}
 			if err == nil && key.PublicKeyVerified && orgID > 0 && plan != "" {
 				go func(oID int, token, plan string) {
@@ -63,7 +65,7 @@ func InitV1Routes(e *echo.Echo) {
 				c.Set("orgUser", ou)
 				c.Set("bearer", token)
 			}
-			return key.PublicKeyVerified, err
+			return len(services) > 0, err
 		},
 	}))
 	eg.POST("/router/group", RpcLoadBalancerRequestHandler)

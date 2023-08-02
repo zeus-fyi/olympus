96352e75faa3a2f0c260f5bb7c1935f8021574d3
alex <alexg89@vt.edu>
2023-08-02 14:16:39 -0700

    fix auth on route

--- apps/olympus/hestia/api/v1/iris/update.go
+++ apps/olympus/hestia/api/v1/iris/update.go
@@ -7,8 +7,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
-	create_org_users "github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/create/org_users"
-	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/read/auth"
 	iris_models "github.com/zeus-fyi/olympus/datastores/postgres/apps/iris"
 	platform_service_orchestrations "github.com/zeus-fyi/olympus/pkg/hestia/platform/iris/orchestrations"
 )
@@ -33,19 +31,17 @@ func (r *OrgGroupRoutesRequest) UpdateOrgGroup(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
-	token := c.Get("bearer").(string)
-	if len(token) <= 0 {
-		return c.JSON(http.StatusInternalServerError, nil)
+	sp, ok := c.Get("servicePlans").(map[string]string)
+	if !ok {
+		log.Warn().Interface("servicePlans", sp).Msg("CreateGroupRoute: marketplace not found")
+		return c.JSON(http.StatusUnprocessableEntity, nil)
 	}
-	key, err := auth.VerifyBearerTokenServiceWithQuickNodePlan(context.Background(), token, create_org_users.IrisQuickNodeService)
-	if err != nil {
-		log.Err(err).Msg("InitV1Routes")
-		return c.JSON(http.StatusUnauthorized, nil)
+	plan, ok := sp[QuickNodeMarketPlace]
+	if !ok {
+		log.Warn().Str("marketplace", QuickNodeMarketPlace).Msg("CreateGroupRoute: marketplace not found")
+		return c.JSON(http.StatusUnprocessableEntity, nil)
 	}
-	if len(key.PublicKeyName) <= 0 {
-		return c.JSON(http.StatusUnauthorized, nil)
-	}
-	err = tc.CheckPlanLimits(key.PublicKeyName)
+	err = tc.CheckPlanLimits(plan)
 	if err != nil {
 		return c.JSON(http.StatusUnprocessableEntity, err)
 	}

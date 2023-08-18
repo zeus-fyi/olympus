e6039267e1d31e5a18139c473a09d4ef6d991886
alex <alexg89@vt.edu>
2023-08-17 18:25:45 -0700

    adds digest settings

--- datastores/redis/apps/iris/adaptive.go
+++ datastores/redis/apps/iris/adaptive.go
@@ -3,17 +3,30 @@ package iris_redis
 import (
 	"context"
 	"fmt"
-	"time"
 
+	"github.com/gomodule/redigo/redis"
 	"github.com/rs/zerolog/log"
 )
 
-func (m *IrisCache) SetMetricLatencyTDigest(ctx context.Context, orgID int, tableName, metricName string, latency float64) error {
+func (m *IrisCache) SetMetricLatencyTDigest(ctx context.Context, orgID int, tableName, metricName string, latencies []float64, sorted bool) error {
 	metricTdigestKey := fmt.Sprintf("%d:%s:%s", orgID, tableName, metricName)
+
+	// Convert latencies to interface slice for Redis command
+	latenciesInterface := make([]interface{}, len(latencies))
+	for i, v := range latencies {
+		latenciesInterface[i] = v
+	}
 
 	// Use Redis transaction (pipeline) to perform all operations atomically
 	pipe := m.Writer.TxPipeline()
-	pipe.Set(ctx, metricTdigestKey, latency, time.Minute*60)
+
+	// Determine whether to use PERCENTILE.MERGE or PERCENTILE.MERGESORTED based on sorted flag
+	if sorted {
+		pipe.Do(ctx, "PERCENTILE.MERGESORTED", metricTdigestKey, latenciesInterface)
+	} else {
+		pipe.Do(ctx, "PERCENTILE.MERGE", metricTdigestKey, latenciesInterface)
+	}
+
 	// Execute the transaction
 	_, err := pipe.Exec(ctx)
 	if err != nil {
@@ -21,4 +34,22 @@ func (m *IrisCache) SetMetricLatencyTDigest(ctx context.Context, orgID int, tabl
 		return err
 	}
 	return nil
+}
+
+func (m *IrisCache) GetMetricPercentile(ctx context.Context, orgID int, tableName, metricName string, percentile float64) (float64, error) {
+	metricTdigestKey := fmt.Sprintf("%d:%s:%s", orgID, tableName, metricName)
+
+	resp, err := m.Reader.Do(ctx, "PERCENTILE.GET", metricTdigestKey, percentile).Result()
+	if err != nil {
+		log.Err(err).Msgf("GetMetricPercentile")
+		return 0, err
+	}
+
+	value, err := redis.Float64(resp, err)
+	if err != nil {
+		log.Err(err).Msgf("GetMetricPercentile conversion error")
+		return 0, err
+	}
+
+	return value, nil
 }
--- datastores/redis/apps/iris/adaptive_test.go
+++ datastores/redis/apps/iris/adaptive_test.go
@@ -2,7 +2,15 @@ package iris_redis
 
 import "context"
 
-func (r *IrisRedisTestSuite) TestAdaptiveCache() {
-	err := IrisRedisClient.SetMetricLatencyTDigest(context.Background(), 1, "s", "test", 5.3)
+func (r *IrisRedisTestSuite) TestSetMetricLatencyTDigest() {
+	err := IrisRedisClient.SetMetricLatencyTDigest(context.Background(), 1, "s", "test", []float64{5.3}, false)
 	r.NoError(err)
+}
+
+func (r *IrisRedisTestSuite) TestGetMetricPercentile() {
+	// Assume that you have a method to mock the behavior of the Redis client
+	// and it's assigned to IrisRedisClient.Reader
+	value, err := IrisRedisClient.GetMetricPercentile(context.Background(), 1, "s", "test", 90.0)
+	r.NoError(err)
+	r.Equal(42.0, value)
 }

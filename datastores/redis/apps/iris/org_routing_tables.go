f5b31d8862c887756b7dfb70ca143e21598522fd
alex <alexg89@vt.edu>
2023-09-04 13:00:53 -0700

    fix delete/partial incl ps routes

--- datastores/redis/apps/iris/org_routing_tables.go
+++ datastores/redis/apps/iris/org_routing_tables.go
@@ -3,13 +3,64 @@ package iris_redis
 import (
 	"context"
 
+	"github.com/go-redis/redis/v9"
 	"github.com/rs/zerolog/log"
 	iris_models "github.com/zeus-fyi/olympus/datastores/postgres/apps/iris"
 )
 
+// endpointPriorityScoreKey := createAdaptiveEndpointPriorityScoreKey(stats.OrgID, stats.TableName)
+
+func (m *IrisCache) GetPriorityRoutes(ctx context.Context, orgID int, rgName string) ([]string, error) {
+	pipe := m.Reader.TxPipeline()
+	endpointPriorityScoreKey := createAdaptiveEndpointPriorityScoreKey(orgID, rgName)
+
+	// Execute the transaction
+	_, err := pipe.Exec(ctx)
+	if err != nil && err != redis.Nil {
+		log.Err(err).Msgf("GetPriorityRoutes: %s", rgName)
+		return nil, err
+	}
+	if err == redis.Nil {
+		return nil, nil
+	}
+	existingRoutes, err := m.Reader.ZRange(ctx, endpointPriorityScoreKey, 0, -1).Result()
+	if err != nil && err != redis.Nil {
+		log.Err(err).Msgf("Failed to fetch existing routes from set: %s", endpointPriorityScoreKey)
+		return nil, err
+	}
+	if err == redis.Nil {
+		return nil, nil
+	}
+	return existingRoutes, nil
+}
+
 func (m *IrisCache) AddOrUpdateOrgRoutingGroup(ctx context.Context, orgID int, rgName string, routes []iris_models.RouteInfo) error {
+	// Get the existing priority scored routes
+	endpointPriorityScoreKey := createAdaptiveEndpointPriorityScoreKey(orgID, rgName)
+	existingRoutes, err := m.GetPriorityRoutes(ctx, orgID, rgName)
+	if err != nil {
+		log.Err(err).Msgf("AddOrUpdateOrgRoutingGroup: %s", rgName)
+		return err
+	}
+	// Create a map of new routes for quick lookup
+	newRouteMap := make(map[string]bool)
+	for _, routeInfo := range routes {
+		newRouteMap[routeInfo.RoutePath] = true
+	}
+	// Identify elements to be removed
+	var routesToRemove []string
+	for _, existingRoute := range existingRoutes {
+		if !newRouteMap[existingRoute] {
+			routesToRemove = append(routesToRemove, existingRoute)
+		}
+	}
 	// Start a new transaction
 	pipe := m.Writer.TxPipeline()
+
+	// Remove outdated elements from sorted set
+	if len(routesToRemove) > 0 {
+		pipe.ZRem(ctx, endpointPriorityScoreKey, routesToRemove)
+	}
 
 	// Define the route group tag
 	rgTag := getOrgRouteKey(orgID, rgName)
@@ -41,7 +92,7 @@ func (m *IrisCache) AddOrUpdateOrgRoutingGroup(ctx context.Context, orgID int, r
 	}
 
 	// Execute the transaction
-	_, err := pipe.Exec(ctx)
+	_, err = pipe.Exec(ctx)
 	if err != nil {
 		log.Err(err).Msgf("AddOrUpdateOrgRoutingGroup: %s", rgTag)
 		return err

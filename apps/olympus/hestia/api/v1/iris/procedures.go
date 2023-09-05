47cccaf2e551cb0c7bcd1c0a43d7cb0bc5b9ff90
alex <alexg89@vt.edu>
2023-09-05 09:26:58 -0700

    adds show details toggle

--- apps/olympus/hestia/api/v1/iris/procedures.go
+++ apps/olympus/hestia/api/v1/iris/procedures.go
@@ -103,14 +103,28 @@ func (p *ProceduresRequest) GetProceduresCatalog(c echo.Context) error {
 		},
 	}
 	resp := []ProceduresRequestResponse{{
-		Name:         "eth_maxBlockAggReduce",
-		Description:  "This procedure will return the latest block number and the block data for the latest block. ",
-		Protocol:     "ethereum",
+		Name: "eth_maxBlockAggReduce",
+		Description: "This procedure will poll your table routes for the latest block number, and then send the request" +
+			" to routes at the max block number seen and then return the first successful response.",
+		Protocol:     "ethereum, arbitrum, arbitrum-nova, bsc, base, celo, optimism, polygon, zkSync",
 		OrderedSteps: []iris_programmable_proxy_v1_beta.IrisRoutingProcedureStep{getBlockHeightProcedure, getBlockProcedure},
 	}, {
-		Name:         "near_maxBlockAggReduce",
-		Protocol:     "near",
-		Description:  "This procedure will return the latest block number and the block data for the latest block. ",
+		Name:     "near_maxBlockAggReduce",
+		Protocol: "near",
+		Description: "This procedure will poll your table routes for the latest block number, and then send the request" +
+			" to routes at the max block number seen and then return the first successful response.",
+		OrderedSteps: []iris_programmable_proxy_v1_beta.IrisRoutingProcedureStep{getBlockHeightProcedure, getBlockProcedure},
+	}, {
+		Name:     "near_maxBlockAggReduce",
+		Protocol: "near",
+		Description: "This procedure will poll your table routes for the latest block number, and then send the request" +
+			" to routes at the max block number seen and then return the first successful response.",
+		OrderedSteps: []iris_programmable_proxy_v1_beta.IrisRoutingProcedureStep{getBlockHeightProcedure, getBlockProcedure},
+	}, {
+		Name:     "btc_maxBlockAggReduce",
+		Protocol: "btc",
+		Description: "This procedure will poll your table routes for the latest block number, and then send the request" +
+			" to routes at the max block number seen and then return the first successful response.",
 		OrderedSteps: []iris_programmable_proxy_v1_beta.IrisRoutingProcedureStep{getBlockHeightProcedure, getBlockProcedure},
 	},
 	}

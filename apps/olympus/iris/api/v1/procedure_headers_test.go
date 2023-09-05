560e2176f1d5144c623d4851b2a943719f9ff3c0
alex <alexg89@vt.edu>
2023-09-05 13:05:33 -0700

    add test wip

--- apps/olympus/iris/api/v1/procedure_headers_test.go
+++ apps/olympus/iris/api/v1/procedure_headers_test.go
@@ -2,13 +2,16 @@ package v1_iris
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	iris_api_requests "github.com/zeus-fyi/olympus/pkg/iris/proxy/orchestrations/api_requests"
 	"github.com/zeus-fyi/olympus/pkg/utils/test_utils/test_suites/test_suites_base"
+	"github.com/zeus-fyi/zeus/zeus/iris_programmable_proxy"
 	iris_programmable_proxy_v1_beta "github.com/zeus-fyi/zeus/zeus/iris_programmable_proxy/v1beta"
+	resty_base "github.com/zeus-fyi/zeus/zeus/z_client/base"
 )
 
 type IrisV1TestSuite struct {
@@ -22,8 +25,36 @@ func (s *IrisV1TestSuite) TestEthHeaders() {
 	/*
 		needs to test that
 		1. max block finder
-		2. max block finder with fan in for main method request
+			a. etl attributes: IrisRoutingResponseETL
+				a1. extraction key: eg. result
+				a2. extraction key data type: eg. int
+				a3. agg op: eg. max
+				a4. fan-in rule: eg. returnOnFirstSuccess
+		2. needs to update so that it works with json-rpc to replace procedure headers & any method chain
 	*/
+
+	groupName := "ethereum-mainnet"
+	irisClient := resty_base.GetBaseRestyClient("https://iris.zeus.fyi/v1/router", s.Tc.ProductionLocalTemporalBearerToken)
+	irisClient.Header.Set("Content-Type", "application/json")
+	irisClient.Header.Set(iris_programmable_proxy.RouteGroupHeader, groupName)
+	irisClient.Header.Set(iris_programmable_proxy_v1_beta.LoadBalancingStrategy, iris_programmable_proxy_v1_beta.Adaptive)
+
+	payload := `{
+		"jsonrpc": "2.0",
+		"procedure": "eth_maxBlockAggReduce",
+		"method": "eth_getBlockByNumber",
+		"params": ["latest", true],
+		"id": 1
+	}`
+	resp, err := irisClient.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(payload).
+		Post("/")
+
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	fmt.Println(resp.String())
+
 }
 
 func (s *IrisV1TestSuite) TestHeaders() {

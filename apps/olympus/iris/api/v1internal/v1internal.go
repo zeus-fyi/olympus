980d028d64d2f9b9f39df2606941eb71e828306f
alex <alexg89@vt.edu>
2023-09-18 18:13:48 -0700

    adds v2 router path

--- apps/olympus/iris/api/v1internal/v1internal.go
+++ apps/olympus/iris/api/v1internal/v1internal.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/read/auth"
+	v1_iris "github.com/zeus-fyi/olympus/iris/api/v1"
 )
 
 const (
@@ -54,4 +55,24 @@ func InitV1InternalRoutes(e *echo.Echo) {
 	eg.DELETE(DeleteQnOrgAuthCache, InternalDeleteQnOrgAuthCacheHandler)
 
 	eg.DELETE(DeleteSessionAuthCache, InternalDeleteSessionAuthCacheHandler)
+}
+
+func InitV2InternalRoutes1(e *echo.Echo) {
+	eg := e.Group("/v2/internal")
+	eg.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		AuthScheme: "Bearer",
+		Validator: func(token string, c echo.Context) (bool, error) {
+			ctx := context.Background()
+			key, err := auth.VerifyInternalBearerToken(ctx, token)
+			if err != nil {
+				log.Err(err).Msg("InitV1InternalRoutes")
+				return false, c.JSON(http.StatusUnauthorized, nil)
+			}
+			ou := org_users.NewOrgUserWithID(key.OrgID, key.GetUserID())
+			c.Set("orgUser", ou)
+			c.Set("bearer", key.PublicKey)
+			return key.PublicKeyVerified, err
+		},
+	}))
+	v1_iris.AddIrisRouter(eg)
 }

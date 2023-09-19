7b76ed5dc377ee661420e9676d2ad299264a054d
alex <alexg89@vt.edu>
2023-09-18 23:53:25 -0700

    adds digits cap

--- datastores/redis/apps/iris/adaptive_scores.go
+++ datastores/redis/apps/iris/adaptive_scores.go
@@ -246,7 +246,7 @@ func (m *IrisCache) SetLatestAdaptiveEndpointPriorityScoreAndUpdateRateUsage(ctx
 
 	// todo tune this
 	if stats.MemberRankScoreIn.Score > 1 {
-		if stats.MemberRankScoreIn.Score > 10000 && stats.DecayScaleFactor > 0.90 {
+		if stats.MemberRankScoreIn.Score > 10000 && stats.DecayScaleFactor > 0.80 {
 			stats.DecayScaleFactor = 0.80
 		}
 		stats.MemberRankScoreIn.Score *= stats.DecayScaleFactor

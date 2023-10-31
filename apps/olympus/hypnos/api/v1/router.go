e6b40e22d5371e781088cf1ed9aa5e4661cad9a9
alex <alexg89@vt.edu>
2023-10-30 17:42:29 -0700

    adds hypnos

--- apps/olympus/hypnos/api/v1/router.go
+++ apps/olympus/hypnos/api/v1/router.go
@@ -18,7 +18,9 @@ func Routes(e *echo.Echo) *echo.Echo {
 	// Routes
 	e.GET("/health", Health)
 
-	e.POST("/", RpcLoadBalancerRequestHandler("POST"))
+	e.POST("/resp", RpcLoadBalancerRequestHandler("POST"))
+
+	e.POST("/", RpcLoadBalancerRequestHandler2("POST"))
 	return e
 }
 
@@ -41,6 +43,42 @@ func RpcLoadBalancerRequestHandler(method string) func(c echo.Context) error {
 		rw := iris_api_requests.NewIrisApiRequestsActivities()
 		req := &iris_api_requests.ApiProxyRequest{
 			Url:              "http://localhost:8545",
+			Payload:          request.Body,
+			PayloadTypeREST:  method,
+			IsInternal:       true,
+			Timeout:          1 * time.Minute,
+			StatusCode:       http.StatusOK, // default
+			PayloadSizeMeter: payloadSizingMeter,
+		}
+		resp, err := rw.ExtLoadBalancerRequest(context.Background(), req)
+		if err != nil {
+			log.Err(err).Msgf("Hypnos: RpcLoadBalancerRequestHandler: rw.ExtLoadBalancerRequest")
+			return err
+		}
+		return c.JSON(resp.StatusCode, resp.Response)
+	}
+}
+
+func RpcLoadBalancerRequestHandler2(method string) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		bodyBytes, err := io.ReadAll(c.Request().Body)
+		if err != nil {
+			log.Err(err)
+			return err
+		}
+
+		payloadSizingMeter := iris_usage_meters.NewPayloadSizeMeter(bodyBytes)
+		request := new(v1_iris.ProxyRequest)
+		request.Body = echo.Map{}
+
+		if err = json.NewDecoder(payloadSizingMeter).Decode(&request.Body); err != nil {
+			log.Err(err).Msgf("Hypnos: RpcLoadBalancerRequestHandler: json.NewDecoder.Decode")
+			return err
+		}
+		irisBetaSvc := "http://iris.iris.svc.cluster.local/v1/internal/router"
+		rw := iris_api_requests.NewIrisApiRequestsActivities()
+		req := &iris_api_requests.ApiProxyRequest{
+			Url:              irisBetaSvc,
 			Payload:          request.Body,
 			PayloadTypeREST:  method,
 			IsInternal:       true,

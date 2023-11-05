eafbf2a84d26d77793dd457d6b521209022ea6b7
alex <alexg89@vt.edu>
2023-11-05 12:01:38 -0800

    adds top k tokens

--- pkg/artemis/trading/reporting/call_bundle.go
+++ pkg/artemis/trading/reporting/call_bundle.go
@@ -48,7 +48,6 @@ func InsertCallBundleResp(ctx context.Context, builder string, protocolID int, c
 	eventID := ts.UnixTimeStampNow()
 
 	jsonStr := strconv.QuoteToASCII(string(b))
-
 	_, err = apps.Pg.Exec(ctx, q.RawQuery, eventID, builder, callBundlesResp.BundleHash, protocolID, []byte(jsonStr))
 	if err == pgx.ErrNoRows {
 		err = nil

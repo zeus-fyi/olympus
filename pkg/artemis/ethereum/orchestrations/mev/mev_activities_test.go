8f8e724ad6da548a72720ed98c06b9805e62e441
alex <alexg89@vt.edu>
2023-11-07 14:14:20 -0800

    update err

--- pkg/artemis/ethereum/orchestrations/mev/mev_activities_test.go
+++ pkg/artemis/ethereum/orchestrations/mev/mev_activities_test.go
@@ -2,9 +2,11 @@ package artemis_mev_transcations
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/gochain/web3/accounts"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
 	artemis_orchestration_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/orchestration_auth"
 	"github.com/zeus-fyi/olympus/pkg/artemis/web3_client"
@@ -46,6 +48,24 @@ func (s *MevActivitiesTestSuite) TestMonitor() {
 	list, err := aa.MonitorTxStatusReceipts(ctx)
 	s.NoError(err)
 	s.NotEmpty(list)
+}
+
+func (s *MevActivitiesTestSuite) TestWaitForTx() {
+	apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+	beaconPath := "https://iris.zeus.fyi/v1/router"
+	wc := web3_client.NewWeb3ClientFakeSigner(beaconPath)
+	wc.AddBearerToken(s.Tc.ProductionLocalTemporalBearerToken)
+	wc.AddDefaultEthereumMainnetTableHeader()
+	aa := NewArtemisMevActivities(wc)
+
+	ha := accounts.HexToHash("0x7f6b9610da77c92960660af86b6e136668d57ba26f525f2a0033b249ad2c6d4b")
+	_, err := aa.WaitForTxReceipt(ctx, ha)
+	if strings.Contains(err.Error(), "not found") {
+		err = nil
+	}
+
+	s.NoError(err)
+	//	s.NotEmpty(rx)
 
 }
 

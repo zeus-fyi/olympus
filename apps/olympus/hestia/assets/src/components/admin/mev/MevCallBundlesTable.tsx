425324c20404fc5c6e897c69f24254a8a5c7c480
alex <alexg89@vt.edu>
2023-11-07 16:20:42 -0800

    adds pair address

--- apps/olympus/hestia/assets/src/components/admin/mev/MevCallBundlesTable.tsx
+++ apps/olympus/hestia/assets/src/components/admin/mev/MevCallBundlesTable.tsx
@@ -110,7 +110,7 @@ function CallBundlesRow(props: { row: ReturnType<typeof createCallBundleData> })
                 <TableCell align="left">{row.gasFees + ' Eth'}</TableCell>
             </TableRow>
             <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={10}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box sx={{ margin: 1 }}>
                             <Typography variant="h6" gutterBottom component="div">

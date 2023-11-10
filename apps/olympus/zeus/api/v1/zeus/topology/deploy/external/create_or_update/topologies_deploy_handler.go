6409a6e7a9885a642f7c5fe235ce1dd34dd4dce8
alex <alexg89@vt.edu>
2023-11-10 15:40:05 -0800

    add more logs & checks

--- apps/olympus/zeus/api/v1/zeus/topology/deploy/external/create_or_update/topologies_deploy_handler.go
+++ apps/olympus/zeus/api/v1/zeus/topology/deploy/external/create_or_update/topologies_deploy_handler.go
@@ -3,6 +3,7 @@ package create_or_update_deploy
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -26,11 +27,15 @@ func TopologyDeploymentHandler(c echo.Context) error {
 	}
 	authed, err := read_topology.IsOrgCloudCtxNsAuthorized(ctx, ou.OrgID, request.TopologyKubeCtxNs.CloudCtxNs)
 	if authed != true || err != nil {
-		log.Ctx(ctx).Err(err).Msg("ClusterTopologyDeploymentHandler: IsOrgCloudCtxNsAuthorized")
+		if err == nil {
+			err = fmt.Errorf("not authorized")
+		}
+		log.Err(err).Interface("ou", ou).Interface("req", request).Msg("ClusterTopologyDeploymentHandler: IsOrgCloudCtxNsAuthorized")
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
 	if request.TopologyKubeCtxNs.TopologyID == 0 {
 		err = errors.New("no topology id provided")
+		log.Err(err).Interface("ou", ou).Interface("req", request).Msg("ClusterTopologyDeploymentHandler: IsOrgCloudCtxNsAuthorized")
 		return c.JSON(http.StatusBadRequest, err)
 	}
 	return request.DeployTopology(c)

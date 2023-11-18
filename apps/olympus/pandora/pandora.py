5d4918e5a64bd57c7477b554a1bf88bcb960709d
alex <alexg89@vt.edu>
2023-11-18 12:49:07 -0800

    adds wf

--- apps/olympus/pandora/pandora.py
+++ apps/olympus/pandora/pandora.py
@@ -20,13 +20,13 @@ API_ID = int(os.environ['TELEGRAM_API_ID'])
 API_HASH = os.environ['TELEGRAM_API_HASH']
 
 
-async def start_client( token_in):
+async def start_client(token_in, group):
     client = TelegramClient('session_name', API_ID, API_HASH)
     await client.start(phone=lambda: get_number(), code_callback=lambda: token_in)
     await client.connect()
     msgs = []
     async for dialog in client.iter_dialogs():
-        if dialog.is_group and str.startswith(dialog.name, 'Lay'):
+        if dialog.is_group and str.startswith(dialog.name, group):
             print(f'Group name: {dialog.name}')
 
             messages = await client.get_messages(dialog, limit=10)
@@ -39,12 +39,16 @@ async def start_client( token_in):
     client.disconnect()
     return msgs
 
+
 @app.route('/msgs', methods=['POST'])
 async def initialize_telegram_client_endpoint():
     token = request.json.get('token')
     if not token:
         return jsonify({'error': 'Token is required'}), 400
-    return await start_client(token)
+    group = request.json.get('group')
+    if not group:
+        return jsonify({'error': 'Group is required'}), 400
+    return await start_client(token, group)
 
 
 if __name__ == '__main__':

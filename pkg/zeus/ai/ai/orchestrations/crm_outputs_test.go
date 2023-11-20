88465234c5a3d0cb69d8078c9cec03f5d6b55bee
alex <alexg89@vt.edu>
2023-11-19 22:57:33 -0800

    adds search analysis cache

---
+++ pkg/zeus/ai/ai/orchestrations/crm_outputs_test.go
@@ -0,0 +1,34 @@
+package ai_platform_service_orchestrations
+
+import (
+	hera_search "github.com/zeus-fyi/olympus/datastores/postgres/apps/hera/models/search"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
+	artemis_hydra_orchestrations_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/validator_signature_requests/aws_auth"
+	hera_openai "github.com/zeus-fyi/olympus/pkg/hera/openai"
+	artemis_orchestration_auth "github.com/zeus-fyi/olympus/pkg/zeus/topologies/orchestrations/orchestration_auth"
+	aegis_aws_auth "github.com/zeus-fyi/zeus/pkg/aegis/aws/auth"
+)
+
+func (t *ZeusWorkerTestSuite) TestAiTelegramTask() {
+	ou := org_users.NewOrgUserWithID(7138983863666903883, 7138958574876245567)
+	artemis_orchestration_auth.Bearer = t.Tc.ProductionLocalTemporalBearerToken
+	auth := aegis_aws_auth.AuthAWS{
+		Region:    "us-west-1",
+		AccessKey: t.Tc.AwsAccessKeySecretManager,
+		SecretKey: t.Tc.AwsSecretKeySecretManager,
+	}
+	artemis_hydra_orchestrations_auth.InitHydraSecretManagerAuthAWS(ctx, auth)
+	msgs, err := GetPandoraMessages(ctx, "Zeus")
+	t.Require().Nil(err)
+	t.Require().NotNil(msgs)
+	params := hera_search.AiSearchParams{
+		GroupFilter: "Zeus <> Li",
+	}
+	hera_openai.InitHeraOpenAI(t.Tc.OpenAIAuth)
+	// resty_base.GetBaseRestyClient("https://api.openai.com", bearer)
+	sr, err := hera_search.SearchTelegram(ctx, ou, params)
+	resp, err := AiTelegramTask(ctx, ou, sr, params)
+	t.Require().Nil(err)
+	t.Require().NotNil(resp)
+
+}

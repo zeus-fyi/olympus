f7bfa4035e1f214d9705b00e1d04c04fc4e575fa
alex <alexg89@vt.edu>
2023-11-24 15:17:52 -0800

    adds ingest

--- pkg/zeus/ai/orchestrations/workflows_discord.go
+++ pkg/zeus/ai/orchestrations/workflows_discord.go
@@ -91,7 +91,8 @@ func (h *ZeusAiPlatformServiceWorkflows) AiFetchDataToIngestDiscordWorkflow(ctx 
 	}
 	for _, jib := range sq.Results {
 		jobCtx := workflow.WithActivityOptions(ctx, ao)
-		timeAfter := time.Unix(int64(jib.MaxMessageID), 0).Format(time.RFC3339)
+		seconds := jib.MaxMessageID / 1e9
+		timeAfter := time.Unix(int64(seconds), 0).Format(time.RFC3339)
 		err = workflow.ExecuteActivity(jobCtx, h.CreateDiscordJob, sq.SearchID, jib.ChannelID, timeAfter).Get(jobCtx, nil)
 		if err != nil {
 			logger.Error("failed to execute CreateDiscordJob", "Error", err)

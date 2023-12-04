df90bf6d103662fdbbdb65dd204fcfbbb0a6f37d
alex <alexg89@vt.edu>
2023-12-03 17:28:37 -0800

    inserts retrieval

--- datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals.go
+++ datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals.go
@@ -1,1 +1,37 @@
 package artemis_orchestrations
+
+import (
+	"context"
+
+	"github.com/jackc/pgtype"
+	"github.com/rs/zerolog/log"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
+	"github.com/zeus-fyi/olympus/pkg/utils/string_utils/sql_query_templates"
+)
+
+type RetrievalItem struct {
+	RetrievalID    int    `json:"retrievalID"`    // ID of the retrieval
+	RetrievalName  string `json:"retrievalName"`  // Name of the retrieval
+	RetrievalGroup string `json:"retrievalGroup"` // Group of the retrieval
+}
+
+func InsertRetrieval(ctx context.Context, ou org_users.OrgUser, item *RetrievalItem, b []byte) error {
+	q := sql_query_templates.QueryParams{}
+	q.RawQuery = `
+        INSERT INTO public.ai_retrieval_library (org_id, user_id, retrieval_name, retrieval_group, instructions)
+        VALUES ($1, $2, $3, $4, $5)
+        ON CONFLICT (org_id, retrieval_name) 
+        DO UPDATE SET 
+            user_id = EXCLUDED.user_id,
+            retrieval_group = EXCLUDED.retrieval_group,
+            instructions = EXCLUDED.instructions
+        RETURNING retrieval_id;`
+	// Executing the query
+	err := apps.Pg.QueryRowWArgs(ctx, q.RawQuery, ou.OrgID, ou.UserID, item.RetrievalName, item.RetrievalGroup, &pgtype.JSONB{Bytes: sanitizeBytesUTF8(b), Status: IsNull(b)}).Scan(&item.RetrievalID)
+	if err != nil {
+		log.Err(err).Msg("failed to insert retrieval")
+		return err
+	}
+	return nil
+}
--- datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals_test.go
+++ datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals_test.go
@@ -1,8 +1,26 @@
 package artemis_orchestrations
 
-import "github.com/zeus-fyi/olympus/datastores/postgres/apps"
+import (
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
+)
 
 func (s *OrchestrationsTestSuite) TestInsertRetrieval() {
-	apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+	apps.Pg.InitPG(ctx, s.Tc.LocalDbPgconn)
+	ou := org_users.OrgUser{}
+	ou.OrgID = s.Tc.ProductionLocalTemporalOrgID
+	ou.UserID = s.Tc.ProductionLocalTemporalUserID
 
+	mockRetrievalItem := RetrievalItem{
+		RetrievalName:  "TestRetrieval",
+		RetrievalGroup: "TestGroup1",
+	}
+
+	// Example instructions in byte array
+	instructions := []byte(`{"key": "value"}`)
+
+	// Step 2: Call InsertRetrieval
+	err := InsertRetrieval(ctx, ou, &mockRetrievalItem, instructions)
+	s.Require().Nil(err)
+	s.Require().NotZero(mockRetrievalItem.RetrievalID)
 }

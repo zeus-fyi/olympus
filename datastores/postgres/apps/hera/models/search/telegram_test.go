4a88e6766804fc9902648f4d0bcf0cf94ed260df
alex <alexg89@vt.edu>
2023-12-08 11:27:44 -0800

    update search

--- datastores/postgres/apps/hera/models/search/telegram_test.go
+++ datastores/postgres/apps/hera/models/search/telegram_test.go
@@ -1,1 +1,47 @@
 package hera_search
+
+import (
+	"time"
+
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/artemis/models/artemis_orchestrations"
+	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
+)
+
+func (s *SearchAITestSuite) TestSelectTelegramResults() {
+	apps.Pg.InitPG(ctx, s.Tc.ProdLocalDbPgconn)
+	ou := org_users.OrgUser{}
+	ou.OrgID = s.Tc.ProductionLocalTemporalOrgID
+	ou.UserID = s.Tc.ProductionLocalTemporalUserID
+
+	si := artemis_orchestrations.Window{}
+	si.Start = time.Now().AddDate(0, 0, -60)
+	si.End = time.Now()
+
+	sp := AiSearchParams{
+		Retrieval: artemis_orchestrations.RetrievalItem{
+			RetrievalID:    0,
+			RetrievalName:  "",
+			RetrievalGroup: "",
+			RetrievalItemInstruction: artemis_orchestrations.RetrievalItemInstruction{
+				RetrievalPlatform:       "telegram",
+				RetrievalPrompt:         "",
+				RetrievalPlatformGroups: "Ze",
+				RetrievalKeywords:       "",
+				RetrievalUsernames:      "",
+				DiscordFilters:          nil,
+			},
+			Instructions: nil,
+		},
+		TimeRange: "",
+		Window:    si,
+	}
+	res, err := SearchTelegram(ctx, ou, sp)
+	s.Require().Nil(err)
+	s.Assert().NotEmpty(res)
+
+	ou.OrgID = 0
+	res, err = SearchTelegram(ctx, ou, sp)
+	s.Require().Nil(err)
+	s.Assert().Nil(res)
+}

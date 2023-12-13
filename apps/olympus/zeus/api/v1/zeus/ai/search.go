e5526061a228788b6f7f6737cca3f230452aa915
alex <alexg89@vt.edu>
2023-12-12 21:48:41 -0800

    empty platform search all

--- apps/olympus/zeus/api/v1/zeus/ai/search.go
+++ apps/olympus/zeus/api/v1/zeus/ai/search.go
@@ -46,11 +46,13 @@ func (r *AiSearchRequest) Search(c echo.Context) error {
 		r.Window.Start = ts.AddDate(0, 0, -30)
 		r.Window.End = ts
 	case "all":
-		r.Window.Start = ts.AddDate(-4, 0, 0)
+		r.Window.Start = time.Unix(0, 0)
 		r.Window.End = ts
 	case "window":
 		log.Info().Interface("searchInterval", r.Window).Msg("window")
 	}
+	r.Window.UnixStartTime = int(r.Window.Start.Unix())
+	r.Window.UnixEndTime = int(r.Window.End.Unix())
 	res, err := hera_search.PerformPlatformSearches(c.Request().Context(), ou, r.AiSearchParams)
 	if err != nil {
 		log.Err(err).Interface("ou", ou).Msg("error performing platform searches")

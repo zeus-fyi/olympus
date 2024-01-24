9ce1a87169d36c7d4b9c48d7af8fcd9caf81f7de
alex <alexg89@vt.edu>
2024-01-23 23:36:33 -0800

    fix task select to eval fns on tasks

--- datastores/postgres/apps/artemis/models/artemis_orchestrations/tasks.go
+++ datastores/postgres/apps/artemis/models/artemis_orchestrations/tasks.go
@@ -98,22 +98,23 @@ func SelectTask(ctx context.Context, ou org_users.OrgUser, taskID int) ([]AITask
 		queryAddOn = "AND tl.task_id = $2"
 	}
 	query := `
-        WITH cte_tasks AS (
-            SELECT 
-                tl.task_id, 
-                tl.max_tokens_per_task, 
-                tl.task_type,
-                tl.task_name, 
-                tl.task_group, 
-                tl.token_overflow_strategy, 
-                tl.model, 
-                tl.prompt, 
-                tl.response_format
-            FROM 
-                public.ai_task_library tl
-            WHERE 
-                tl.org_id = $1 ` + queryAddOn + ` 
-			), cte_tasks_0 AS (
+			WITH cte_tasks AS (
+				SELECT 
+					tl.task_id, 
+					tl.max_tokens_per_task, 
+					tl.task_type,
+					tl.task_name, 
+					tl.task_group, 
+					tl.token_overflow_strategy, 
+					tl.model, 
+					tl.prompt, 
+					tl.response_format
+				FROM 
+					public.ai_task_library tl
+        		WHERE 
+            		tl.org_id = $1 ` + queryAddOn + ` 
+			),
+			cte_tasks_0 AS (
 				SELECT 
 					tl.task_id, 
 					tl.max_tokens_per_task, 
@@ -128,7 +129,9 @@ func SelectTask(ctx context.Context, ou org_users.OrgUser, taskID int) ([]AITask
 				FROM  
 					cte_tasks tl
 				LEFT JOIN ai_workflow_template_eval_task_relationships ate ON tl.task_id = ate.task_id
-				GROUP BY tl.task_id, tl.max_tokens_per_task, tl.task_type, tl.task_name, tl.task_group, tl.token_overflow_strategy, tl.model, tl.prompt, tl.response_format, ate.eval_id
+				GROUP BY 
+					tl.task_id, tl.max_tokens_per_task, tl.task_type, tl.task_name, tl.task_group,
+					tl.token_overflow_strategy, tl.model, tl.prompt, tl.response_format, ate.eval_id
 			),
 			cte_1 AS (
 				SELECT 
@@ -150,10 +153,11 @@ func SelectTask(ctx context.Context, ou org_users.OrgUser, taskID int) ([]AITask
 					af.field_name, 
 					af.field_description,
 					af.data_type
-				FROM cte_tasks_0 tl
+				FROM 
+					cte_tasks_0 tl
 				JOIN ai_task_schemas ats ON ats.task_id = tl.task_id
-					JOIN public.ai_json_schema_definitions jsd ON ats.schema_id = jsd.schema_id
-					JOIN public.ai_fields af ON ats.schema_id = af.schema_id AND af.is_field_archived = false
+				JOIN public.ai_json_schema_definitions jsd ON ats.schema_id = jsd.schema_id
+				JOIN public.ai_fields af ON ats.schema_id = af.schema_id AND af.is_field_archived = false
 				GROUP BY 
 					tl.task_id, 
 					tl.max_tokens_per_task, 
@@ -168,56 +172,58 @@ func SelectTask(ctx context.Context, ou org_users.OrgUser, taskID int) ([]AITask
 					jsd.schema_id, jsd.schema_name, jsd.schema_group, jsd.is_obj_array,
 					af.field_id
 			),
-cte_2 AS (
-    SELECT
-        tl.task_id,
-        tl.eval_id,
-        tl.field_id,
-        tl.field_name,
-        tl.field_description,
-        tl.data_type,
-        tl.schema_id,
-				tl.is_obj_array,
-        tl.schema_name,
-        tl.schema_group,
-				jsonb_agg(
-            jsonb_build_object(
-                'evalMetricID', evm.eval_metric_id,
-                'evalComparisonNumber', evm.eval_comparison_number,
-                'evalComparisonBoolean', evm.eval_comparison_boolean,
-                'evalComparisonString', evm.eval_comparison_string,
-                'evalOperator', evm.eval_operator,
-                'evalState', evm.eval_state,
-                'evalExpectedResultState', evm.eval_metric_result
-        )) AS eval_metrics_jsonb
-    FROM  
-        cte_1 tl
+			cte_2 AS (
+				SELECT
+					tl.task_id,
+					tl.eval_id,
+					tl.field_id,
+					tl.field_name,
+					tl.field_description,
+					tl.data_type,
+					tl.schema_id,
+					tl.is_obj_array,
+					tl.schema_name,
+					tl.schema_group,
+					jsonb_agg(
+						jsonb_build_object(
+							'evalMetricID', evm.eval_metric_id,
+							'evalComparisonNumber', evm.eval_comparison_number,
+							'evalComparisonBoolean', evm.eval_comparison_boolean,
+							'evalComparisonString', evm.eval_comparison_string,
+							'evalOperator', evm.eval_operator,
+							'evalState', evm.eval_state,
+							'evalExpectedResultState', evm.eval_metric_result
+					)) AS eval_metrics_jsonb
+				FROM  
+					cte_1 tl
 					LEFT JOIN public.eval_metrics evm ON evm.eval_id = tl.eval_id AND evm.is_eval_metric_archived = false AND evm.eval_state != 'ignore'
-    GROUP BY tl.task_id, tl.eval_id, tl.schema_id, tl.field_id, tl.field_name,
-        tl.field_description, tl.data_type, tl.schema_name, tl.schema_group, tl.is_obj_array
-),
-cte_schema_definitions AS (
-    SELECT 
-        te.task_id,
-        te.eval_id,
-        te.schema_id,
-		te.is_obj_array,
-        te.schema_name,
-        te.schema_group,
-        jsonb_agg(
-            jsonb_build_object(
-                'fieldID', te.field_id,
-                'fieldName', te.field_name,
-                'fieldDescription', te.field_description,
-                'dataType', te.data_type,
-                'evalMetrics', COALESCE(te.eval_metrics_jsonb, '[]'::jsonb)
-            )
-        ) AS eval_fn_metrics_schemas_jsonb
-    FROM 
-        cte_2 te
-    GROUP BY 
-        te.task_id, te.eval_id, te.schema_id, te.schema_name, te.schema_group,	te.is_obj_array
-			), cte_xy AS (
+				GROUP BY 
+					tl.task_id, tl.eval_id, tl.schema_id, tl.field_id, tl.field_name,
+					tl.field_description, tl.data_type, tl.schema_name, tl.schema_group, tl.is_obj_array
+			),
+			cte_schema_definitions AS (
+				SELECT 
+					te.task_id,
+					te.eval_id,
+					te.schema_id,
+					te.is_obj_array,
+					te.schema_name,
+					te.schema_group,
+					jsonb_agg(
+						jsonb_build_object(
+							'fieldID', te.field_id,
+							'fieldName', te.field_name,
+							'fieldDescription', te.field_description,
+							'dataType', te.data_type,
+							'evalMetrics', COALESCE(te.eval_metrics_jsonb, '[]'::jsonb)
+						)
+					) AS eval_fn_metrics_schemas_jsonb
+				FROM 
+					cte_2 te
+				GROUP BY 
+					te.task_id, te.eval_id, te.schema_id, te.schema_name, te.schema_group, te.is_obj_array
+			),
+			cte_xy AS (
 				SELECT 
 					ct.task_id,
 					ct.eval_id,
@@ -228,39 +234,41 @@ cte_schema_definitions AS (
 						'schemaGroup', ct.schema_group,
 						'isObjArray', ct.is_obj_array,
 						'fields', ct.eval_fn_metrics_schemas_jsonb 
-				   ) AS task_schemas_jsonb
+					) AS task_schemas_jsonb
 				FROM 
 					cte_schema_definitions ct
 				GROUP BY 
-					ct.task_id, ct.eval_id, ct.schema_id, ct.schema_name,  ct.schema_group,ct.is_obj_array, ct.eval_fn_metrics_schemas_jsonb
-			), cte_eval_fn_slice AS (
+					ct.task_id, ct.eval_id, ct.schema_id, ct.schema_name, ct.schema_group, ct.is_obj_array, ct.eval_fn_metrics_schemas_jsonb
+			),
+			cte_eval_fn_slice AS (
 				SELECT 
 					ct.task_id,
 					ct.eval_id,
-						jsonb_build_object(
-							'evalID', ct.eval_id,
-							'evalName', evf.eval_name,
-							'evalType', evf.eval_type,
-							'evalGroupName', evf.eval_group_name,
-							'evalModel', evf.eval_model,
-							'evalFormat', evf.eval_format,
-							'schemas', jsonb_agg(ct.task_schemas_jsonb)
-
+					jsonb_build_object(
+						'evalID', ct.eval_id,
+						'evalName', evf.eval_name,
+						'evalType', evf.eval_type,
+						'evalGroupName', evf.eval_group_name,
+						'evalModel', evf.eval_model,
+						'evalFormat', evf.eval_format,
+						'schemas', jsonb_agg(ct.task_schemas_jsonb)
 					) AS eval_fn_metrics_jsonb
-						FROM 
-							cte_xy ct
-						JOIN eval_fns evf ON evf.eval_id = ct.eval_id
-						GROUP BY 
-							ct.task_id,ct.eval_id, evf.eval_name,evf.eval_group_name,evf.eval_type, evf.eval_model,evf.eval_format
-			), cte_abc AS (
-							SELECT 
-							ct.task_id,
-							jsonb_agg(eval_fn_metrics_jsonb) AS eval_fn_metrics_jsonb
-						FROM 
-							cte_eval_fn_slice ct
-						GROUP BY 
-							ct.task_id
-			), cte_task_schema_definitions AS (
+				FROM 
+					cte_xy ct
+				JOIN eval_fns evf ON evf.eval_id = ct.eval_id
+				GROUP BY 
+					ct.task_id, ct.eval_id, evf.eval_name, evf.eval_group_name, evf.eval_type, evf.eval_model, evf.eval_format
+			),
+			cte_abc AS (
+				SELECT 
+					ct.task_id,
+					jsonb_agg(eval_fn_metrics_jsonb) AS eval_fn_metrics_jsonb
+				FROM 
+					cte_eval_fn_slice ct
+				GROUP BY 
+					ct.task_id
+			),
+			cte_task_schema_definitions AS (
 				SELECT 
 					te.task_id,
 					jsd.schema_id,
@@ -282,28 +290,29 @@ cte_schema_definitions AS (
 					JOIN public.ai_fields af ON ats.schema_id = af.schema_id AND af.is_field_archived = false
 				GROUP BY 
 					te.task_id, jsd.schema_id, jsd.schema_name, jsd.schema_group, jsd.is_obj_array
-			), 
+			),
 			cte_task_schemas AS (
 				SELECT 
 					ct.task_id,
-							ct.schema_id,
+					ct.schema_id,
 					jsonb_build_object(
 						'schemaID', ct.schema_id,
 						'schemaName', ct.schema_name,
 						'schemaGroup', ct.schema_group,
 						'isObjArray', ct.is_obj_array,
-						'fields', ct.fields  -- Corrected here by adding a comma before 'fields'
-				   ) AS task_schemas_jsonb
+						'fields', ct.fields
+					) AS task_schemas_jsonb
 				FROM 
 					cte_task_schema_definitions ct
 				GROUP BY 
-					ct.task_id, ct.schema_id, ct.schema_name,  ct.schema_group,ct.is_obj_array, ct.fields
+					ct.task_id, ct.schema_id, ct.schema_name, ct.schema_group, ct.is_obj_array, ct.fields
 			),
 			cte_x AS (
 				SELECT 
 					ct.task_id,
 					jsonb_agg(ct.task_schemas_jsonb) AS task_schemas_jsonb
-				FROM  cte_task_schemas ct
+				FROM 
+					cte_task_schemas ct
 				GROUP BY 
 					ct.task_id
 			)
@@ -326,6 +335,7 @@ cte_schema_definitions AS (
 			ORDER BY 
 				ct.task_id DESC;`
 	// Executing the query
+
 	rows, err := apps.Pg.Query(ctx, query, params...)
 	if err != nil {
 		if err == pgx.ErrNoRows {

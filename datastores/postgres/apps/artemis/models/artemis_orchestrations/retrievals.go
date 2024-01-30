a8d7200784f6fb29b53f99732ac3fc2359f8ec0b
alex <alexg89@vt.edu>
2024-01-29 20:23:14 -0800

    update retrievals

--- datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals.go
+++ datastores/postgres/apps/artemis/models/artemis_orchestrations/retrievals.go
@@ -33,7 +33,7 @@ type RetrievalItemInstruction struct {
 	DiscordFilters            *DiscordFilters `json:"discordFilters,omitempty"`            // Discord filters for the retrieval
 	WebFilters                *WebFilters     `json:"webFilters,omitempty"`                // Web filters for the retrieval
 
-	Instructions json.RawMessage `json:"instructions"` // Instructions for the retrieval
+	Instructions json.RawMessage `json:"instructions,omitempty"` // Instructions for the retrieval
 }
 
 func (r *RetrievalItemInstruction) GetNegativeKeywords() {

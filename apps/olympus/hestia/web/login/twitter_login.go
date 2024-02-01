5f2f5f3b0f23f0d5f4db88c3284b94fa27d4e088
alex <alexg89@vt.edu>
2024-01-31 18:16:22 -0800

    adds session

--- apps/olympus/hestia/web/login/twitter_login.go
+++ apps/olympus/hestia/web/login/twitter_login.go
@@ -4,13 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/oauth2"
 )
 
 var TwitterOAuthConfig = &oauth2.Config{}
 
 func CallbackHandler(c echo.Context) error {
-	return c.Redirect(http.StatusTemporaryRedirect, TwitterOAuthConfig.AuthCodeURL(state))
+	redirectURL := TwitterOAuthConfig.AuthCodeURL(state)
+	log.Info().Msgf("Redirecting to %s", redirectURL)
+	return c.Redirect(http.StatusTemporaryRedirect, redirectURL)
 }
 
 func LogoutHandler(c echo.Context) error {

0998540ae83784889573683cb460525f4d02ff1c
alex <alexg89@vt.edu>
2024-01-31 19:46:19 -0800

    adds session

--- apps/olympus/hestia/server/hestia.go
+++ apps/olympus/hestia/server/hestia.go
@@ -106,7 +106,7 @@ func Hestia() {
 			log.Warn().Msg("Hestia: TwitterClientID or TwitterClientSecret is empty")
 		}
 
-		authorizeURL := "https://api.twitter.com/oauth2/authorize"
+		authorizeURL := "https://twitter.com/i/oauth2/authorize"
 		tokenURL := "https://api.twitter.com/2/oauth2/token"
 		conf := &oauth2.Config{
 			RedirectURL:  "https://hestia.zeus.fyi/twitter/callback",

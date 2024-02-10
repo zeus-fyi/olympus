060d0b67bc635cce465fbda030ca07878d1f1772
alex <alexg89@vt.edu>
2024-02-09 19:12:36 -0800

    updates view

--- apps/olympus/zeus/pkg/zeus/core.go
+++ apps/olympus/zeus/pkg/zeus/core.go
@@ -10,6 +10,7 @@ import (
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/authorized_clusters"
 	"github.com/zeus-fyi/olympus/datastores/postgres/apps/hestia/models/bases/org_users"
 	"github.com/zeus-fyi/olympus/pkg/aegis/auth_startup"
+	"github.com/zeus-fyi/olympus/pkg/aegis/aws_secrets"
 	"github.com/zeus-fyi/olympus/pkg/utils/file_io/lib/v0/encryption"
 	autok8s_core "github.com/zeus-fyi/olympus/pkg/zeus/core"
 	"github.com/zeus-fyi/zeus/zeus/z_client/zeus_common_types"
@@ -49,12 +50,17 @@ func VerifyClusterAuthAndGetKubeCfgPtr(ctx context.Context, ou org_users.OrgUser
 }
 
 func GetKubeConfig(ctx context.Context, ou org_users.OrgUser, p authorized_clusters.K8sClusterConfig) (autok8s_core.K8Util, error) {
+	k := autok8s_core.K8Util{}
+	_, perr := aws_secrets.GetServiceAccountSecrets(ctx, ou)
+	if perr != nil {
+		log.Err(perr).Interface("ou", ou).Msg("GetExtClusterConfigs: GetServiceAccountSecrets")
+		return k, perr
+	}
 	inKey := &s3.GetObjectInput{
 		Bucket: aws.String("zeus-fyi"),
 		Key:    aws.String(p.GetHashedKey(ou.OrgID) + ".kube.tar.gz.age"),
 	}
 	inMemFsK8sConfig := auth_startup.ExtClustersRunDigitalOceanS3BucketObjAuthProcedure(ctx, KeysCfg, inKey)
-	k := autok8s_core.K8Util{}
 	err := k.ConnectToK8sFromInMemFsCfgPathOrErr(inMemFsK8sConfig)
 	if err != nil {
 		log.Err(err).Interface("key", p.GetHashedKey(ou.OrgID)).Msg("CheckKubeConfig: ConnectToK8sFromInMemFsCfgPathOrErr")

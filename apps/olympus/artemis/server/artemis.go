f098fc968d7acbad2d0e3167f4d945d84e2d670c
alex <alexg89@vt.edu>
2024-04-12 20:29:43 -0700

    change join order

--- apps/olympus/artemis/server/artemis.go
+++ apps/olympus/artemis/server/artemis.go
@@ -1,23 +1,16 @@
 package artemis_server
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	"github.com/go-resty/resty/v2"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	artemis_api_router "github.com/zeus-fyi/olympus/artemis/api"
 	"github.com/zeus-fyi/olympus/pkg/aegis/auth_startup/auth_keys_config"
-	artemis_mev_tx_fetcher "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/mev"
-	artemis_orchestration_auth "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/orchestration_auth"
-	artemis_ethereum_transcations "github.com/zeus-fyi/olympus/pkg/artemis/ethereum/orchestrations/transcations"
 	temporal_auth "github.com/zeus-fyi/olympus/pkg/iris/temporal/auth"
-	"github.com/zeus-fyi/olympus/pkg/utils/misc"
 	resty_base "github.com/zeus-fyi/zeus/zeus/z_client/base"
 )
 
@@ -63,94 +56,94 @@ func Artemis() {
 	}
 	cfg.Host = "0.0.0.0"
 	srv := NewArtemisServer(cfg)
-	// Echo instance
-	ctx := context.Background()
-	SetConfigByEnv(ctx, env)
-
-	// goerli
-	log.Info().Msg("Artemis: Starting ArtemisEthereumGoerliTxBroadcastWorker")
-	c := artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.ConnectTemporalClient()
-	defer c.Close()
-	artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.Worker.RegisterWorker(c)
-	err := artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumGoerliTxBroadcastWorker.Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisEthereumGoerliTxBroadcastWorker Started")
-	log.Info().Msg("Artemis: Starting ArtemisMevWorkerGoerli")
-	artemis_mev_tx_fetcher.ArtemisMevWorkerGoerli.Worker.RegisterWorker(c)
-	err = artemis_mev_tx_fetcher.ArtemisMevWorkerGoerli.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerGoerli Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisMevWorkerGoerli Started")
-	// mainnet
-	log.Info().Msg("Artemis: Starting ArtemisEthereumMainnetTxBroadcastWorker")
-	artemis_ethereum_transcations.ArtemisEthereumMainnetTxBroadcastWorker.Worker.RegisterWorker(c)
-	err = artemis_ethereum_transcations.ArtemisEthereumMainnetTxBroadcastWorker.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumMainnetTxBroadcastWorker.Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisEthereumMainnetTxBroadcastWorker Started")
-
-	log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnet")
-	artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet.Worker.RegisterWorker(c)
-	err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnet Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisMevWorkerMainnet Started")
-
-	log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnet2")
-	artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet2.Worker.RegisterWorker(c)
-	err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet2.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnet1 Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisMevWorkerMainnet2 Started")
-
-	log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnetHistoricalTxs")
-	artemis_mev_tx_fetcher.ArtemisMevWorkerMainnetHistoricalTxs.Worker.RegisterWorker(c)
-	err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnetHistoricalTxs.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnetHistoricalTxs Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisMevWorkerMainnetHistoricalTxs Started")
-	// ephemeral
-	log.Info().Msg("Artemis: Starting ArtemisEthereumEphemeralTxBroadcastWorker")
-	artemis_ethereum_transcations.ArtemisEthereumEphemeralTxBroadcastWorker.Worker.RegisterWorker(c)
-	err = artemis_ethereum_transcations.ArtemisEthereumEphemeralTxBroadcastWorker.Worker.Start()
-	if err != nil {
-		log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumEphemeralTxBroadcastWorker.Worker.Start failed", env)
-		misc.DelayedPanic(err)
-	}
-	log.Info().Msg("Artemis: ArtemisEthereumEphemeralTxBroadcastWorker Started")
-
-	// Start server
-	log.Info().Msg("Artemis: Starting Server")
-	if env == "local" || env == "production-local" {
-		srv.E.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-			AllowOrigins:     []string{"http://localhost:3000"},
-			AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
-			AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-			AllowCredentials: true,
-		}))
-	} else {
-		srv.E.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-			AllowOrigins:     []string{"https://cloud.zeus.fyi", "https://api.zeus.fyi", "https://hestia.zeus.fyi"},
-			AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
-			AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Headers", "X-CSRF-Token", "Accept-Encoding", "CloudCtxNsID"},
-			AllowCredentials: true,
-		}))
-	}
-
-	artemis_mev_tx_fetcher.InitArtemisUniswap(ctx, artemis_orchestration_auth.Bearer)
+	//// Echo instance
+	//ctx := context.Background()
+	//SetConfigByEnv(ctx, env)
+	//
+	//// goerli
+	//log.Info().Msg("Artemis: Starting ArtemisEthereumGoerliTxBroadcastWorker")
+	//c := artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.ConnectTemporalClient()
+	//defer c.Close()
+	//artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.Worker.RegisterWorker(c)
+	//err := artemis_ethereum_transcations.ArtemisEthereumGoerliTxBroadcastWorker.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumGoerliTxBroadcastWorker.Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisEthereumGoerliTxBroadcastWorker Started")
+	//log.Info().Msg("Artemis: Starting ArtemisMevWorkerGoerli")
+	//artemis_mev_tx_fetcher.ArtemisMevWorkerGoerli.Worker.RegisterWorker(c)
+	//err = artemis_mev_tx_fetcher.ArtemisMevWorkerGoerli.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerGoerli Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisMevWorkerGoerli Started")
+	//// mainnet
+	//log.Info().Msg("Artemis: Starting ArtemisEthereumMainnetTxBroadcastWorker")
+	//artemis_ethereum_transcations.ArtemisEthereumMainnetTxBroadcastWorker.Worker.RegisterWorker(c)
+	//err = artemis_ethereum_transcations.ArtemisEthereumMainnetTxBroadcastWorker.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumMainnetTxBroadcastWorker.Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisEthereumMainnetTxBroadcastWorker Started")
+	//
+	//log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnet")
+	//artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet.Worker.RegisterWorker(c)
+	//err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnet Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisMevWorkerMainnet Started")
+	//
+	//log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnet2")
+	//artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet2.Worker.RegisterWorker(c)
+	//err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnet2.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnet1 Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisMevWorkerMainnet2 Started")
+	//
+	//log.Info().Msg("Artemis: Starting ArtemisMevWorkerMainnetHistoricalTxs")
+	//artemis_mev_tx_fetcher.ArtemisMevWorkerMainnetHistoricalTxs.Worker.RegisterWorker(c)
+	//err = artemis_mev_tx_fetcher.ArtemisMevWorkerMainnetHistoricalTxs.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisMevWorkerMainnetHistoricalTxs Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisMevWorkerMainnetHistoricalTxs Started")
+	//// ephemeral
+	//log.Info().Msg("Artemis: Starting ArtemisEthereumEphemeralTxBroadcastWorker")
+	//artemis_ethereum_transcations.ArtemisEthereumEphemeralTxBroadcastWorker.Worker.RegisterWorker(c)
+	//err = artemis_ethereum_transcations.ArtemisEthereumEphemeralTxBroadcastWorker.Worker.Start()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msgf("Artemis: %s ArtemisEthereumEphemeralTxBroadcastWorker.Worker.Start failed", env)
+	//	misc.DelayedPanic(err)
+	//}
+	//log.Info().Msg("Artemis: ArtemisEthereumEphemeralTxBroadcastWorker Started")
+	//
+	//// Start server
+	//log.Info().Msg("Artemis: Starting Server")
+	//if env == "local" || env == "production-local" {
+	//	srv.E.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	//		AllowOrigins:     []string{"http://localhost:3000"},
+	//		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+	//		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+	//		AllowCredentials: true,
+	//	}))
+	//} else {
+	//	srv.E.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	//		AllowOrigins:     []string{"https://cloud.zeus.fyi", "https://api.zeus.fyi", "https://hestia.zeus.fyi"},
+	//		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+	//		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Headers", "X-CSRF-Token", "Accept-Encoding", "CloudCtxNsID"},
+	//		AllowCredentials: true,
+	//	}))
+	//}
+	//
+	//artemis_mev_tx_fetcher.InitArtemisUniswap(ctx, artemis_orchestration_auth.Bearer)
 	srv.E = artemis_api_router.Routes(srv.E)
 	srv.Start()
 }
